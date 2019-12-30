bec323e7e2da3d417854d5e5d731dcd62ecbca1a
bufdev <bufdev-github@buf.build>
2019-12-30 13:15:39 -0500

    Update to v0.4.1

--- internal/buf/cmd/buf/buf.go
+++ internal/buf/cmd/buf/buf.go
@@ -2,7 +2,7 @@ package buf
 
 import "github.com/bufbuild/cli/clicobra"
 
-const version = "0.5.0-dev"
+const version = "0.4.1"
 
 // Main is the main.
 func Main(use string, options ...RootCommandOption) {
