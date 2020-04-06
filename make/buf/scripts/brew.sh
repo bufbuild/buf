ae7cd52e2fad651446f0fe899e43422130072dac
bufdev <bufdev-github@buf.build>
2020-04-06 16:49:17 -0400

    Add brew script

---
+++ make/buf/scripts/brew.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+DIR="$(cd "$(dirname "${0}")/../../.." && pwd)"
+cd "${DIR}"
+
+if [ -z "${1}" ]; then
+  echo "usage: ${0} out_dir" >&2
+  exit 1
+fi
+
+OUT_DIR="${1}"
+rm -rf "${OUT_DIR}"
+mkdir -p "${OUT_DIR}/bin"
+mkdir -p "${OUT_DIR}/etc/bash_completion.d"
+mkdir -p "${OUT_DIR}/etc/zsh/site-functions"
+
+set -x
+
+go build -ldflags "-s -w" -trimpath -o "${OUT_DIR}/bin/buf" "./cmd/buf"
+go build -ldflags "-s -w" -trimpath -o "${OUT_DIR}/bin/protoc-gen-buf-check-breaking" "./cmd/protoc-gen-buf-check-breaking"
+go build -ldflags "-s -w" -trimpath -o "${OUT_DIR}/bin/protoc-gen-buf-check-lint" "./cmd/protoc-gen-buf-check-lint"
+"${OUT_DIR}/bin/buf" bash-completion > "${OUT_DIR}/etc/bash_completion.d/buf"
+"${OUT_DIR}/bin/buf" zsh-completion > "${OUT_DIR}/etc/zsh/site-functions/_buf"
+cp "LICENSE" "${OUT_DIR}/LICENSE"
