08fb6ab1bb1f6b1b1f1d1d6565d82f178d49c2fb
bufdev <4228796+bufdev@users.noreply.github.com>
2020-05-31 13:32:40 -0400

    Move to google.golang.org/protobuf protoc-gen-go plugin (#75)

--- make/go/dep_protoc_gen_go.mk
+++ make/go/dep_protoc_gen_go.mk
@@ -7,16 +7,16 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,GOBIN)
 
 # Settable
-# https://github.com/golang/protobuf/releases 20200514 checked 20200531
-PROTOC_GEN_GO_VERSION ?= v1.4.2
+# https://github.com/protocolbuffers/protobuf-go/releases 20200527 checked 20200531
+PROTOC_GEN_GO_VERSION ?= v1.24.0
 
-GO_GET_PKGS := $(GO_GET_PKGS) github.com/golang/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
+GO_GET_PKGS := $(GO_GET_PKGS) google.golang.org/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
 
 PROTOC_GEN_GO := $(CACHE_VERSIONS)/protoc-gen-go/$(PROTOC_GEN_GO_VERSION)
 $(PROTOC_GEN_GO):
 	@rm -f $(GOBIN)/protoc-gen-go
 	$(eval PROTOC_GEN_GO_TMP := $(shell mktemp -d))
-	cd $(PROTOC_GEN_GO_TMP); go get github.com/golang/protobuf/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
+	cd $(PROTOC_GEN_GO_TMP); go get google.golang.org/protobuf/cmd/protoc-gen-go@$(PROTOC_GEN_GO_VERSION)
 	@rm -rf $(PROTOC_GEN_GO_TMP)
 	@rm -rf $(dir $(PROTOC_GEN_GO))
 	@mkdir -p $(dir $(PROTOC_GEN_GO))
