5704dbaf92a0b839e139d45bffcb4ff1e7b57414
bufdev <bufdev-github@buf.build>
2020-06-25 12:47:29 -0400

    Update to v0.18.1

--- internal/buf/cmd/buf/buf.go
+++ internal/buf/cmd/buf/buf.go
@@ -21,7 +21,7 @@ import (
 	"github.com/bufbuild/buf/internal/pkg/app/appflag"
 )
 
-const version = "0.19.0-dev"
+const version = "0.18.1"
 
 // Main is the main.
 func Main(use string, options ...RootCommandOption) {
