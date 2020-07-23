2fa8108f79874b47be9917e5b82acc6c147234bc
bufdev <bufdev-github@buf.build>
2020-07-23 18:40:51 -0400

    Fix issue where parser would fail on files starting with byte order marks

---
+++ internal/buf/bufcli/bufcli.go
@@ -0,0 +1,66 @@
+// Copyright 2020 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bufcli
+
+import (
+	"net/http"
+
+	"github.com/bufbuild/buf/internal/buf/buffetch"
+	"github.com/bufbuild/buf/internal/pkg/git"
+	"github.com/bufbuild/buf/internal/pkg/httpauth"
+	"go.uber.org/zap"
+)
+
+// Constants used by the buf CLI
+const (
+	inputHTTPSUsernameEnvKey      = "BUF_INPUT_HTTPS_USERNAME"
+	inputHTTPSPasswordEnvKey      = "BUF_INPUT_HTTPS_PASSWORD"
+	inputSSHKeyFileEnvKey         = "BUF_INPUT_SSH_KEY_FILE"
+	inputSSHKnownHostsFilesEnvKey = "BUF_INPUT_SSH_KNOWN_HOSTS_FILES"
+)
+
+var (
+	// defaultHTTPClient is the client we use for HTTP requests.
+	// Timeout should be set through context for calls to EnvReader, not through http.Client
+	defaultHTTPClient = &http.Client{}
+	// defaultHTTPAuthenticator is the default authenticator
+	// used for HTTP requests.
+	defaultHTTPAuthenticator = httpauth.NewMultiAuthenticator(
+		httpauth.NewNetrcAuthenticator(),
+		// must keep this for legacy purposes
+		httpauth.NewEnvAuthenticator(
+			inputHTTPSPasswordEnvKey,
+			inputHTTPSPasswordEnvKey,
+		),
+	)
+	// defaultGitClonerOptions defines the default git clone options.
+	defaultGitClonerOptions = git.ClonerOptions{
+		HTTPSUsernameEnvKey:      inputHTTPSUsernameEnvKey,
+		HTTPSPasswordEnvKey:      inputHTTPSPasswordEnvKey,
+		SSHKeyFileEnvKey:         inputSSHKeyFileEnvKey,
+		SSHKnownHostsFilesEnvKey: inputSSHKnownHostsFilesEnvKey,
+	}
+)
+
+// NewFetchReader creates a new buffetch.Reader with the default HTTP client
+// and git cloner.
+func NewFetchReader(logger *zap.Logger) buffetch.Reader {
+	return buffetch.NewReader(
+		logger,
+		defaultHTTPClient,
+		defaultHTTPAuthenticator,
+		git.NewCloner(logger, defaultGitClonerOptions),
+	)
+}
