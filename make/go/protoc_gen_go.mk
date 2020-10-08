1b3199c29d6120842f88924285e53e564c733116
bufdev <4228796+bufdev@users.noreply.github.com>
2020-10-08 12:02:02 -0400

    Delete protoc_gen_plugin.bash (#156)

--- make/go/protoc_gen_go.mk
+++ make/go/protoc_gen_go.mk
@@ -4,46 +4,26 @@
 $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
 $(call _conditional_include,$(MAKEGO)/dep_buf.mk)
-$(call _conditional_include,$(MAKEGO)/dep_protoc.mk)
 $(call _conditional_include,$(MAKEGO)/dep_protoc_gen_go.mk)
-# Must be set
-$(call _assert_var,PROTO_PATH)
 # Must be set
 $(call _assert_var,PROTOC_GEN_GO_OUT)
 $(call _assert_var,CACHE_BIN)
-$(call _assert_var,CACHE_INCLUDE)
-$(call _assert_var,PROTOC)
+$(call _assert_var,BUF)
 $(call _assert_var,PROTOC_GEN_GO)
 
 # Not modifiable for now
 PROTOC_GEN_GO_OPT := paths=source_relative
 
-EXTRA_MAKEGO_FILES := $(EXTRA_MAKEGO_FILES) scripts/protoc_gen_plugin.bash
-
-PROTOC_GEN_GO_EXTRA_FLAGS :=
-ifdef USE_BUF_PROTOC
-PROTOC_GEN_GO_EXTRA_FLAGS := --use-buf-protoc
-endif
-ifdef USE_BUF_PROTOC_BY_DIR
-PROTOC_GEN_GO_EXTRA_FLAGS := --use-buf-protoc-by-dir
-endif
-ifdef USE_BUF_GENERATE
-PROTOC_GEN_GO_EXTRA_FLAGS := --use-buf-generate
-endif
-
 .PHONY: protocgengoclean
 protocgengoclean:
 	rm -rf "$(PROTOC_GEN_GO_OUT)"
+	mkdir -p "$(PROTOC_GEN_GO_OUT)"
 
 .PHONY: protocgengo
-protocgengo: protocgengoclean $(PROTOC) $(BUF) $(PROTOC_GEN_GO)
-	bash $(MAKEGO)/scripts/protoc_gen_plugin.bash $(PROTOC_GEN_GO_EXTRA_FLAGS) \
-		"--buf_path=$(CACHE_BIN)/buf" \
-		"--proto_path=$(PROTO_PATH)" \
-		"--proto_include_path=$(CACHE_INCLUDE)" \
-		$(patsubst %,--proto_include_path=%,$(PROTO_INCLUDE_PATHS)) \
-		"--plugin_name=go" \
-		"--plugin_out=$(PROTOC_GEN_GO_OUT)" \
-		"--plugin_opt=$(PROTOC_GEN_GO_OPT)"
+protocgengo: protocgengoclean $(BUF) $(PROTOC_GEN_GO)
+	$(CACHE_BIN)/buf beta generate \
+		--plugin go \
+		--plugin-out $(PROTOC_GEN_GO_OUT) \
+		--plugin-opt $(PROTOC_GEN_GO_OPT)
 
-protocgenerate:: protocgengo
+bufgenerate:: protocgengo
