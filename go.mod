65a83c06cd5584686f985e3c16910368de4393ba
bufdev <4228796+bufdev@users.noreply.github.com>
2021-01-18 16:35:09 -0500

    Add rpc package from internal (#245)

--- go.mod
+++ go.mod
@@ -11,14 +11,21 @@ require (
 	github.com/jhump/protoreflect v1.8.1
 	github.com/klauspost/compress v1.11.7
 	github.com/klauspost/pgzip v1.2.5
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/profile v1.5.0
 	github.com/spf13/cobra v1.0.1-0.20201006035406-b97b5ead31f7
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
+	github.com/twitchtv/twirp v7.1.0+incompatible
 	go.opencensus.io v0.22.5
 	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.16.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f // indirect
+	google.golang.org/grpc v1.35.0-dev.0.20201218190559-666aea1fb34c
 	google.golang.org/protobuf v1.25.1-0.20201208041424-160c7477e0e8
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
---
+++ make/go/dep_protoc_gen_go_grpc.mk
@@ -0,0 +1,26 @@
+# Managed by makego. DO NOT EDIT.
+
+# Must be set
+$(call _assert_var,MAKEGO)
+$(call _conditional_include,$(MAKEGO)/base.mk)
+$(call _assert_var,CACHE_VERSIONS)
+$(call _assert_var,CACHE_BIN)
+
+# Settable
+# https://github.com/grpc/grpc-go/commits/master 20201218 checked 20201222
+PROTOC_GEN_GO_GRPC_VERSION ?= 666aea1fb34c9ceeab997f7a9c37c2cb51deb6e5
+
+GO_GET_PKGS := $(GO_GET_PKGS) google.golang.org/grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
+
+PROTOC_GEN_GO_GRPC := $(CACHE_VERSIONS)/protoc-gen-go-grpc/$(PROTOC_GEN_GO_GRPC_VERSION)
+$(PROTOC_GEN_GO_GRPC):
+	@rm -f $(CACHE_BIN)/protoc-gen-go-grpc
+	$(eval PROTOC_GEN_GO_GRPC_TMP := $(shell mktemp -d))
+	cd $(PROTOC_GEN_GO_GRPC_TMP); GOBIN=$(CACHE_BIN) go get \
+		google.golang.org/grpc/cmd/protoc-gen-go-grpc@$(PROTOC_GEN_GO_GRPC_VERSION)
+	@rm -rf $(PROTOC_GEN_GO_GRPC_TMP)
+	@rm -rf $(dir $(PROTOC_GEN_GO_GRPC))
+	@mkdir -p $(dir $(PROTOC_GEN_GO_GRPC))
+	@touch $(PROTOC_GEN_GO_GRPC)
+
+dockerdeps:: $(PROTOC_GEN_GO_GRPC)
