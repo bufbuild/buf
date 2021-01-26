891b4f2c63ca35605bd461b5667107b4c31b4e6d
bufdev <4228796+bufdev@users.noreply.github.com>
2021-01-26 12:36:54 -0500

    Sync from internal (#256)

--- go.mod
+++ go.mod
@@ -21,10 +21,10 @@ require (
 	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.16.0
 	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto v0.0.0-20210121164019-fc48d45331c7 // indirect
+	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506 // indirect
 	google.golang.org/grpc v1.35.0-dev.0.20201218190559-666aea1fb34c
 	google.golang.org/protobuf v1.25.1-0.20201208041424-160c7477e0e8
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- make/buf/all.mk
+++ make/buf/all.mk
@@ -1,7 +1,148 @@
-COPYRIGHT_OWNER := Buf Technologies, Inc.
-COPYRIGHT_YEAR := 2020-2021
-LICENSE_TYPE := apache
-ADDLICENSE_IGNORES := \/testdata
+BUF_BIN ?= cmd/buf
 
-include make/buf/base.mk
-include make/go/addlicense.mk
+PROTOREFLECT_VERSION := v1.8.1
+# Remove when https://github.com/spf13/cobra/pull/1070 is released
+COBRA_VERSION := b97b5ead31f7d34f764ac8666e40c214bb8e06dc
+GO_GET_PKGS := $(GO_GET_PKGS) \
+	github.com/jhump/protoreflect@$(PROTOREFLECT_VERSION) \
+	github.com/spf13/cobra@$(COBRA_VERSION)
+GO_BINS := $(GO_BINS) \
+	$(BUF_BIN) \
+	cmd/protoc-gen-buf-breaking \
+	cmd/protoc-gen-buf-lint \
+	cmd/protoc-gen-buf-check-breaking \
+	cmd/protoc-gen-buf-check-lint \
+	internal/pkg/git/cmd/git-ls-files-unstaged \
+	internal/pkg/storage/cmd/ddiff \
+	internal/pkg/storage/cmd/storage-go-binary-data \
+	internal/pkg/license/cmd/license-header \
+	internal/protoplugin/cmd/protoc-gen-go-api \
+	internal/protoplugin/cmd/protoc-gen-go-apiclient \
+	internal/protoplugin/cmd/protoc-gen-go-apiclientgrpc \
+	internal/protoplugin/cmd/protoc-gen-go-apiclienttwirp \
+	internal/protoplugin/cmd/protoc-gen-proxy
+GO_TEST_BINS := $(GO_TEST_BINS) \
+	internal/buf/cmd/buf/command/protoc/internal/protoc-gen-insertion-point-receiver \
+	internal/buf/cmd/buf/command/protoc/internal/protoc-gen-insertion-point-writer
+DOCKER_BINS := $(DOCKER_BINS) buf
+FILE_IGNORES := $(FILE_IGNORES) \
+	.build/ \
+	.vscode/ \
+	internal/buf/cmd/buf/cache/ \
+	internal/buf/internal/buftesting/cache/ \
+	internal/pkg/storage/storageos/tmp/
+LICENSE_HEADER_LICENSE_TYPE := apache
+LICENSE_HEADER_COPYRIGHT_HOLDER := Buf Technologies, Inc.
+LICENSE_HEADER_YEAR_RANGE := 2020-2021
+LICENSE_HEADER_IGNORES := \/testdata enterprise
+
+include make/go/bootstrap.mk
+include make/go/dep_buf.mk
+include make/go/dep_protoc.mk
+include make/go/dep_protoc_gen_go.mk
+include make/go/dep_protoc_gen_go_grpc.mk
+include make/go/dep_protoc_gen_twirp.mk
+include make/go/dep_go_fuzz.mk
+include make/go/go.mk
+include make/go/docker.mk
+
+# Settable
+BUF_BREAKING_PROTO_INPUT ?= .git\#branch=master,subdir=proto
+
+installtest:: $(PROTOC) $(PROTOC_GEN_GO)
+
+.PHONY: wkt
+wkt: installstorage-go-binary-data $(PROTOC)
+	rm -rf internal/gen/data
+	mkdir -p internal/gen/data/wkt
+	storage-go-binary-data $(CACHE_INCLUDE) --package wkt > internal/gen/data/wkt/wkt.gen.go
+
+prepostgenerate:: wkt
+
+.PHONY: licenseheader
+licenseheader: installlicense-header installgit-ls-files-unstaged
+	license-header \
+		--license-type "$(LICENSE_HEADER_LICENSE_TYPE)" \
+		--copyright-holder "$(LICENSE_HEADER_COPYRIGHT_HOLDER)" \
+		--year-range "$(LICENSE_HEADER_YEAR_RANGE)" \
+		$(shell git-ls-files-unstaged | grep -v $(patsubst %,-e %,$(sort $(LICENSE_HEADER_IGNORES))))
+
+licensegenerate:: licenseheader
+
+# Runtime variable BUFBINARYHEAD uses the buf binary installed
+# from this repository to do buf generate instead of using a
+# downloaded release binary. We typically use the downloaded
+# release binary as when generating, the codebase can break,
+# and if we have generate depend on installing buf, we have
+# a chicken or egg problem.
+
+.PHONY: bufbinaryinstall
+ifeq ($(BUFBINARYHEAD),)
+BUF_BINARY := $(CACHE_BIN)/buf
+bufbinaryinstall: $(BUF)
+else
+BUF_BINARY := $(CACHE_GOBIN)/buf
+bufbinaryinstall: installbuf
+endif
+
+.PHONY: bufgeneratedeps
+bufgeneratedeps:: \
+	bufbinaryinstall \
+	installprotoc-gen-go-api \
+	installprotoc-gen-go-apiclient \
+	installprotoc-gen-go-apiclientgrpc \
+	installprotoc-gen-go-apiclienttwirp \
+	$(PROTOC_GEN_GO) $(PROTOC_GEN_GO_GRPC) $(PROTOC_GEN_TWIRP)
+
+.PHONY: bufgenerateclean
+bufgenerateclean::
+
+.PHONY: bufgeneratecleango
+bufgeneratecleango:
+	rm -rf internal/gen/proto
+
+bufgenerateclean:: bufgeneratecleango
+
+.PHONY: bufgeneratesteps
+bufgeneratesteps::
+
+.PHONY: bufgenerateprotogo
+bufgenerateprotogo:
+	$(BUF_BINARY) generate proto --template data/buf/template/buf.go.gen.yaml
+
+.PHONY: bufgenerateprotogoclient
+bufgenerateprotogoclient:
+	$(BUF_BINARY) generate proto --template data/buf/template/buf.go-client.gen.yaml
+
+bufgeneratesteps:: \
+	bufgenerateprotogo \
+	bufgenerateprotogoclient
+
+.PHONY: bufgenerate
+bufgenerate:
+	$(MAKE) bufgeneratedeps
+	$(MAKE) bufgenerateclean
+	$(MAKE) bufgeneratesteps
+
+pregenerate:: bufgenerate
+
+.PHONY: buflintproto
+buflintproto: bufbinaryinstall
+	$(BUF_BINARY) lint proto
+
+.PHONY: bufbreakingproto
+bufbreakingproto: bufbinaryinstall
+	-$(BUF_BINARY) breaking proto --against $(BUF_BREAKING_PROTO_INPUT)
+
+postlint:: buflintproto bufbreakingproto
+
+.PHONY: bufrelease
+bufrelease:
+	DOCKER_IMAGE=golang:1.15.7-buster bash make/buf/scripts/release.bash
+
+.PHONY: gofuzz
+gofuzz: $(GO_FUZZ)
+	@rm -rf $(TMP)/gofuzz
+	@mkdir -p $(TMP)/gofuzz
+	cd internal/buf/bufbuild/bufbuildtesting; go-fuzz-build -o $(abspath $(TMP))/gofuzz/gofuzz.zip
+	go-fuzz -bin $(TMP)/gofuzz/gofuzz.zip -workdir $(TMP)/gofuzz
