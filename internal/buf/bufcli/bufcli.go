bfc44f82f291a6650c18755083288a69ced8f663
bufdev <bufdev-github@buf.build>
2021-02-09 14:26:41 -0500

    Back to development

--- internal/buf/bufcli/bufcli.go
+++ internal/buf/bufcli/bufcli.go
@@ -52,7 +52,7 @@ import (
 
 const (
 	// Version is the version of buf.
-	Version = "0.37.0"
+	Version = "0.38.0-dev"
 
 	// FlagDeprecationMessageSuffix is the suffix for flag deprecation messages.
 	FlagDeprecationMessageSuffix = `
