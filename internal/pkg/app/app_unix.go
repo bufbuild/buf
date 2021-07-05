66e2e3fade3b3e670ae5e0c4c8c6d4299e8aae3c
bufdev <4228796+bufdev@users.noreply.github.com>
2021-07-05 12:29:21 -0400

    Expand build tags in app packages (#365)

--- internal/pkg/app/app_unix.go
+++ internal/pkg/app/app_unix.go
@@ -12,7 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin linux
+// Matching the unix-like build tags in the Golang source i.e. https://github.com/golang/go/blob/912f0750472dd4f674b69ca1616bfaf377af1805/src/os/file_unix.go#L6
+//
+// We expanded this to all unix-like platforms, including those we don't support, as most
+// of this should work without issue, and there are bigger problems with supporting i.e. js,wasm
+// that are outside the scope of these build tags. Being able to build buf on i.e. openbsd
+// was a blocker, see https://github.com/bufbuild/buf/issues/362 and the linked discussions.
+// We still only officially support linux and darwin for buf as a whole.
+
+// +build aix darwin dragonfly freebsd js,wasm linux netbsd openbsd solaris
 
 package app
 
