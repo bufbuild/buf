7e7555c12fe28f3ad4ae5a723556f2012aa478b3
bufdev <bufdev-github@buf.build>
2021-07-26 14:53:03 -0400

    Back to development

--- make/go/dep_buf.mk
+++ make/go/dep_buf.mk
@@ -8,7 +8,7 @@ $(call _assert_var,CACHE_BIN)
 
 # Settable
 # https://github.com/bufbuild/buf/releases
-BUF_VERSION ?= v0.44.0
+BUF_VERSION ?= v0.45.0
 
 BUF := $(CACHE_VERSIONS)/buf/$(BUF_VERSION)
 $(BUF):
