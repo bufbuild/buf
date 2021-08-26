4346f82b03a3d9a3686d451507a204ccf38ed40f
bufdev <4228796+bufdev@users.noreply.github.com>
2021-08-26 12:53:46 -0400

    Move internal/buf to private/buf (#453)

---
+++ private/buf/cmd/protoc-gen-buf-lint/lint.go
@@ -0,0 +1,131 @@
+// Copyright 2020-2021 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lint
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/bufbuild/buf/private/buf/bufcheck/buflint"
+	"github.com/bufbuild/buf/private/buf/bufconfig"
+	"github.com/bufbuild/buf/private/bufpkg/bufimage"
+	"github.com/bufbuild/buf/private/pkg/app"
+	"github.com/bufbuild/buf/private/pkg/app/applog"
+	"github.com/bufbuild/buf/private/pkg/app/appproto"
+	"github.com/bufbuild/buf/private/pkg/encoding"
+	"github.com/bufbuild/buf/private/pkg/storage/storageos"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+const defaultTimeout = 10 * time.Second
+
+// Main is the main.
+func Main() {
+	appproto.Main(
+		context.Background(),
+		appproto.HandlerFunc(
+			func(
+				ctx context.Context,
+				container app.EnvStderrContainer,
+				responseWriter appproto.ResponseWriter,
+				request *pluginpb.CodeGeneratorRequest,
+			) error {
+				return handle(
+					ctx,
+					container,
+					responseWriter,
+					request,
+				)
+			},
+		),
+	)
+}
+
+func handle(
+	ctx context.Context,
+	container app.EnvStderrContainer,
+	responseWriter appproto.ResponseWriter,
+	request *pluginpb.CodeGeneratorRequest,
+) error {
+	responseWriter.SetFeatureProto3Optional()
+	externalConfig := &externalConfig{}
+	if err := encoding.UnmarshalJSONOrYAMLStrict(
+		[]byte(request.GetParameter()),
+		externalConfig,
+	); err != nil {
+		return err
+	}
+	timeout := externalConfig.Timeout
+	if timeout == 0 {
+		timeout = defaultTimeout
+	}
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	logger, err := applog.NewLogger(container.Stderr(), externalConfig.LogLevel, externalConfig.LogFormat)
+	if err != nil {
+		return err
+	}
+	storageosProvider := storageos.NewProvider(storageos.ProviderWithSymlinks())
+	readWriteBucket, err := storageosProvider.NewReadWriteBucket(
+		".",
+		storageos.ReadWriteBucketWithSymlinksIfSupported(),
+	)
+	if err != nil {
+		return err
+	}
+	config, err := bufconfig.ReadConfig(
+		ctx,
+		bufconfig.NewProvider(logger),
+		readWriteBucket,
+		bufconfig.ReadConfigWithOverride(
+			encoding.GetJSONStringOrStringValue(externalConfig.InputConfig),
+		),
+	)
+	if err != nil {
+		return err
+	}
+	image, err := bufimage.NewImageForCodeGeneratorRequest(request)
+	if err != nil {
+		return err
+	}
+	image = bufimage.ImageWithoutImports(image)
+	fileAnnotations, err := buflint.NewHandler(logger).Check(
+		ctx,
+		config.Lint,
+		image,
+	)
+	if err != nil {
+		return err
+	}
+	if len(fileAnnotations) > 0 {
+		buffer := bytes.NewBuffer(nil)
+		if err := buflint.PrintFileAnnotations(buffer, fileAnnotations, externalConfig.ErrorFormat); err != nil {
+			return err
+		}
+		responseWriter.AddError(strings.TrimSpace(buffer.String()))
+	}
+	return nil
+}
+
+type externalConfig struct {
+	InputConfig json.RawMessage `json:"input_config,omitempty" yaml:"input_config,omitempty"`
+	LogLevel    string          `json:"log_level,omitempty" yaml:"log_level,omitempty"`
+	LogFormat   string          `json:"log_format,omitempty" yaml:"log_format,omitempty"`
+	ErrorFormat string          `json:"error_format,omitempty" yaml:"error_format,omitempty"`
+	Timeout     time.Duration   `json:"timeout,omitempty" yaml:"timeout,omitempty"`
+}
