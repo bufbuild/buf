6dc31872cda647c372a212395bee50febe7a1d75
Doria Keung <doria.keung@gmail.com>
2021-08-27 11:05:55 -0400

    Add Github Action test CI job on a Windows runner

---
+++ windows/setupExternalPlugins.ps1
@@ -0,0 +1,3 @@
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
+go get google.golang.org/grpc@30dfb4b933a50fd366d7ed36ed4f71dbba2d382e
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@30dfb4b933a50fd366d7ed36ed4f71dbba2d382e
---
+++ windows/setupInternalPlugins.ps1
@@ -0,0 +1,2 @@
+go install .\private\buf\cmd\buf\command\protoc\internal\protoc-gen-insertion-point-writer
+go install .\private\buf\cmd\buf\command\protoc\internal\protoc-gen-insertion-point-receiver
