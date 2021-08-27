5fabacffc542ce36417d88ee3a7c3f8b9ee3efd0
Doria Keung <doria.keung@gmail.com>
2021-08-27 13:42:38 -0400

    Parametize external plugin versions.

--- windows/setupExternalPlugins.ps1
+++ windows/setupExternalPlugins.ps1
@@ -1,3 +1,6 @@
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.27.1
-go get google.golang.org/grpc@30dfb4b933a50fd366d7ed36ed4f71dbba2d382e
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@30dfb4b933a50fd366d7ed36ed4f71dbba2d382e
+$protocGenGoVersion = 'v1.27.1'
+$protocGenGoGRPCVersion = '30dfb4b933a50fd366d7ed36ed4f71dbba2d382e'
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@$protocGenGoVersion
+go get google.golang.org/grpc@$protocGenGoGRPCVersion
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@$protocGenGoGRPCVersion
