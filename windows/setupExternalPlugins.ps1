66b153675a00e287895670dec45d45eeb55082c6
Doria Keung <doria.keung@gmail.com>
2021-08-27 14:37:36 -0400

    Address comments.

--- windows/setupExternalPlugins.ps1
+++ windows/setupExternalPlugins.ps1
@@ -1,6 +1,4 @@
-$protocGenGoVersion = 'v1.27.1'
 $protocGenGoGRPCVersion = '30dfb4b933a50fd366d7ed36ed4f71dbba2d382e'
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@$protocGenGoVersion
-go get google.golang.org/grpc@$protocGenGoGRPCVersion
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@$protocGenGoGRPCVersion
