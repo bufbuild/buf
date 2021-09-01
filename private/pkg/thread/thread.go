d067c51969d8eefd98a7705f1d39c45d35dd5a99
bufdev <4228796+bufdev@users.noreply.github.com>
2021-09-01 12:48:15 -0400

    Expand thread.Parallelize (#502)

--- private/pkg/thread/thread.go
+++ private/pkg/thread/thread.go
@@ -15,6 +15,7 @@
 package thread
 
 import (
+	"context"
 	"runtime"
 	"sync"
 
@@ -52,32 +53,99 @@ func SetParallelism(parallelism int) {
 //
 // A max of Parallelism jobs will be run at once.
 // Returns the combined error from the jobs.
-func Parallelize(jobs ...func() error) error {
+func Parallelize(ctx context.Context, jobs []func(context.Context) error, options ...ParallelizeOption) error {
+	parallelizeOptions := newParallelizeOptions()
+	for _, option := range options {
+		option(parallelizeOptions)
+	}
 	switch len(jobs) {
 	case 0:
 		return nil
 	case 1:
-		return jobs[0]()
+		return jobs[0](ctx)
 	default:
-		semaphoreC := make(chan struct{}, Parallelism())
+		multiplier := parallelizeOptions.multiplier
+		if multiplier < 1 {
+			multiplier = 1
+		}
+		semaphoreC := make(chan struct{}, Parallelism()*multiplier)
 		var retErr error
 		var wg sync.WaitGroup
 		var lock sync.Mutex
+		stop := false
 		for _, job := range jobs {
+			if stop {
+				break
+			}
 			job := job
-			wg.Add(1)
-			semaphoreC <- struct{}{}
-			go func() {
-				if err := job(); err != nil {
-					lock.Lock()
-					retErr = multierr.Append(retErr, err)
-					lock.Unlock()
-				}
-				<-semaphoreC
-				wg.Done()
-			}()
+			// First, check if the context is done before even hitting a new job start.
+			// We don't want to do the select with the semaphore as we want the context
+			// being done to take precedence.
+			select {
+			case <-ctx.Done():
+				stop = true
+			default:
+			}
+			if stop {
+				// We want to break the for loop without using labels/gotos but if
+				// we put a break inside the select then it only breaks the select
+				break
+			}
+			// Next, we still do a select between the context and the semaphore, so that
+			// if we are blocking on the semaphore, and then the context is cancelled,
+			// we will then end up breaking.
+			select {
+			case <-ctx.Done():
+				stop = true
+			case semaphoreC <- struct{}{}:
+				wg.Add(1)
+				go func() {
+					if err := job(ctx); err != nil {
+						lock.Lock()
+						retErr = multierr.Append(retErr, err)
+						lock.Unlock()
+						if parallelizeOptions.cancel != nil {
+							parallelizeOptions.cancel()
+						}
+					}
+					// This will never block.
+					<-semaphoreC
+					wg.Done()
+				}()
+			}
 		}
 		wg.Wait()
 		return retErr
 	}
+}
+
+// ParallelizeOption is an option to Parallelize.
+type ParallelizeOption func(*parallelizeOptions)
+
+// ParallelizeWithMultiplier returns a new ParallelizeOption that will use a multiple
+// of Parallelism() for the number of jobs that can be run at once.
+//
+// The default is to only do Parallelism() number of jobs.
+// A multiplier of <1 has no meaning.
+func ParallelizeWithMultiplier(multiplier int) ParallelizeOption {
+	return func(parallelizeOptions *parallelizeOptions) {
+		parallelizeOptions.multiplier = multiplier
+	}
+}
+
+// ParallelizeWithCancel returns a new ParallelizeOption that will call the
+// given context.CancelFunc if any job fails.
+func ParallelizeWithCancel(cancel context.CancelFunc) ParallelizeOption {
+	return func(parallelizeOptions *parallelizeOptions) {
+		parallelizeOptions.cancel = cancel
+	}
+}
+
+type parallelizeOptions struct {
+	multiplier int
+	cancel     context.CancelFunc
+}
+
+func newParallelizeOptions() *parallelizeOptions {
+	return &parallelizeOptions{}
 }
