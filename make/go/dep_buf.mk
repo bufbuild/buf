15644915b9f930429708011e93414db4fff9b81e
bufdev <bufdev-github@buf.build>
2021-09-07 16:52:25 -0400

    Add ability to install buf directly in makego

--- make/go/dep_buf.mk
+++ make/go/dep_buf.mk
@@ -9,7 +9,24 @@ $(call _assert_var,CACHE_BIN)
 # Settable
 # https://github.com/bufbuild/buf/releases
 BUF_VERSION ?= v0.55.0
+# Settable
+#
+# If set, this path will be installed every time someone depends on $(BUF)
+# as opposed to installing from github with @$(BUF_VERSION).
+#
+# This can be used to always do "go install ./cmd/buf" or
+# "go install github.com/bufbuild/buf/cmd/buf".
+BUF_GO_INSTALL_PATH ?=
+ifneq ($(BUF_GO_INSTALL_PATH),)
+.PHONY: __goinstallbuf
+__goinstallbuf:
+	go install $(BUF_GO_INSTALL_PATH)
 
+BUF := __goinstallbuf
+
+# Use this instead of "buf" when using buf.
+BUF_BIN := $(CACHE_GOBIN)/buf
+else
 BUF := $(CACHE_VERSIONS)/buf/$(BUF_VERSION)
 $(BUF):
 	@rm -f $(CACHE_BIN)/buf
@@ -18,4 +35,11 @@ $(BUF):
 	@mkdir -p $(dir $(BUF))
 	@touch $(BUF)
 
+# Use this instead of "buf" when using buf.
+BUF_BIN := $(CACHE_BIN)/buf
+
 dockerdeps:: $(BUF)
+endif
+
+
+
