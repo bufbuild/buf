f3e4a8bef37d38ae3c4e1c7a127b31a983ff98d0
bufdev <bufdev-github@buf.build>
2021-09-09 09:33:40 -0400

    Cleanup brew.sh

--- make/buf/scripts/brew.sh
+++ make/buf/scripts/brew.sh
@@ -17,17 +17,15 @@ mkdir -p "${OUT_DIR}/etc/bash_completion.d"
 mkdir -p "${OUT_DIR}/share/fish/vendor_completions.d"
 mkdir -p "${OUT_DIR}/share/zsh/site-functions"
 
-set -x
-
-for binary in \
-  buf \
-  protoc-gen-buf-breaking \
-  protoc-gen-buf-lint \
-  protoc-gen-buf-check-breaking \
-  protoc-gen-buf-check-lint; do
+for binary in buf protoc-gen-buf-breaking protoc-gen-buf-lint; do
+  echo go build -ldflags \"-s -w\" -trimpath -o \"${OUT_DIR}/bin/${binary}\" \"cmd/${binary}/main.go\"
   go build -ldflags "-s -w" -trimpath -o "${OUT_DIR}/bin/${binary}" "cmd/${binary}/main.go"
 done
+echo \"${OUT_DIR}/bin/buf\" bash-completion \> \"${OUT_DIR}/etc/bash_completion.d/buf\"
 "${OUT_DIR}/bin/buf" bash-completion > "${OUT_DIR}/etc/bash_completion.d/buf"
+echo \"${OUT_DIR}/bin/buf\" fish-completion \> \"${OUT_DIR}/share/fish/vendor_completions.d/buf.fish\"
 "${OUT_DIR}/bin/buf" fish-completion > "${OUT_DIR}/share/fish/vendor_completions.d/buf.fish"
+echo \"${OUT_DIR}/bin/buf\" zsh-completion \> \"${OUT_DIR}/share/zsh/site-functions/_buf\"
 "${OUT_DIR}/bin/buf" zsh-completion > "${OUT_DIR}/share/zsh/site-functions/_buf"
+echo cp \"LICENSE\" \"${OUT_DIR}/LICENSE\"
 cp "LICENSE" "${OUT_DIR}/LICENSE"
