e60437d8828b44c084d2350759bfd9face920d47
bufdev <bufdev-github@buf.build>
2021-09-19 10:22:16 -0400

    Use variable

--- windows/setupDeps.ps1
+++ windows/setupDeps.ps1
@@ -1,9 +1,9 @@
-$protocVersion = 'v3.18.0'
+$protocVersion = '3.18.0'
 $protocGenGoVersion = 'v1.27.1'
 $protocGenGoGRPCVersion = '30dfb4b933a50fd366d7ed36ed4f71dbba2d382e'
 
 choco install --confirm curl zip
-curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v3.18.0/protoc-3.18.0-win64.zip -o protoc.zip
+curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-win64.zip -o protoc.zip
 unzip protoc.zip
 # TODO: move bin/protoc to somewhere on the PATH
 go install google.golang.org/protobuf/cmd/protoc-gen-go@$protocGenGoVersion
