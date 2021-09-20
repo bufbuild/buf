7ab7c009261dba5bdc937a61906127313e5c96ef
Doria Keung <doria.keung@gmail.com>
2021-09-20 10:21:20 -0400

    Check existing path

--- windows/setupDeps.ps1
+++ windows/setupDeps.ps1
@@ -5,7 +5,7 @@ $protocGenGoGRPCVersion = '30dfb4b933a50fd366d7ed36ed4f71dbba2d382e'
 choco install --confirm curl zip
 curl -sSL https://github.com/protocolbuffers/protobuf/releases/download/v$protocVersion/protoc-$protocVersion-win64.zip -o protoc.zip
 unzip protoc.zip
-ls
+echo $env:Path
 # TODO: move bin/protoc to somewhere on the PATH
 go install google.golang.org/protobuf/cmd/protoc-gen-go@$protocGenGoVersion
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@$protocGenGoGRPCVersion
