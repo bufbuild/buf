7e6763b354cc3bddbc405140f33ef06f52f946b0
Doria Keung <doria.keung@gmail.com>
2021-10-18 14:06:21 -0400

    Add comment to explain why we need to check for proto file ref in the workspace.

--- private/buf/bufwire/module_config_reader.go
+++ private/buf/bufwire/module_config_reader.go
@@ -253,6 +253,11 @@ func (m *moduleConfigReader) getProtoFileModuleSourceConfigs(
 			}
 			readBucketCloser.SetSubDirPath(normalpath.Normalize(relativePath))
 		} else {
+			// If there are no module configs in the path to the workspace, we need to check whether or not
+			// proto file ref is contained within one of the workspace directories.
+			// If yes, we can set the `SubDirPath` for the bucket to the directory, to ensure we build all the
+			// dependencies for the directory. If not, then we will keep the relative directory path of the proto file
+			// ref as the SubDirPath.
 			workspaceConfig, err := bufwork.GetConfigForBucket(ctx, readBucketCloser, readBucketCloser.RelativeRootPath())
 			if err != nil {
 				return nil, err
@@ -260,6 +265,7 @@ func (m *moduleConfigReader) getProtoFileModuleSourceConfigs(
 			for _, directory := range workspaceConfig.Directories {
 				if normalpath.EqualsOrContainsPath(directory, readBucketCloser.SubDirPath(), normalpath.Relative) {
 					readBucketCloser.SetSubDirPath(normalpath.Normalize(directory))
+					break
 				}
 			}
 		}
