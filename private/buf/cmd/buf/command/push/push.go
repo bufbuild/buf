f2f46aeecd8fe222ab5b035741dbc3d3e333506f
bufdev <bufdev-github@buf.build>
2021-10-22 16:26:34 -0400

    Fix

--- private/buf/cmd/buf/command/push/push.go
+++ private/buf/cmd/buf/command/push/push.go
@@ -47,7 +47,7 @@ func NewCommand(
 ) *appcmd.Command {
 	flags := newFlags()
 	return &appcmd.Command{
-		Use:   name + " <input>",
+		Use:   name + " <source>",
 		Short: "Push a module to a registry.",
 		Long:  bufcli.GetSourceLong(`the source to push`),
 		Args:  cobra.MaximumNArgs(1),
