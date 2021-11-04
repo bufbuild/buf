f2ec5ca11ca498abf5126c7dab7d4f5be70ca699
Samantha Small <ssmall@buf.build>
2021-11-04 14:40:35 +0000

    adding rpcs for searching users, plugins, templates

--- private/gen/proto/go/buf/alpha/registry/v1alpha1/search_grpc.pb.go
+++ private/gen/proto/go/buf/alpha/registry/v1alpha1/search_grpc.pb.go
@@ -38,6 +38,12 @@ const _ = grpc.SupportPackageIsVersion7
 type SearchServiceClient interface {
 	// Search searches the BSR.
 	Search(ctx context.Context, in *SearchRequest, opts ...grpc.CallOption) (*SearchResponse, error)
+	// SearchUsers searches the users in the BSR.
+	SearchUsers(ctx context.Context, in *SearchUsersRequest, opts ...grpc.CallOption) (*SearchUsersResponse, error)
+	// SearchPlugins searches the plugins in the  BSR.
+	SearchPlugins(ctx context.Context, in *SearchPluginsRequest, opts ...grpc.CallOption) (*SearchPluginsResponse, error)
+	// SearchTemplates searches the templates in the BSR.
+	SearchTemplates(ctx context.Context, in *SearchTemplatesRequest, opts ...grpc.CallOption) (*SearchTemplatesResponse, error)
 }
 
 type searchServiceClient struct {
@@ -57,12 +63,45 @@ func (c *searchServiceClient) Search(ctx context.Context, in *SearchRequest, opt
 	return out, nil
 }
 
+func (c *searchServiceClient) SearchUsers(ctx context.Context, in *SearchUsersRequest, opts ...grpc.CallOption) (*SearchUsersResponse, error) {
+	out := new(SearchUsersResponse)
+	err := c.cc.Invoke(ctx, "/buf.alpha.registry.v1alpha1.SearchService/SearchUsers", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *searchServiceClient) SearchPlugins(ctx context.Context, in *SearchPluginsRequest, opts ...grpc.CallOption) (*SearchPluginsResponse, error) {
+	out := new(SearchPluginsResponse)
+	err := c.cc.Invoke(ctx, "/buf.alpha.registry.v1alpha1.SearchService/SearchPlugins", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *searchServiceClient) SearchTemplates(ctx context.Context, in *SearchTemplatesRequest, opts ...grpc.CallOption) (*SearchTemplatesResponse, error) {
+	out := new(SearchTemplatesResponse)
+	err := c.cc.Invoke(ctx, "/buf.alpha.registry.v1alpha1.SearchService/SearchTemplates", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SearchServiceServer is the server API for SearchService service.
 // All implementations should embed UnimplementedSearchServiceServer
 // for forward compatibility
 type SearchServiceServer interface {
 	// Search searches the BSR.
 	Search(context.Context, *SearchRequest) (*SearchResponse, error)
+	// SearchUsers searches the users in the BSR.
+	SearchUsers(context.Context, *SearchUsersRequest) (*SearchUsersResponse, error)
+	// SearchPlugins searches the plugins in the  BSR.
+	SearchPlugins(context.Context, *SearchPluginsRequest) (*SearchPluginsResponse, error)
+	// SearchTemplates searches the templates in the BSR.
+	SearchTemplates(context.Context, *SearchTemplatesRequest) (*SearchTemplatesResponse, error)
 }
 
 // UnimplementedSearchServiceServer should be embedded to have forward compatible implementations.
@@ -71,6 +110,15 @@ type UnimplementedSearchServiceServer struct {
 
 func (UnimplementedSearchServiceServer) Search(context.Context, *SearchRequest) (*SearchResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Search not implemented")
+}
+func (UnimplementedSearchServiceServer) SearchUsers(context.Context, *SearchUsersRequest) (*SearchUsersResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchUsers not implemented")
+}
+func (UnimplementedSearchServiceServer) SearchPlugins(context.Context, *SearchPluginsRequest) (*SearchPluginsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchPlugins not implemented")
+}
+func (UnimplementedSearchServiceServer) SearchTemplates(context.Context, *SearchTemplatesRequest) (*SearchTemplatesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchTemplates not implemented")
 }
 
 // UnsafeSearchServiceServer may be embedded to opt out of forward compatibility for this service.
@@ -102,6 +150,60 @@ func _SearchService_Search_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SearchService_SearchUsers_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SearchUsersRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SearchServiceServer).SearchUsers(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/buf.alpha.registry.v1alpha1.SearchService/SearchUsers",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SearchServiceServer).SearchUsers(ctx, req.(*SearchUsersRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SearchService_SearchPlugins_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SearchPluginsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SearchServiceServer).SearchPlugins(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/buf.alpha.registry.v1alpha1.SearchService/SearchPlugins",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SearchServiceServer).SearchPlugins(ctx, req.(*SearchPluginsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _SearchService_SearchTemplates_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SearchTemplatesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SearchServiceServer).SearchTemplates(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/buf.alpha.registry.v1alpha1.SearchService/SearchTemplates",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SearchServiceServer).SearchTemplates(ctx, req.(*SearchTemplatesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // SearchService_ServiceDesc is the grpc.ServiceDesc for SearchService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -112,6 +214,18 @@ var SearchService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Search",
 			Handler:    _SearchService_Search_Handler,
+		},
+		{
+			MethodName: "SearchUsers",
+			Handler:    _SearchService_SearchUsers_Handler,
+		},
+		{
+			MethodName: "SearchPlugins",
+			Handler:    _SearchService_SearchPlugins_Handler,
+		},
+		{
+			MethodName: "SearchTemplates",
+			Handler:    _SearchService_SearchTemplates_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
