ff6063800d13eb06cedb60a57743feb5768921d2
cyinma <33741651+cyinma@users.noreply.github.com>
2021-11-23 10:18:47 -0500

    add endpoints for explicit resources role (#736)

--- private/gen/proto/api/buf/alpha/registry/v1alpha1/registryv1alpha1api/repository.pb.go
+++ private/gen/proto/api/buf/alpha/registry/v1alpha1/registryv1alpha1api/repository.pb.go
@@ -83,4 +83,11 @@ type RepositoryService interface {
 	// GetRepositoriesByFullName gets repositories by full name. Response order is unspecified.
 	// Errors if any of the repositories don't exist or the caller does not have access to any of the repositories.
 	GetRepositoriesByFullName(ctx context.Context, fullNames []string) (repositories []*v1alpha1.Repository, err error)
+	// SetRepositoryContributor sets the role of a user in the repository.
+	SetRepositoryContributor(
+		ctx context.Context,
+		repositoryId string,
+		userId string,
+		repositoryRole v1alpha1.RepositoryRole,
+	) (err error)
 }
