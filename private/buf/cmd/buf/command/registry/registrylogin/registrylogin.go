80bfd9c8763d74d91b0e5ae766c503f922d52401
Luke Rewega <lrewega@buf.build>
2021-12-01 14:00:48 -0500

    Display correct remote in `buf login`
    The message displayed as part of an interactive `buf login` command
    should refer to the Buf Schema Registry address that the user is
    attempting to login to.

--- private/buf/cmd/buf/command/registry/registrylogin/registrylogin.go
+++ private/buf/cmd/buf/command/registry/registrylogin/registrylogin.go
@@ -86,10 +86,15 @@ func run(
 	container appflag.Container,
 	flags *flags,
 ) error {
+	remote := bufrpc.DefaultRemote
+	if container.NumArgs() == 1 {
+		remote = container.Arg(0)
+	}
 	// Do not print unless we are prompting
 	if flags.Username == "" && !flags.TokenStdin {
+		loginMessage := fmt.Sprintf("Login with your Buf Schema Registry username. If you don't have a username, head over to https://%s to create one.\n\n", remote)
 		if _, err := container.Stdout().Write(
-			[]byte("Login with your Buf Schema Registry username. If you don't have a username, head over to https://buf.build to create one.\n\n"),
+			[]byte(loginMessage),
 		); err != nil {
 			return err
 		}
@@ -121,10 +126,6 @@ func run(
 			}
 			return err
 		}
-	}
-	remote := bufrpc.DefaultRemote
-	if container.NumArgs() == 1 {
-		remote = container.Arg(0)
 	}
 	if err := netrc.PutMachines(
 		container,
