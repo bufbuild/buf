fe5fbb478e71c74d2b7b0031c422305b328cd717
Alex McKinney <alexmckinney01@gmail.com>
2022-02-14 17:48:17 -0800

    Add BUFSTYLE_VERSION

--- make/go/dep_bufstyle.mk
+++ make/go/dep_bufstyle.mk
@@ -5,9 +5,12 @@ $(call _assert_var,MAKEGO)
 $(call _conditional_include,$(MAKEGO)/base.mk)
 $(call _assert_var,CACHE_BIN)
 
+# Settable
+BUFSTYLE_VERSION ?= v1.0.0
+
 # Unlike 'buf', we always install bufstyle from source since
 # we don't release it as an independent binary.
-.PHONY: $(BUFSTYLE)
+BUFSTYLE := $(CACHE_VERSIONS)/bufstyle/$(BUFSTYLE_VERSION)
 $(BUFSTYLE):
 	GOBIN=$(CACHE_BIN) go install ./cmd/bufstyle
 
