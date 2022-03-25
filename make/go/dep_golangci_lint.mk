d07f7d079431ac57a6ee872b15337b1585ca930c
bufdev <4228796+bufdev@users.noreply.github.com>
2022-03-25 10:13:15 -0400

    Update makego and add buf format to build (#1026)

--- make/go/dep_golangci_lint.mk
+++ make/go/dep_golangci_lint.mk
@@ -7,9 +7,9 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/golangci/golangci-lint/releases 20220217 checked 20220224
+# https://github.com/golangci/golangci-lint/releases 20220318 checked 20220321
 # Check for new linters and add to .golangci.yml (even if commented out) when upgrading
-GOLANGCI_LINT_VERSION ?= v1.44.2
+GOLANGCI_LINT_VERSION ?= v1.45.0
 
 GOLANGCI_LINT := $(CACHE_VERSIONS)/golangci-lint/$(GOLANGCI_LINT_VERSION)
 $(GOLANGCI_LINT):
