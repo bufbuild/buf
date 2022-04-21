9bf8edca363ea34f9b84fe537ca5731d75d2a80b
Steve Ayers <smaye81@gmail.com>
2022-04-21 12:28:09 -0400

    Adds a Github Service definition (#1078)
    * Github Service
    
    * New Github Service messages
    
    Co-authored-by: Steve Ayers <sayers@uber.com>

--- private/gen/proto/apiclientgrpc/buf/alpha/registry/v1alpha1/registryv1alpha1apiclientgrpc/registryv1alpha1apiclientgrpc.pb.go
+++ private/gen/proto/apiclientgrpc/buf/alpha/registry/v1alpha1/registryv1alpha1apiclientgrpc/registryv1alpha1apiclientgrpc.pb.go
@@ -273,6 +273,29 @@ func (p *provider) NewGenerateService(ctx context.Context, address string) (regi
 	}, nil
 }
 
+func (p *provider) NewGithubService(ctx context.Context, address string) (registryv1alpha1api.GithubService, error) {
+	var contextModifier func(context.Context) context.Context
+	var err error
+	if p.contextModifierProvider != nil {
+		contextModifier, err = p.contextModifierProvider(address)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if p.addressMapper != nil {
+		address = p.addressMapper(address)
+	}
+	clientConn, err := p.clientConnProvider.NewClientConn(ctx, address)
+	if err != nil {
+		return nil, err
+	}
+	return &githubService{
+		logger:          p.logger,
+		client:          v1alpha1.NewGithubServiceClient(clientConn),
+		contextModifier: contextModifier,
+	}, nil
+}
+
 func (p *provider) NewImageService(ctx context.Context, address string) (registryv1alpha1api.ImageService, error) {
 	var contextModifier func(context.Context) context.Context
 	var err error
