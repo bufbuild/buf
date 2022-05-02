b5e1cf93ffcf65693d50d0c8d7d57884f58efbd0
Steve Ayers <sayers@uber.com>
2022-05-02 16:25:58 -0400

    Protoc connect

--- make/go/dep_protoc_gen_connect_go.mk
+++ make/go/dep_protoc_gen_connect_go.mk
@@ -11,6 +11,7 @@ CONNECT_VERSION ?= dea593382913dc82a99a711c9568ad9a71c1be7e
 
 PROTOC_GEN_CONNECT_GO := $(CACHE_VERSIONS)/connect-go/$(CONNECT_VERSION)
 $(PROTOC_GEN_CONNECT_GO):
+	echo "booshy"
 	@rm -f $(CACHE_BIN)/connect-go
 	GOBIN=$(CACHE_BIN) go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go@$(CONNECT_VERSION)
 	@rm -rf $(dir $(PROTOC_GEN_CONNECT_GO))
