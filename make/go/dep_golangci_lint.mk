9600986b405892c3499f257684f7255cc4c95f85
Michael Fridman <mfridman@buf.build>
2022-05-03 12:35:45 -0400

    Upgrade go and makego dependency (#1104)

--- make/go/dep_golangci_lint.mk
+++ make/go/dep_golangci_lint.mk
@@ -7,9 +7,9 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/golangci/golangci-lint/releases 20220318 checked 20220321
+# https://github.com/golangci/golangci-lint/releases 20220324 checked 20220503
 # Check for new linters and add to .golangci.yml (even if commented out) when upgrading
-GOLANGCI_LINT_VERSION ?= v1.45.0
+GOLANGCI_LINT_VERSION ?= v1.45.2
 
 GOLANGCI_LINT := $(CACHE_VERSIONS)/golangci-lint/$(GOLANGCI_LINT_VERSION)
 $(GOLANGCI_LINT):
