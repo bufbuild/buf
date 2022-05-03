ccc840e23a85d86bbf68ac82e0f64b3e360ab695
Steve Ayers <sayers@uber.com>
2022-05-03 12:37:26 -0400

    Unset skip lint

--- make/go/go.mk
+++ make/go/go.mk
@@ -21,7 +21,7 @@ GO_MOD_VERSION ?= 1.18
 # Settable
 GO_ALL_REPO_PKGS ?= ./cmd/... ./internal/...
 # Settable
-SKIP_GOLANGCI_LINT ?= 1
+SKIP_GOLANGCI_LINT ?=
 
 # Runtime
 GOPKGS ?= $(GO_ALL_REPO_PKGS)
