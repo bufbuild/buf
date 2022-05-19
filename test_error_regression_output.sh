238b8c32d0def7aba50d05cf79d62498b57c2cc5
Alan Chiu <awlchiiu@gmail.com>
2022-05-19 15:18:25 -0400

    more

--- test_error_regression_output.sh
+++ test_error_regression_output.sh
@@ -46,12 +46,12 @@ printf "twirp" | ./compare.sh  beta registry plugin delete buf.build/achiu/plugi
 ./compare.sh  beta registry repository get buf.build/achiu/bufcli
 ./compare.sh  beta registry repository deprecate buf.build/achiu/bufcli
 ./compare.sh  beta registry repository undeprecate buf.build/achiu/bufcli
-./compare.sh  beta registry repository delete buf.build/achiu/bufcli
+printf "bufcli" | ./compare.sh  beta registry repository delete buf.build/achiu/bufcli
 ./compare.sh  beta registry repository update buf.build/achiu/bufcli --visibility=public
 ./compare.sh  beta registry tag create buf.build/achiu/paymentapis:<commit SHA> paymenttag
 ./compare.sh  beta registry tag list buf.build/achiu/paymentapis
 ./compare.sh  beta registry track list buf.build/achiu/petapis
-./compare.sh  beta registry track delete buf.build/achiu/petapis
+print "buf.build/achiu/petapis" | ./compare.sh  beta registry track delete buf.build/achiu/petapis
 ./compare.sh  beta registry template create buf.build/achiu/templates/twirp-go --visibility public --config '{"version":"v1","plugins":[{"owner":"library","name":"go","opt":["paths=source_relative"]},{"owner":"achiu","name":"twirp","opt":["paths=source_relative"]}]}'
 ./compare.sh  beta registry template list buf.build
 ./compare.sh  beta registry template deprecate buf.build/achiu/templates/twirp-go
@@ -59,9 +59,11 @@ printf "twirp" | ./compare.sh  beta registry plugin delete buf.build/achiu/plugi
 ./compare.sh  beta registry template list buf.build
 ./compare.sh  beta registry template version create buf.build/achiu/templates/twirp-go --name v1 --config '{"version":"v1","plugin_versions":[{"owner":"library","name":"go","version":"v1.27.1-1"},{"owner":"achiu","name":"twirp","version":"v8.1.0-1"}]}'
 ./compare.sh  beta registry template version list buf.build/achiu/templates/twirp-go
-./compare.sh  beta registry template delete buf.build/achiu/templates/testtemplate
+printf "testtemplate" | ./compare.sh  beta registry template delete buf.build/achiu/templates/testtemplate
 ./compare.sh  alpha protoc petapis
-./compare.sh  alpha registry token create buf.build --note token-CLI
 ./compare.sh  alpha registry token list buf.build
-./compare.sh  alpha registry token get buf.build --token-id <id>
-./compare.sh  alpha registry token delete buf.build --token-id <id>
+./compare.sh  alpha registry token get buf.build --token-id 41746a4b-47cd-4ae0-82d1-9fdebde113c4
+
+# The following is going to be noisy. Maybe we can do this manually?
+# ./compare.sh  alpha registry token create buf.build --note token-CLI # This is going to be different since the output returned is a generated sha
+# ./compare.sh  alpha registry token delete buf.build --token-id <id>
