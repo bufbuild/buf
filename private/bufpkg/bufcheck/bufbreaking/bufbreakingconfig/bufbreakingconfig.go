da1f0a019aa77e0f23ef16662e8a765bdadec796
Doria Keung <doria.keung@gmail.com>
2022-06-08 11:28:12 -0400

    Do not sort in place when converting breaking and lint configs to JSON

--- private/bufpkg/bufcheck/bufbreaking/bufbreakingconfig/bufbreakingconfig.go
+++ private/bufpkg/bufcheck/bufbreaking/bufbreakingconfig/bufbreakingconfig.go
@@ -176,13 +176,18 @@ func configToJSON(config *Config) *configJSON {
 		})
 	}
 	sort.Slice(ignoreIDPathsJSON, func(i, j int) bool { return ignoreIDPathsJSON[i].ID < ignoreIDPathsJSON[j].ID })
-	sort.Strings(config.Use)
-	sort.Strings(config.Except)
-	sort.Strings(config.IgnoreRootPaths)
+	// We should not be sorting in place for the config structure, since it will mutate the
+	// underlying config ordering.
+	use := config.Use
+	except := config.Except
+	ignoreRootPaths := config.IgnoreRootPaths
+	sort.Strings(use)
+	sort.Strings(except)
+	sort.Strings(ignoreRootPaths)
 	return &configJSON{
-		Use:                           config.Use,
-		Except:                        config.Except,
-		IgnoreRootPaths:               config.IgnoreRootPaths,
+		Use:                           use,
+		Except:                        except,
+		IgnoreRootPaths:               ignoreRootPaths,
 		IgnoreIDOrCategoryToRootPaths: ignoreIDPathsJSON,
 		IgnoreUnstablePackages:        config.IgnoreUnstablePackages,
 		Version:                       config.Version,
