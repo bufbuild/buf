ac3d886bdb7b7ce5110c186be9a546d6da6d44f2
Philip K. Warren <pkwarren@users.noreply.github.com>
2022-06-28 17:07:39 -0500

    add buf alpha plugin push (#1245)
    Add back the plugin push command under 'alpha'. Pass .netrc
    authentication when publishing Docker images. Add additional test
    coverage to Docker client simulating issues seen while testing locally
    (in particular, errors returned via response.Body instead of via an
    error in ImagePush).

--- private/bufpkg/bufplugin/bufplugindocker/docker_test.go
+++ private/bufpkg/bufplugin/bufplugindocker/docker_test.go
@@ -16,24 +16,42 @@ package bufplugindocker_test
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"regexp"
 	"runtime"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/bufbuild/buf/private/bufpkg/bufplugin/bufpluginconfig"
 	"github.com/bufbuild/buf/private/bufpkg/bufplugin/bufplugindocker"
 	"github.com/bufbuild/buf/private/bufpkg/bufplugin/bufpluginref"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/stringid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
-var dockerEnabled = false
+const (
+	examplePluginIdentity = "buf.build/library/go"
+	dockerVersion         = "1.41"
+)
 
-const examplePluginIdentity = "plugins.buf.build/library/go"
+var (
+	dockerEnabled = false
+	imagePattern  = regexp.MustCompile("^(?P<image>[^/]+/[^/]+/[^/:]+)(?::(?P<tag>[^/]+))?(?:/(?P<op>[^/]+))?$")
+)
 
 func TestBuildSuccess(t *testing.T) {
 	t.Parallel()
@@ -44,7 +62,7 @@ func TestBuildSuccess(t *testing.T) {
 		t.Skip("docker tests disabled in short mode")
 	}
 	dockerClient := createClient(t)
-	response, err := buildDockerPlugin(t, dockerClient, "testdata/success/Dockerfile")
+	response, err := buildDockerPlugin(t, dockerClient, "testdata/success/Dockerfile", examplePluginIdentity)
 	require.Nilf(t, err, "failed to build docker plugin")
 	assert.Truef(
 		t,
@@ -65,8 +83,60 @@ func TestBuildFailure(t *testing.T) {
 		t.Skip("docker tests disabled in short mode")
 	}
 	dockerClient := createClient(t)
-	_, err := buildDockerPlugin(t, dockerClient, "testdata/failure/Dockerfile")
+	_, err := buildDockerPlugin(t, dockerClient, "testdata/failure/Dockerfile", examplePluginIdentity)
 	assert.NotNil(t, err)
+}
+
+func TestPushSuccess(t *testing.T) {
+	t.Parallel()
+	server := newDockerServer(t, dockerVersion)
+	listenerAddr := server.httpServer.Listener.Addr().String()
+	dockerClient := createClient(
+		t,
+		bufplugindocker.WithHost("tcp://"+listenerAddr),
+		bufplugindocker.WithVersion(dockerVersion),
+	)
+	response, err := buildDockerPlugin(t, dockerClient, "testdata/success/Dockerfile", listenerAddr+"/library/go")
+	require.Nilf(t, err, "failed to build docker plugin")
+	require.NotNil(t, response)
+	pushResponse, err := dockerClient.Push(context.Background(), response.Image, &bufplugindocker.RegistryAuthConfig{})
+	require.Nilf(t, err, "failed to push docker plugin")
+	require.NotNil(t, pushResponse)
+}
+
+func TestPushError(t *testing.T) {
+	t.Parallel()
+	server := newDockerServer(t, dockerVersion)
+	// Send back an error on ImagePush (still return 200 OK).
+	server.pushErr = errors.New("failed to push image")
+	listenerAddr := server.httpServer.Listener.Addr().String()
+	dockerClient := createClient(
+		t,
+		bufplugindocker.WithHost("tcp://"+listenerAddr),
+		bufplugindocker.WithVersion(dockerVersion),
+	)
+	response, err := buildDockerPlugin(t, dockerClient, "testdata/success/Dockerfile", listenerAddr+"/library/go")
+	require.Nilf(t, err, "failed to build docker plugin")
+	require.NotNil(t, response)
+	_, err = dockerClient.Push(context.Background(), response.Image, &bufplugindocker.RegistryAuthConfig{})
+	require.NotNil(t, err, "expected error")
+	assert.Equal(t, server.pushErr.Error(), err.Error())
+}
+
+func TestBuildError(t *testing.T) {
+	t.Parallel()
+	server := newDockerServer(t, dockerVersion)
+	// Send back an error on ImageBuild (still return 200 OK).
+	server.buildErr = errors.New("failed to build image")
+	listenerAddr := server.httpServer.Listener.Addr().String()
+	dockerClient := createClient(
+		t,
+		bufplugindocker.WithHost("tcp://"+listenerAddr),
+		bufplugindocker.WithVersion(dockerVersion),
+	)
+	_, err := buildDockerPlugin(t, dockerClient, "testdata/success/Dockerfile", listenerAddr+"/library/go")
+	require.NotNilf(t, err, "expected error during build")
+	assert.Equal(t, server.buildErr.Error(), err.Error())
 }
 
 func TestMain(m *testing.M) {
@@ -88,11 +158,11 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func createClient(t testing.TB) bufplugindocker.Client {
+func createClient(t testing.TB, options ...bufplugindocker.ClientOption) bufplugindocker.Client {
 	t.Helper()
 	logger, err := zap.NewDevelopment()
 	require.Nilf(t, err, "failed to create zap logger")
-	dockerClient, err := bufplugindocker.NewClient(logger)
+	dockerClient, err := bufplugindocker.NewClient(logger, options...)
 	require.Nilf(t, err, "failed to create client")
 	t.Cleanup(func() {
 		if err := dockerClient.Close(); err != nil {
@@ -102,11 +172,11 @@ func createClient(t testing.TB) bufplugindocker.Client {
 	return dockerClient
 }
 
-func buildDockerPlugin(t testing.TB, dockerClient bufplugindocker.Client, dockerfilePath string) (*bufplugindocker.BuildResponse, error) {
+func buildDockerPlugin(t testing.TB, dockerClient bufplugindocker.Client, dockerfilePath string, pluginIdentity string) (*bufplugindocker.BuildResponse, error) {
 	t.Helper()
 	dockerfile, err := os.Open(dockerfilePath)
 	require.Nilf(t, err, "failed to open dockerfile")
-	pluginName, err := bufpluginref.PluginIdentityForString(examplePluginIdentity)
+	pluginName, err := bufpluginref.PluginIdentityForString(pluginIdentity)
 	require.Nilf(t, err, "failed to create plugin identity")
 	pluginConfig := &bufpluginconfig.Config{Name: pluginName}
 	response, err := dockerClient.Build(context.Background(), dockerfile, pluginConfig)
@@ -118,4 +188,141 @@ func buildDockerPlugin(t testing.TB, dockerClient bufplugindocker.Client, docker
 		})
 	}
 	return response, err
+}
+
+// dockerServer allows testing some failure flows by simulating the responses to Docker CLI commands.
+type dockerServer struct {
+	httpServer    *httptest.Server
+	t             testing.TB
+	versionPrefix string
+	buildErr      error
+	pushErr       error
+	// protects builtImages
+	mu          sync.RWMutex
+	builtImages map[string]*builtImage
+}
+
+type builtImage struct {
+	tags []string
+}
+
+func newDockerServer(t testing.TB, version string) *dockerServer {
+	versionPrefix := "/v" + version
+	dockerServer := &dockerServer{
+		t:             t,
+		builtImages:   make(map[string]*builtImage),
+		versionPrefix: versionPrefix,
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/session", dockerServer.sessionHandler)
+	mux.HandleFunc(versionPrefix+"/build", dockerServer.buildHandler)
+	mux.HandleFunc(versionPrefix+"/images/", dockerServer.imagesHandler)
+	dockerServer.httpServer = httptest.NewServer(h2c.NewHandler(mux, &http2.Server{}))
+	t.Cleanup(dockerServer.httpServer.Close)
+	return dockerServer
+}
+
+func (d *dockerServer) sessionHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(io.Discard, r.Body); err != nil {
+		d.t.Error("failed to discard body:", err)
+	}
+	if r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
+	if strings.EqualFold(r.Header.Get("Connection"), "upgrade") && r.ProtoMajor < 2 {
+		// Needed to allow h2c upgrade to proceed on the /session endpoint.
+		w.WriteHeader(http.StatusSwitchingProtocols)
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func (d *dockerServer) buildHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(io.Discard, r.Body); err != nil {
+		d.t.Error("failed to discard body:", err)
+	}
+	w.WriteHeader(http.StatusOK)
+	if d.buildErr != nil {
+		d.writeError(w, d.buildErr)
+		return
+	}
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	imageID := stringid.GenerateRandomID()
+	d.builtImages[imageID] = &builtImage{tags: r.URL.Query()["t"]}
+}
+
+func (d *dockerServer) imagesHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(io.Discard, r.Body); err != nil {
+		d.t.Error("failed to discard body:", err)
+	}
+	pathSuffix := strings.TrimPrefix(r.URL.Path, d.versionPrefix+"/images/")
+	submatches := imagePattern.FindStringSubmatch(pathSuffix)
+	if len(submatches) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	image, tag, operation := submatches[1], submatches[2], submatches[3]
+	// ImageInspectWithRaw
+	if r.Method == http.MethodGet && operation == "json" {
+		d.mu.RLock()
+		defer d.mu.RUnlock()
+		foundImageID := d.findImageIDFromName(image + ":" + tag)
+		if len(foundImageID) == 0 {
+			http.NotFound(w, r)
+			return
+		}
+		if err := json.NewEncoder(w).Encode(&types.ImageInspect{
+			ID:       "sha256:" + foundImageID,
+			RepoTags: d.builtImages[foundImageID].tags,
+		}); err != nil {
+			d.t.Error("failed to encode image inspect response:", err)
+		}
+
+		return
+	}
+	// ImageRemove
+	if r.Method == http.MethodDelete {
+		d.mu.Lock()
+		defer d.mu.Unlock()
+		foundImageID := d.findImageIDFromName(image + ":" + tag)
+		if len(foundImageID) == 0 {
+			http.NotFound(w, r)
+			return
+		}
+		delete(d.builtImages, foundImageID)
+		if err := json.NewEncoder(w).Encode([]types.ImageDeleteResponseItem{
+			{Deleted: "sha256:" + foundImageID},
+		}); err != nil {
+			d.t.Error("failed to encode image delete response:", err)
+		}
+		return
+	}
+	// ImagePush
+	w.WriteHeader(http.StatusOK)
+	if d.pushErr != nil {
+		d.writeError(w, d.pushErr)
+		return
+	}
+}
+
+func (d *dockerServer) findImageIDFromName(name string) string {
+	for imageID, builtImageInfo := range d.builtImages {
+		for _, imageTag := range builtImageInfo.tags {
+			if imageTag == name {
+				return imageID
+			}
+		}
+	}
+	return ""
+}
+
+func (d *dockerServer) writeError(w http.ResponseWriter, err error) {
+	if err := json.NewEncoder(w).Encode(&jsonmessage.JSONMessage{Error: &jsonmessage.JSONError{Message: err.Error()}}); err != nil {
+		d.t.Error("failed to write json message:", err)
+	}
+	if _, err := w.Write([]byte{'\r', '\n'}); err != nil {
+		d.t.Error("failed to write CRLF", err)
+	}
 }
