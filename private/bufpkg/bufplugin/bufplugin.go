0c22a4afb4fa6287df7917783a14e317e9eb8803
Philip K. Warren <pkwarren@users.noreply.github.com>
2022-06-30 16:37:58 -0500

    update plugin push to create curated plugin (#1254)
    Update the `buf alpha plugin push` command to call CreateCuratedPlugin
    to add the plugin to the BSR.

--- private/bufpkg/bufplugin/bufplugin.go
+++ private/bufpkg/bufplugin/bufplugin.go
@@ -15,7 +15,10 @@
 package bufplugin
 
 import (
+	"strings"
+
 	"github.com/bufbuild/buf/private/bufpkg/bufplugin/bufpluginconfig"
+	registryv1alpha1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/registry/v1alpha1"
 )
 
 // Plugin represents a plugin defined by a buf.plugin.yaml.
@@ -59,4 +62,133 @@ func NewPlugin(
 	imageDigest string,
 ) (Plugin, error) {
 	return newPlugin(version, options, runtimeConfig, imageDigest)
+}
+
+// PluginToProtoPluginLanguage determines the appropriate registryv1alpha1.PluginLanguage for the plugin.
+func PluginToProtoPluginLanguage(plugin Plugin) registryv1alpha1.PluginLanguage {
+	language := registryv1alpha1.PluginLanguage_PLUGIN_LANGUAGE_UNSPECIFIED
+	if plugin.Runtime() != nil {
+		if plugin.Runtime().Go != nil {
+			language = registryv1alpha1.PluginLanguage_PLUGIN_LANGUAGE_GO
+		} else if plugin.Runtime().NPM != nil {
+			language = registryv1alpha1.PluginLanguage_PLUGIN_LANGUAGE_NPM
+		}
+	}
+	return language
+}
+
+// PluginRuntimeToProtoRuntimeConfig converts a bufpluginconfig.RuntimeConfig to a registryv1alpha1.RuntimeConfig.
+func PluginRuntimeToProtoRuntimeConfig(pluginRuntime *bufpluginconfig.RuntimeConfig) *registryv1alpha1.RuntimeConfig {
+	if pluginRuntime == nil {
+		return nil
+	}
+	runtimeConfig := &registryv1alpha1.RuntimeConfig{}
+	if pluginRuntime.Go != nil {
+		goConfig := &registryv1alpha1.GoConfig{}
+		goConfig.MinimumVersion = pluginRuntime.Go.MinVersion
+		goConfig.RuntimeLibraries = make([]*registryv1alpha1.GoConfig_RuntimeLibrary, 0, len(pluginRuntime.Go.Deps))
+		for _, dependency := range pluginRuntime.Go.Deps {
+			goConfig.RuntimeLibraries = append(goConfig.RuntimeLibraries, goRuntimeDependencyToProtoGoRuntimeLibrary(dependency))
+		}
+		runtimeConfig.RuntimeConfig = &registryv1alpha1.RuntimeConfig_GoConfig{GoConfig: goConfig}
+	} else if pluginRuntime.NPM != nil {
+		npmConfig := &registryv1alpha1.NPMConfig{}
+		npmConfig.RuntimeLibraries = make([]*registryv1alpha1.NPMConfig_RuntimeLibrary, 0, len(pluginRuntime.NPM.Deps))
+		for _, dependency := range pluginRuntime.NPM.Deps {
+			npmConfig.RuntimeLibraries = append(npmConfig.RuntimeLibraries, npmRuntimeDependencyToProtoNPMRuntimeLibrary(dependency))
+		}
+		runtimeConfig.RuntimeConfig = &registryv1alpha1.RuntimeConfig_NpmConfig{NpmConfig: npmConfig}
+	}
+	return runtimeConfig
+}
+
+// ProtoRuntimeConfigToPluginRuntime converts a registryv1alpha1.RuntimeConfig to a bufpluginconfig.RuntimeConfig .
+func ProtoRuntimeConfigToPluginRuntime(config *registryv1alpha1.RuntimeConfig) *bufpluginconfig.RuntimeConfig {
+	if config == nil {
+		return nil
+	}
+	runtimeConfig := &bufpluginconfig.RuntimeConfig{}
+	if config.GetGoConfig() != nil {
+		goConfig := &bufpluginconfig.GoRuntimeConfig{}
+		goConfig.MinVersion = config.GetGoConfig().GetMinimumVersion()
+		goConfig.Deps = make([]*bufpluginconfig.GoRuntimeDependencyConfig, 0, len(config.GetGoConfig().GetRuntimeLibraries()))
+		for _, library := range config.GetGoConfig().GetRuntimeLibraries() {
+			goConfig.Deps = append(goConfig.Deps, protoGoRuntimeLibraryToGoRuntimeDependency(library))
+		}
+		runtimeConfig.Go = goConfig
+	} else if config.GetNpmConfig() != nil {
+		npmConfig := &bufpluginconfig.NPMRuntimeConfig{}
+		npmConfig.Deps = make([]*bufpluginconfig.NPMRuntimeDependencyConfig, 0, len(config.GetNpmConfig().GetRuntimeLibraries()))
+		for _, library := range config.GetNpmConfig().GetRuntimeLibraries() {
+			npmConfig.Deps = append(npmConfig.Deps, protoNPMRuntimeLibraryToNPMRuntimeDependency(library))
+		}
+		runtimeConfig.NPM = npmConfig
+	}
+	return runtimeConfig
+}
+
+// goRuntimeDependencyToProtoGoRuntimeLibrary converts a bufpluginconfig.GoRuntimeDependencyConfig to a registryv1alpha1.GoConfig_RuntimeLibrary.
+func goRuntimeDependencyToProtoGoRuntimeLibrary(config *bufpluginconfig.GoRuntimeDependencyConfig) *registryv1alpha1.GoConfig_RuntimeLibrary {
+	return &registryv1alpha1.GoConfig_RuntimeLibrary{
+		Module:  config.Module,
+		Version: config.Version,
+	}
+}
+
+// protoGoRuntimeLibraryToGoRuntimeDependency converts a registryv1alpha1.GoConfig_RuntimeLibrary to a bufpluginconfig.GoRuntimeDependencyConfig.
+func protoGoRuntimeLibraryToGoRuntimeDependency(config *registryv1alpha1.GoConfig_RuntimeLibrary) *bufpluginconfig.GoRuntimeDependencyConfig {
+	return &bufpluginconfig.GoRuntimeDependencyConfig{
+		Module:  config.Module,
+		Version: config.Version,
+	}
+}
+
+// npmRuntimeDependencyToProtoNPMRuntimeLibrary converts a bufpluginconfig.NPMRuntimeConfig to a registryv1alpha1.NPMConfig_RuntimeLibrary.
+func npmRuntimeDependencyToProtoNPMRuntimeLibrary(config *bufpluginconfig.NPMRuntimeDependencyConfig) *registryv1alpha1.NPMConfig_RuntimeLibrary {
+	return &registryv1alpha1.NPMConfig_RuntimeLibrary{
+		Package: config.Package,
+		Version: config.Version,
+	}
+}
+
+// protoNPMRuntimeLibraryToNPMRuntimeDependency converts a registryv1alpha1.NPMConfig_RuntimeLibrary to a bufpluginconfig.NPMRuntimeDependencyConfig.
+func protoNPMRuntimeLibraryToNPMRuntimeDependency(config *registryv1alpha1.NPMConfig_RuntimeLibrary) *bufpluginconfig.NPMRuntimeDependencyConfig {
+	return &bufpluginconfig.NPMRuntimeDependencyConfig{
+		Package: config.Package,
+		Version: config.Version,
+	}
+}
+
+// PluginOptionsToOptionsSlice converts a map representation of plugin options to a slice of the form '<key>=<value>' or '<key>' for empty values.
+func PluginOptionsToOptionsSlice(pluginOptions map[string]string) []string {
+	if pluginOptions == nil {
+		return nil
+	}
+	options := make([]string, 0, len(pluginOptions))
+	for key, value := range pluginOptions {
+		if len(value) > 0 {
+			options = append(options, key+"="+value)
+		} else {
+			options = append(options, key)
+		}
+	}
+	return options
+}
+
+// OptionsSliceToPluginOptions converts a slice of plugin options to a map (using the first '=' as a delimiter between key and value).
+// If no '=' is found, the option will be stored in the map with an empty string value.
+func OptionsSliceToPluginOptions(options []string) map[string]string {
+	if options == nil {
+		return nil
+	}
+	pluginOptions := make(map[string]string, len(options))
+	for _, option := range options {
+		fields := strings.SplitN(option, "=", 2)
+		if len(fields) == 2 {
+			pluginOptions[fields[0]] = fields[1]
+		} else {
+			pluginOptions[option] = ""
+		}
+	}
+	return pluginOptions
 }
