1478af319897553ce64830acc5e0e55f01cb8d7f
Philip K. Warren <pkwarren@users.noreply.github.com>
2022-07-12 17:00:39 -0500

    BSR-333: define plugin generation RPC (#1274)
    Add a new RPC to perform remote code generation using curated plugins.

--- private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect/plugin_curation.connect.go
+++ private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect/plugin_curation.connect.go
@@ -37,6 +37,8 @@ const _ = connect_go.IsAtLeastVersion0_1_0
 const (
 	// PluginCurationServiceName is the fully-qualified name of the PluginCurationService service.
 	PluginCurationServiceName = "buf.alpha.registry.v1alpha1.PluginCurationService"
+	// CodeGenerationServiceName is the fully-qualified name of the CodeGenerationService service.
+	CodeGenerationServiceName = "buf.alpha.registry.v1alpha1.CodeGenerationService"
 )
 
 // PluginCurationServiceClient is a client for the buf.alpha.registry.v1alpha1.PluginCurationService
@@ -151,4 +153,69 @@ func (UnimplementedPluginCurationServiceHandler) CreateCuratedPlugin(context.Con
 
 func (UnimplementedPluginCurationServiceHandler) GetLatestCuratedPlugin(context.Context, *connect_go.Request[v1alpha1.GetLatestCuratedPluginRequest]) (*connect_go.Response[v1alpha1.GetLatestCuratedPluginResponse], error) {
 	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("buf.alpha.registry.v1alpha1.PluginCurationService.GetLatestCuratedPlugin is not implemented"))
+}
+
+// CodeGenerationServiceClient is a client for the buf.alpha.registry.v1alpha1.CodeGenerationService
+// service.
+type CodeGenerationServiceClient interface {
+	// GenerateCode generates code using the specified remote plugins.
+	GenerateCode(context.Context, *connect_go.Request[v1alpha1.GenerateCodeRequest]) (*connect_go.Response[v1alpha1.GenerateCodeResponse], error)
+}
+
+// NewCodeGenerationServiceClient constructs a client for the
+// buf.alpha.registry.v1alpha1.CodeGenerationService service. By default, it uses the Connect
+// protocol with the binary Protobuf Codec, asks for gzipped responses, and sends uncompressed
+// requests. To use the gRPC or gRPC-Web protocols, supply the connect.WithGRPC() or
+// connect.WithGRPCWeb() options.
+//
+// The URL supplied here should be the base URL for the Connect or gRPC server (for example,
+// http://api.acme.com or https://acme.com/grpc).
+func NewCodeGenerationServiceClient(httpClient connect_go.HTTPClient, baseURL string, opts ...connect_go.ClientOption) CodeGenerationServiceClient {
+	baseURL = strings.TrimRight(baseURL, "/")
+	return &codeGenerationServiceClient{
+		generateCode: connect_go.NewClient[v1alpha1.GenerateCodeRequest, v1alpha1.GenerateCodeResponse](
+			httpClient,
+			baseURL+"/buf.alpha.registry.v1alpha1.CodeGenerationService/GenerateCode",
+			opts...,
+		),
+	}
+}
+
+// codeGenerationServiceClient implements CodeGenerationServiceClient.
+type codeGenerationServiceClient struct {
+	generateCode *connect_go.Client[v1alpha1.GenerateCodeRequest, v1alpha1.GenerateCodeResponse]
+}
+
+// GenerateCode calls buf.alpha.registry.v1alpha1.CodeGenerationService.GenerateCode.
+func (c *codeGenerationServiceClient) GenerateCode(ctx context.Context, req *connect_go.Request[v1alpha1.GenerateCodeRequest]) (*connect_go.Response[v1alpha1.GenerateCodeResponse], error) {
+	return c.generateCode.CallUnary(ctx, req)
+}
+
+// CodeGenerationServiceHandler is an implementation of the
+// buf.alpha.registry.v1alpha1.CodeGenerationService service.
+type CodeGenerationServiceHandler interface {
+	// GenerateCode generates code using the specified remote plugins.
+	GenerateCode(context.Context, *connect_go.Request[v1alpha1.GenerateCodeRequest]) (*connect_go.Response[v1alpha1.GenerateCodeResponse], error)
+}
+
+// NewCodeGenerationServiceHandler builds an HTTP handler from the service implementation. It
+// returns the path on which to mount the handler and the handler itself.
+//
+// By default, handlers support the Connect, gRPC, and gRPC-Web protocols with the binary Protobuf
+// and JSON codecs. They also support gzip compression.
+func NewCodeGenerationServiceHandler(svc CodeGenerationServiceHandler, opts ...connect_go.HandlerOption) (string, http.Handler) {
+	mux := http.NewServeMux()
+	mux.Handle("/buf.alpha.registry.v1alpha1.CodeGenerationService/GenerateCode", connect_go.NewUnaryHandler(
+		"/buf.alpha.registry.v1alpha1.CodeGenerationService/GenerateCode",
+		svc.GenerateCode,
+		opts...,
+	))
+	return "/buf.alpha.registry.v1alpha1.CodeGenerationService/", mux
+}
+
+// UnimplementedCodeGenerationServiceHandler returns CodeUnimplemented from all methods.
+type UnimplementedCodeGenerationServiceHandler struct{}
+
+func (UnimplementedCodeGenerationServiceHandler) GenerateCode(context.Context, *connect_go.Request[v1alpha1.GenerateCodeRequest]) (*connect_go.Response[v1alpha1.GenerateCodeResponse], error) {
+	return nil, connect_go.NewError(connect_go.CodeUnimplemented, errors.New("buf.alpha.registry.v1alpha1.CodeGenerationService.GenerateCode is not implemented"))
 }
