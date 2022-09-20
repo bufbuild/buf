a8c5fb326998b8e33d6ef2bc6b492787975d7356
oliversun09 <112964599+oliversun09@users.noreply.github.com>
2022-09-20 11:03:45 -0400

    Add SearchCommitReference Method (#1418)

--- private/gen/proto/api/buf/alpha/registry/v1alpha1/registryv1alpha1api/search.pb.go
+++ private/gen/proto/api/buf/alpha/registry/v1alpha1/registryv1alpha1api/search.pb.go
@@ -31,4 +31,13 @@ type SearchService interface {
 		pageToken uint32,
 		filters []v1alpha1.SearchFilter,
 	) (searchResults []*v1alpha1.SearchResult, nextPageToken uint32, err error)
+	// SearchCommitReference searches in a repository
+	SearchCommitReference(
+		ctx context.Context,
+		repositoryOwner string,
+		repositoryName string,
+		query string,
+		pageSize uint32,
+		pageToken uint32,
+	) (repositoryCommits []*v1alpha1.RepositoryCommit, nextPageToken uint32, err error)
 }
