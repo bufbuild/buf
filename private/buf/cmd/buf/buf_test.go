9bfce0145c0db3051ed682525bf532bb1ccfb50a
joshcarp <jcarpeggiani@buf.build>
2022-10-19 10:44:50 -0400

    Merge convert_test into buf_test

--- private/buf/cmd/buf/buf_test.go
+++ private/buf/cmd/buf/buf_test.go
@@ -21,6 +21,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/bufbuild/buf/private/buf/bufcli"
@@ -1880,6 +1881,153 @@ func TestConvertInvalidTypeName(t *testing.T) {
 		"--type",
 		".foo",
 	)
+}
+
+func TestConvert(t *testing.T) {
+	t.Run("bin-to-json-file-proto", func(t *testing.T) {
+		testRunStdoutFile(t,
+			nil,
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"--from=testdata/convert/bin_json/payload.bin",
+			"testdata/convert/bin_json/buf.proto",
+		)
+	})
+	t.Run("json-to-bin-file-proto", func(t *testing.T) {
+		testRunStdoutFile(t,
+			nil,
+			0,
+			"testdata/convert/bin_json/payload.bin",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"--from=testdata/convert/bin_json/payload.json",
+			"testdata/convert/bin_json/buf.proto",
+		)
+	})
+	t.Run("stdin-json-to-bin-proto", func(t *testing.T) {
+		testRunStdoutFile(t,
+			strings.NewReader(`{"one":"55"}`),
+			0,
+			"testdata/convert/bin_json/payload.bin",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"--from",
+			"-#format=json",
+			"testdata/convert/bin_json/buf.proto",
+		)
+	})
+	t.Run("stdin-bin-to-json-proto", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/payload.bin")
+		require.NoError(t, err)
+		testRunStdoutFile(t, file,
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"--from",
+			"-#format=bin",
+			"testdata/convert/bin_json/buf.proto",
+		)
+	})
+	t.Run("stdin-json-to-json-proto", func(t *testing.T) {
+		testRunStdoutFile(t,
+			strings.NewReader(`{"one":"55"}`),
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"testdata/convert/bin_json/buf.proto",
+			"--from",
+			"-#format=json",
+			"--to",
+			"-#format=json")
+	})
+	t.Run("stdin-input-to-json-image", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/image.bin")
+		require.NoError(t, err)
+		testRunStdoutFile(t, file,
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"-",
+			"--from=testdata/convert/bin_json/payload.bin",
+			"--to",
+			"-#format=json",
+		)
+	})
+	t.Run("stdin-json-to-json-image", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/payload.bin")
+		require.NoError(t, err)
+		testRunStdoutFile(t,
+			file,
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"testdata/convert/bin_json/image.bin",
+			"--from",
+			"-#format=bin",
+			"--to",
+			"-#format=json")
+	})
+	t.Run("stdin-bin-payload-to-json-with-image", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/payload.bin")
+		require.NoError(t, err)
+		testRunStdoutFile(t,
+			file,
+			0,
+			"testdata/convert/bin_json/payload.json",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"testdata/convert/bin_json/image.bin",
+			"--to",
+			"-#format=json",
+		)
+	})
+	t.Run("stdin-json-payload-to-bin-with-image", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/payload.json")
+		require.NoError(t, err)
+		testRunStdoutFile(t,
+			file,
+			0,
+			"testdata/convert/bin_json/payload.bin",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"testdata/convert/bin_json/image.bin",
+			"--from",
+			"-#format=json",
+			"--to",
+			"-#format=bin",
+		)
+	})
+	t.Run("stdin-image-json-to-bin", func(t *testing.T) {
+		file, err := os.Open("testdata/convert/bin_json/image.json")
+		require.NoError(t, err)
+		testRunStdoutFile(t,
+			file,
+			0,
+			"testdata/convert/bin_json/payload.bin",
+			"beta",
+			"convert",
+			"--type=buf.Foo",
+			"-#format=json",
+			"--from=testdata/convert/bin_json/payload.json",
+			"--to",
+			"-#format=bin",
+		)
+	})
 }
 
 func TestFormat(t *testing.T) {
