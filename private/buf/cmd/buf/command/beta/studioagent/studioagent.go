a187c818aeb09e45ad3f98d3c26ba70137d1fd26
Stefan VanBuren <svanburen@buf.build>
2022-10-26 17:06:38 -0400

    Remove `httpserver.{Mapper,Runner}` (#1541)
    This removes `httpserver.{Mapper,Runner}` and associated code, in favor
    of using `httpserver.Run`.

--- private/buf/cmd/buf/command/beta/studioagent/studioagent.go
+++ private/buf/cmd/buf/command/beta/studioagent/studioagent.go
@@ -187,19 +187,14 @@ func run(
 		return err
 	}
 
-	httpRunner := httpserver.NewRunner(
+	return httpserver.Run(
+		ctx,
 		container.Logger(),
-		httpserver.RunnerWithTLSConfig(
+		httpListener,
+		mux,
+		httpserver.RunWithTLSConfig(
 			serverTLSConfig,
 		),
-		httpserver.RunnerWithMaxBodySize(
-			bufstudioagent.MaxMessageSizeBytesDefault,
-		),
-	)
-	return httpRunner.Run(
-		ctx,
-		httpListener,
-		httpserver.NewHTTPHandlerMapper(mux),
 	)
 }
 
