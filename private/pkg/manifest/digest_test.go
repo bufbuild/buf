a55e38a97a7483d67096627a5c839fb1d955d64a
Julian R Figueroa <jfigueroa@buf.build>
2022-10-28 09:51:05 -0500

    [BSR-721] New hash blobs constructor and validator (#1544)
    Changes in this PR:
    - Separated `Digest` (and new `Digester`) code into its own `digest.go`
    file
    - Separated `NewDigestFromBlob` into `module.go` in case we wanna break
    that dependency later.
    - Separated test files accordingly (manifest/digest/module).
    - Exported `DigestType` type, and supported types (just shake256 for
    now). It didn't make sense to have it private if it was required to be
    used from the consumer in raw string form anyway.
    - Created a new `Digester` interface that allows digesting content w/out
    the need to do it via a `Manifest`.
    - Removed any "file content" concern from the `Manifest`, now it only
    cares about "entries" or "pairs" of paths+ and its digests. Meaning it
    doesn't have a `hash` and it doesn't receive `AddContent` anymore, just
    `AddEntry(path, digest)`. The digests can be built using the
    aforementioned `Digester`, in an upper layer that cares about content.
    - Added checks to make sure manifest paths have a unique digest.
    - Since `Manifest` doesn't care about content anymore, it also doesn't
    check digest types. That was moved down to the `Digest` constructors.
    
    This results in a nicer, slimmer exported funcs for `Manifest`, which
    only cares about paths and manifests:
    
    ![image](https://user-images.githubusercontent.com/4431392/198500219-6d56ab68-c613-424d-a5b4-65ec4a807190.png)
    
    And for `Digest`, we have 3 constructors, all of those using the same
    `NewDigestFromBytes` underneath, which errors (for now) if the dtype is
    not supported. This allows for future extension just adding a new dtype,
    without breaking exported interface.
    
    Two concerns still in my mind:
    - It's possible to declare a `Digest{}` w/out going through a
    constructor, so not sure if we want to make `Digest` into an interface.
    - The `manifest.AddEntry` func checks that the passed digest is
    populated/valid at least, but that check feels out of place. I thought
    of doing a `func (*Digest) Valid() bool` but it felt even weirder, why
    would we allow a Digest to be invalid?
    - If a `Digest` is allowed to be constructed outside of the pkg, then
    somehow `digest []bytes` and `hexstr string` could diverge? feels both
    like two sources of the same state.
    
    Co-authored-by: Tristan Willy <twilly@buf.build>
    Co-authored-by: Tristan Willy <twilly@users.noreply.github.com>

---
+++ private/pkg/manifest/digest_test.go
@@ -0,0 +1,213 @@
+// Copyright 2020-2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package manifest_test
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+	"testing/iotest"
+
+	"github.com/bufbuild/buf/private/pkg/manifest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewDigestBytes(t *testing.T) {
+	t.Parallel()
+	testInvalidDigestBytes(
+		t,
+		"empty",
+		"",
+		mustDigestShake256(t, nil).Bytes(),
+	)
+	testInvalidDigestBytes(
+		t,
+		"unsupported digest type",
+		"md5",
+		mustDigestShake256(t, nil).Bytes(),
+	)
+	testInvalidDigestBytes(
+		t,
+		"invalid digest",
+		manifest.DigestTypeShake256,
+		nil,
+	)
+	testInvalidDigestBytes(
+		t,
+		"invalid digest",
+		manifest.DigestTypeShake256,
+		mustDigestShake256(t, nil).Bytes()[:10],
+	)
+}
+
+func TestNewDigestHex(t *testing.T) {
+	t.Parallel()
+	testInvalidDigestHex(
+		t,
+		"empty",
+		"",
+		mustDigestShake256(t, nil).Hex(),
+	)
+	testInvalidDigestHex(
+		t,
+		"unsupported digest type",
+		"md5",
+		mustDigestShake256(t, nil).Hex(),
+	)
+	testInvalidDigestHex(
+		t,
+		"invalid digest",
+		manifest.DigestTypeShake256,
+		"",
+	)
+	testInvalidDigestHex(
+		t,
+		"encoding/hex",
+		manifest.DigestTypeShake256,
+		"not-a_hex/string",
+	)
+	testInvalidDigestHex(
+		t,
+		"invalid digest",
+		manifest.DigestTypeShake256,
+		mustDigestShake256(t, nil).Hex()[:10],
+	)
+}
+
+func TestNewDigestString(t *testing.T) {
+	t.Parallel()
+	testInvalidDigestString(
+		t,
+		"malformed",
+		"",
+	)
+	testInvalidDigestString(
+		t,
+		"malformed",
+		"foo",
+	)
+	testInvalidDigestString(
+		t,
+		"malformed",
+		"shake256 foo",
+	)
+	testInvalidDigestString(
+		t,
+		"encoding/hex",
+		"shake256:_",
+	)
+	validDigest := mustDigestShake256(t, nil)
+	testInvalidDigestString(
+		t,
+		"invalid digest",
+		fmt.Sprintf("%s:%s", validDigest.Type(), validDigest.Hex()[:10]),
+	)
+	testInvalidDigestString(
+		t,
+		"unsupported digest type",
+		fmt.Sprintf("md5:%s", validDigest.Hex()),
+	)
+}
+
+func TestNewDigester(t *testing.T) {
+	t.Parallel()
+	digester, err := manifest.NewDigester(manifest.DigestTypeShake256)
+	require.NoError(t, err)
+	require.NotNil(t, digester)
+	digester, err = manifest.NewDigester("some unrecognized digest type")
+	require.Error(t, err)
+	require.Nil(t, digester)
+}
+
+func TestDigesterDigest(t *testing.T) {
+	t.Parallel()
+	digester, err := manifest.NewDigester(manifest.DigestTypeShake256)
+	require.NoError(t, err)
+	digest, err := digester.Digest(strings.NewReader("some content"))
+	require.NoError(t, err)
+	assert.Equal(t, manifest.DigestTypeShake256, digest.Type())
+	assert.NotEmpty(t, digest.Bytes())
+	assert.NotEmpty(t, digest.String())
+
+	// failing digesting content
+	expectedErr := errors.New("testing error")
+	digest, err = digester.Digest(iotest.ErrReader(expectedErr))
+	assert.ErrorIs(t, err, expectedErr)
+	assert.Nil(t, digest)
+}
+
+func TestEqualDigests(t *testing.T) {
+	t.Parallel()
+	const fileContent = "one line\nanother line\nyet another one\n"
+	d1 := mustDigestShake256(t, []byte(fileContent))
+	d2 := mustDigestShake256(t, []byte(fileContent))
+	d3 := mustDigestShake256(t, []byte("some other content"))
+	assert.True(t, d1.Equal(*d2))
+	assert.True(t, d2.Equal(*d1))
+	assert.False(t, d1.Equal(*d3))
+}
+
+func mustDigestShake256(t *testing.T, content []byte) *manifest.Digest {
+	digester, err := manifest.NewDigester(manifest.DigestTypeShake256)
+	require.NoError(t, err)
+	require.NotNil(t, digester)
+	digest, err := digester.Digest(bytes.NewReader(content))
+	require.NoError(t, err)
+	return digest
+}
+
+func testInvalidDigestString(
+	t *testing.T,
+	desc string,
+	digest string,
+) {
+	t.Helper()
+	t.Run(desc, func(t *testing.T) {
+		t.Parallel()
+		_, err := manifest.NewDigestFromString(digest)
+		assert.ErrorContains(t, err, desc)
+	})
+}
+
+func testInvalidDigestHex(
+	t *testing.T,
+	desc string,
+	dtype manifest.DigestType,
+	hexstr string,
+) {
+	t.Helper()
+	t.Run(desc, func(t *testing.T) {
+		t.Parallel()
+		_, err := manifest.NewDigestFromHex(dtype, hexstr)
+		assert.ErrorContains(t, err, desc)
+	})
+}
+
+func testInvalidDigestBytes(
+	t *testing.T,
+	desc string,
+	dtype manifest.DigestType,
+	digest []byte,
+) {
+	t.Helper()
+	t.Run(desc, func(t *testing.T) {
+		t.Parallel()
+		_, err := manifest.NewDigestFromBytes(dtype, digest)
+		assert.ErrorContains(t, err, desc)
+	})
+}
