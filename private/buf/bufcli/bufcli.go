ed6199376e6b2dd43e068cc7561ac1d7d09ab14a
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2022-10-28 16:35:36 -0400

    Update version to v0.0.32 (#40)
    Co-authored-by: ElliotMJackson <ElliotMJackson@users.noreply.github.com>

--- private/buf/bufcli/bufcli.go
+++ private/buf/bufcli/bufcli.go
@@ -61,7 +61,7 @@ import (
 
 const (
 	// Version is the CLI version of buf.
-	Version = "0.0.31"
+	Version = "0.0.32"
 
 	inputHTTPSUsernameEnvKey      = "BUF_INPUT_HTTPS_USERNAME"
 	inputHTTPSPasswordEnvKey      = "BUF_INPUT_HTTPS_PASSWORD"
