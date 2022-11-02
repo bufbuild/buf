8430729a2636b92ac8dd0a343409a7b2dfb8b7cc
Philip K. Warren <pkwarren@users.noreply.github.com>
2022-11-02 14:09:31 -0500

    update go to 1.19.3 and update go deps (#1556)

--- make/buf/all.mk
+++ make/buf/all.mk
@@ -24,6 +24,7 @@ DOCKER_BINS := $(DOCKER_BINS) buf
 FILE_IGNORES := $(FILE_IGNORES) \
 	.build/ \
 	.ctrlp \
+	.idea/ \
 	.vscode/ \
 	private/buf/cmd/buf/command/alpha/protoc/test.txt \
 	private/buf/cmd/buf/workspacetests/other/proto/workspacetest/cache/ \
@@ -123,7 +124,7 @@ bufgeneratesteps:: \
 
 .PHONY: bufrelease
 bufrelease: $(MINISIGN)
-	DOCKER_IMAGE=golang:1.19.2-bullseye bash make/buf/scripts/release.bash
+	DOCKER_IMAGE=golang:1.19.3-bullseye bash make/buf/scripts/release.bash
 
 # We have to manually set the Homebrew version on the Homebrew badge as there
 # is no badge on shields.io for Homebrew packages outside of homebrew-core
