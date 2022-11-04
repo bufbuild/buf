c6f26990c5ddd078412e346ebf1e45e59025130b
Julian Figueroa <jfigueroa@buf.build>
2022-11-03 19:09:00 -0500

    Remove redundant comment

--- private/pkg/manifest/storage.go
+++ private/pkg/manifest/storage.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,14 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Manifests are a list of paths and their hash digests, canonically ordered by
-// path in increasing lexographical order. Manifests are encoded as:
-//
-//	<hash type>:<digest>[SP][SP]<path>[LF]
-//
-// "shake256" is the only supported hash type. The digest is 64 bytes of hex
-// encoded output of SHAKE256. See golang.org/x/crypto/sha3 and FIPS 202 for
-// details on the SHAKE hash.
 package manifest
 
 import (
