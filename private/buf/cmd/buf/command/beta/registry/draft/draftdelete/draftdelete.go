a1ab4ee48dd305bd1f731fb170cc49e19dbc6d89
Josh Humphries <2035234+jhump@users.noreply.github.com>
2022-11-14 11:17:45 -0500

    use raw connect clients for RepositoryCommitService

--- private/buf/cmd/buf/command/beta/registry/draft/draftdelete/draftdelete.go
+++ private/buf/cmd/buf/command/beta/registry/draft/draftdelete/draftdelete.go
@@ -21,8 +21,12 @@ import (
 
 	"github.com/bufbuild/buf/private/buf/bufcli"
 	"github.com/bufbuild/buf/private/bufpkg/bufmodule/bufmoduleref"
+	"github.com/bufbuild/buf/private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect"
+	registryv1alpha1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/registry/v1alpha1"
 	"github.com/bufbuild/buf/private/pkg/app/appcmd"
 	"github.com/bufbuild/buf/private/pkg/app/appflag"
+	"github.com/bufbuild/buf/private/pkg/connectclient"
+	"github.com/bufbuild/connect-go"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -84,14 +88,15 @@ func run(
 		}
 		return appcmd.NewInvalidArgumentError("a valid draft name need to be specified")
 	}
-	apiProvider, err := bufcli.NewRegistryProvider(ctx, container)
+	clientConfig, err := bufcli.NewConnectClientConfig(container)
 	if err != nil {
 		return err
 	}
-	service, err := apiProvider.NewRepositoryCommitService(ctx, moduleReference.Remote())
-	if err != nil {
-		return err
-	}
+	service := connectclient.Make(
+		clientConfig,
+		moduleReference.Remote(),
+		registryv1alpha1connect.NewRepositoryCommitServiceClient,
+	)
 	if !flags.Force {
 		if err := bufcli.PromptUserForDelete(
 			container,
@@ -101,11 +106,13 @@ func run(
 			return err
 		}
 	}
-	if err := service.DeleteRepositoryDraftCommit(
+	if _, err := service.DeleteRepositoryDraftCommit(
 		ctx,
-		moduleReference.Owner(),
-		moduleReference.Repository(),
-		moduleReference.Reference(),
+		connect.NewRequest(&registryv1alpha1.DeleteRepositoryDraftCommitRequest{
+			RepositoryOwner: moduleReference.Owner(),
+			RepositoryName:  moduleReference.Repository(),
+			DraftName:       moduleReference.Reference(),
+		}),
 	); err != nil {
 		// not explicitly handling error with connect.CodeNotFound as it can be repository not found or draft not found.
 		return err
