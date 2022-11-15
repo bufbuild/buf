7bc52c6406d053e6383ae92b6ca9cd1a85986926
Joshua Carpeggiani <32605850+joshcarp@users.noreply.github.com>
2022-11-15 11:15:03 -0500

    Update export.go

--- private/buf/cmd/buf/command/export/export.go
+++ private/buf/cmd/buf/command/export/export.go
@@ -69,8 +69,11 @@ output will be a directory with all of the .proto files in the <input>.
 # Export current directory to another local directory. 
 $ buf export . --output=<output-dir>
 
-# Export a remote module to a local directory.
+# Export the latest remote module to a local directory.
 $ buf export buf.build/<owner>/<repo> --output=<output-dir>
+
+# Export a specific version of a remote module to a local directory.
+$ buf export buf.build/<owner>/<repo>:<version> --output=<output-dir>
 
 # Export a git repo to a local directory.
 $ buf export https://<git-server>/<owner>/<repo>.git --output=<output-dir>
