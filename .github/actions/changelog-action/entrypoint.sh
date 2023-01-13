66e3e55f24a5f3c8433f7b5ad421395a171f0ff9
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-01-13 12:13:58 -0500

    conform to styleguide

--- .github/actions/changelog-action/entrypoint.sh
+++ .github/actions/changelog-action/entrypoint.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-filename="CHANGELOG.md"
-printing=false
-#While loop to read line by line
-while IFS= read -r line; do
-  #If the line starts with ## & currently printing, disable printing
-  if [[ $line == \#\#* ]] && [[ $printing == true ]]; then
+set -euo pipefail
+
+FILENAME="CHANGELOG.md"
+PRINT="false"
+# While loop to read line by line
+while IFS= read -r LINE; do
+  # If the line starts with ## & currently printing, disable PRINT
+  if [[ "${LINE}" == "##"* ]] && [[ "${PRINT}" == "true" ]]; then
     break
   fi
   # If printing is enabled, print the line.
-  if [[ $printing == true ]]; then
-    echo "$line"
+  if [[ "${PRINT}" == "true" ]]; then
+    echo "${LINE}"
   fi
-  #If the line starts with ## & not currently printing, enable printing
-  if [[ $line == "## [$VERSION]"* ]] && [[ $printing == false ]]; then
-    printing=true
+  # If the line starts with ## & not currently printing, enable PRINT
+  if [[ "${LINE}" == "## [${VERSION}]"* ]] && [[ "${PRINT}" == "false" ]]; then
+    PRINT="true"
   fi
-done <"$filename"
+done <"${FILENAME}"
