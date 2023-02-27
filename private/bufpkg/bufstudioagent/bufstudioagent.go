61806a9a7eee7c67e2375786671f810b75f5a1ca
Doria Keung <doria.keung@gmail.com>
2023-02-27 11:53:59 -0500

    Add `--private-network` flag to Studio agent

--- private/bufpkg/bufstudioagent/bufstudioagent.go
+++ private/bufpkg/bufstudioagent/bufstudioagent.go
@@ -30,26 +30,34 @@ func NewHandler(
 	tlsClientConfig *tls.Config,
 	disallowedHeaders map[string]struct{},
 	forwardHeaders map[string]string,
+	privateNetwork bool,
 ) http.Handler {
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:      []string{origin},
-		AllowedMethods:      []string{http.MethodPost, http.MethodOptions},
-		AllowCredentials:    true,
-		AllowPrivateNetwork: true,
-	})
+	corsHandlerOptions := cors.Options{
+		AllowedOrigins:   []string{origin},
+		AllowedMethods:   []string{http.MethodPost, http.MethodOptions},
+		AllowCredentials: true,
+	}
+	if privateNetwork {
+		corsHandlerOptions.AllowPrivateNetwork = true
+	}
+	corsHandler := cors.New(corsHandlerOptions)
 	plainHandler := corsHandler.Handler(newPlainPostHandler(logger, disallowedHeaders, forwardHeaders, tlsClientConfig))
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		case http.MethodOptions:
-			corsHandler.HandlerFunc(w, r)
-			return
 		case http.MethodGet:
 			// In the future we could check for an upgrade header here.
 			_, _ = w.Write([]byte("OK"))
 		case http.MethodPost:
 			plainHandler.ServeHTTP(w, r)
 			return
+		case http.MethodOptions:
+			if privateNetwork {
+				corsHandler.HandlerFunc(w, r)
+				return
+			}
+			// If the private network flag is not used, fall through to the default
+			fallthrough
 		default:
 			http.Error(w, "", http.StatusMethodNotAllowed)
 			return
