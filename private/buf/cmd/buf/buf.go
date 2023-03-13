0dd960a78a1e4227df5624dae35c05dde7186b60
bufdev <bufdev-github@buf.build>
2023-03-13 15:55:12 -0400

    commit

--- private/buf/cmd/buf/buf.go
+++ private/buf/cmd/buf/buf.go
@@ -21,7 +21,6 @@ import (
 	"github.com/bufbuild/buf/private/buf/bufcli"
 	curatedplugindelete "github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/plugin/plugindelete"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/plugin/pluginpush"
-	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/price"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/protoc"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/registry/token/tokencreate"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/registry/token/tokendelete"
@@ -29,6 +28,7 @@ import (
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/registry/token/tokenlist"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/alpha/stats"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/beta/migratev1beta1"
+	"github.com/bufbuild/buf/private/buf/cmd/buf/command/beta/price"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/beta/registry/commit/commitget"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/beta/registry/commit/commitlist"
 	"github.com/bufbuild/buf/private/buf/cmd/buf/command/beta/registry/draft/draftdelete"
@@ -146,6 +146,7 @@ func NewRootCommand(name string) *appcmd.Command {
 				Use:   "beta",
 				Short: "Beta commands. Unstable and likely to change",
 				SubCommands: []*appcmd.Command{
+					price.NewCommand("price", builder),
 					migratev1beta1.NewCommand("migrate-v1beta1", builder),
 					studioagent.NewCommand("studio-agent", noTimeoutBuilder),
 					{
@@ -248,7 +249,6 @@ func NewRootCommand(name string) *appcmd.Command {
 					},
 				},
 			},
-			price.NewCommand("price", builder),
 			{
 				Use:    "alpha",
 				Short:  "Alpha commands. Unstable and recommended only for experimentation. These may be deleted",
