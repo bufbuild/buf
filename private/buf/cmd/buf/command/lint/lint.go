43ee6686f6ff504a1aabc1d589dc48718440b118
Saquib Mian <smian@buf.build>
2023-03-21 13:41:45 -0400

    Remove the `buffetch.RefParserWithProtoFileRefAllowed` option.
    This option is always passed into `buffetch.NewRefParser`, so I removed it.
    
    There were no options left, so I also removed all options.

--- private/buf/cmd/buf/command/lint/lint.go
+++ private/buf/cmd/buf/command/lint/lint.go
@@ -109,7 +109,7 @@ func run(
 	if err != nil {
 		return err
 	}
-	ref, err := buffetch.NewRefParser(container.Logger(), buffetch.RefParserWithProtoFileRefAllowed()).GetRef(ctx, input)
+	ref, err := buffetch.NewRefParser(container.Logger()).GetRef(ctx, input)
 	if err != nil {
 		return err
 	}
