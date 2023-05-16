f9c36012518612b96b2646293eff20e855d74810
Julian Figueroa <jfigueroa@buf.build>
2023-05-16 10:44:27 -0500

    Throw ctx cancelled error in `thread.Parallelize` (#2089)
    Found a panic in a test, caused because of a DX assumption. From users
    perspective, doing:
    
    ```go
    foo := make([]Foo, someLen)
    // jobs preparation... each job fills one item in []foo
    if err := thread.Parallelize(
      ctx,
      jobs,
      thread.ParallelizeWithCancel(cancelGetBlobs), // if one job fails, stop all jobs
    ); err != nil {
      return nil, err
    }
    // at this point the user could assume that if no errors happened, then []foo was successfully
    // populated... well, if the ctx was cancelled, some/all items in []foo might be empty/nil.
    // Even if the ctx was cancelled not by an error trying to fill []foo, but upstream from
    // another caller.
    ```

--- private/pkg/thread/thread_test.go
+++ private/pkg/thread/thread_test.go
@@ -26,17 +26,37 @@ func TestParallelizeWithImmediateCancellation(t *testing.T) {
 	t.Parallel()
 	// The bulk of the code relies on subtle timing that's difficult to
 	// reproduce, but we can test the most basic use case.
-	var executed atomic.Int64
-	var jobs []func(context.Context) error
-	for i := 0; i < 10; i++ {
-		jobs = append(jobs, func(_ context.Context) error {
-			executed.Inc()
-			return nil
-		})
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-	err := Parallelize(ctx, jobs)
-	assert.Nil(t, err, "parallelize error")
-	assert.Equal(t, int64(0), executed.Load(), "jobs executed")
+	t.Run("RegularRun", func(t *testing.T) {
+		t.Parallel()
+		const jobsToExecute = 10
+		var (
+			executed atomic.Int64
+			jobs     = make([]func(context.Context) error, 0, jobsToExecute)
+		)
+		for i := 0; i < jobsToExecute; i++ {
+			jobs = append(jobs, func(_ context.Context) error {
+				executed.Inc()
+				return nil
+			})
+		}
+		err := Parallelize(context.Background(), jobs)
+		assert.NoError(t, err)
+		assert.Equal(t, int64(jobsToExecute), executed.Load(), "jobs executed")
+	})
+	t.Run("WithCtxCancellation", func(t *testing.T) {
+		t.Parallel()
+		var executed atomic.Int64
+		var jobs []func(context.Context) error
+		for i := 0; i < 10; i++ {
+			jobs = append(jobs, func(_ context.Context) error {
+				executed.Inc()
+				return nil
+			})
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+		err := Parallelize(ctx, jobs)
+		assert.Error(t, err)
+		assert.Equal(t, int64(0), executed.Load(), "jobs executed")
+	})
 }
