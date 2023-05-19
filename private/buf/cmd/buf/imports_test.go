83b42f5b4859a6599e51815ebca6a710ef1986b2
Julian Figueroa <jfigueroa@buf.build>
2023-05-19 12:59:15 -0500

    Better test format for clear read

--- private/buf/cmd/buf/imports_test.go
+++ private/buf/cmd/buf/imports_test.go
@@ -75,8 +75,11 @@ func TestInvalidImportFromTransitive(t *testing.T) {
 	t.Parallel()
 	testRunStderrWithCache(
 		t, nil, 1,
-		`Failure: target proto file "school/v1/school1.proto" imports "people/v1/people1.proto", not found in your local target files or direct dependencies, but found in transitive dependency "bufbuild.test/bufbot/people", please declare that one as explicit dependency in your buf.yaml file; `+ // "people1.proto" failure
-			`target proto file "school/v1/school1.proto" imports "people/v1/people2.proto", not found in your local target files or direct dependencies, but found in transitive dependency "bufbuild.test/bufbot/people", please declare that one as explicit dependency in your buf.yaml file`, // "people2.proto" failure
+		`Failure: `+
+			// "people1.proto" failure
+			`target proto file "school/v1/school1.proto" imports "people/v1/people1.proto", not found in your local target files or direct dependencies, but found in transitive dependency "bufbuild.test/bufbot/people", please declare that one as explicit dependency in your buf.yaml file; `+
+			// "people2.proto" failure
+			`target proto file "school/v1/school1.proto" imports "people/v1/people2.proto", not found in your local target files or direct dependencies, but found in transitive dependency "bufbuild.test/bufbot/people", please declare that one as explicit dependency in your buf.yaml file`,
 		"build",
 		filepath.Join("testdata", "imports", "failure", "school"),
 	)
