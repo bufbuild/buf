17b517720110c82d180c7b2ee36ca37ca59dcb5d
Oliver Sun <osun@buf.build>
2023-07-21 13:22:17 -0400

    update

--- private/bufpkg/bufimage/bufimagemodify/bufimagemodifyv2/field_option_modifier_test.go
+++ private/bufpkg/bufimage/bufimagemodify/bufimagemodifyv2/field_option_modifier_test.go
@@ -25,62 +25,64 @@ import (
 	"google.golang.org/protobuf/types/descriptorpb"
 )
 
-func TestGetFields(t *testing.T) {
+func TestFieldModifier(t *testing.T) {
 	t.Parallel()
-	image := bufimagemodifytesting.GetTestImage(
-		t,
-		filepath.Join("..", "testdata", "fieldoptions"),
-		true, // TODO: test when this is false
-	)
-	require.NotNil(t, image)
-	markSweeper := newMarkSweeper(image)
-	require.NotNil(t, markSweeper)
-	imageFile := image.GetFile("a.proto")
-	require.NotNil(t, imageFile)
-	m, err := NewFieldOptionModifier(imageFile, markSweeper)
-	require.NoError(t, err)
-	// TODO: remove
-	// printImage(image)
-	expectedNames := []string{
-		"foo.bar.baz.Outer.Inner.i1",
-		"foo.bar.baz.Outer.Inner.i2",
-		"foo.bar.baz.Outer.Inner.i3",
-		"foo.bar.baz.Outer.Inner.i4",
-		"foo.bar.baz.Outer.Inner.i5",
-		"foo.bar.baz.Outer.Inner.i6",
-		"foo.bar.baz.Outer.o1",
-		"foo.bar.baz.Outer.o2",
-		"foo.bar.baz.Outer.o3",
-		"foo.bar.baz.Outer.o4",
-	}
-	actualNames := m.FieldNames()
-	sort.Strings(expectedNames)
-	sort.Strings(actualNames)
-	require.Equal(t, expectedNames, actualNames)
+	for _, includeSourceCodeInfo := range []bool{true, false} {
+		image := bufimagemodifytesting.GetTestImage(
+			t,
+			filepath.Join("..", "testdata", "fieldoptions"),
+			includeSourceCodeInfo,
+		)
+		require.NotNil(t, image)
+		markSweeper := newMarkSweeper(image)
+		require.NotNil(t, markSweeper)
+		imageFile := image.GetFile("a.proto")
+		require.NotNil(t, imageFile)
+		modifier, err := NewFieldOptionModifier(imageFile, markSweeper)
+		require.NoError(t, err)
+		expectedNames := []string{
+			"foo.bar.baz.Outer.Inner.i1",
+			"foo.bar.baz.Outer.Inner.i2",
+			"foo.bar.baz.Outer.Inner.i3",
+			"foo.bar.baz.Outer.Inner.i4",
+			"foo.bar.baz.Outer.Inner.i5",
+			"foo.bar.baz.Outer.Inner.i6",
+			"foo.bar.baz.Outer.o1",
+			"foo.bar.baz.Outer.o2",
+			"foo.bar.baz.Outer.o3",
+			"foo.bar.baz.Outer.o4",
+		}
+		actualNames := modifier.FieldNames()
+		sort.Strings(expectedNames)
+		sort.Strings(actualNames)
+		require.Equal(t, expectedNames, actualNames)
 
-	messages := imageFile.Proto().GetMessageType()
-	require.Len(t, messages, 1)
-	outerMessage := messages[0]
-	require.Len(t, outerMessage.GetNestedType(), 1)
-	innerMessage := outerMessage.GetNestedType()[0]
-	require.Len(t, innerMessage.GetField(), 6)
-	fifthField := innerMessage.GetField()[4]
-	require.NotNil(t, fifthField.GetOptions())
-	require.NotNil(t, fifthField.GetOptions().Jstype)
-	require.Equal(t, descriptorpb.FieldOptions_JS_STRING, *fifthField.GetOptions().Jstype)
+		messages := imageFile.Proto().GetMessageType()
+		require.Len(t, messages, 1)
+		outerMessage := messages[0]
+		require.Len(t, outerMessage.GetNestedType(), 1)
+		innerMessage := outerMessage.GetNestedType()[0]
+		require.Len(t, innerMessage.GetField(), 6)
+		fifthField := innerMessage.GetField()[4]
+		require.NotNil(t, fifthField.GetOptions())
+		require.NotNil(t, fifthField.GetOptions().Jstype)
+		require.Equal(t, descriptorpb.FieldOptions_JS_STRING, *fifthField.GetOptions().Jstype)
 
-	err = m.ModifyJSType("foo.bar.baz.Outer.Inner.i5", newValueOverride(descriptorpb.FieldOptions_JS_NUMBER))
-	require.NoError(t, err)
+		err = modifier.ModifyJSType("foo.bar.baz.Outer.Inner.i5", NewValueOverride(descriptorpb.FieldOptions_JS_NUMBER))
+		require.NoError(t, err)
 
-	require.Equal(t, descriptorpb.FieldOptions_JS_NUMBER, *fifthField.GetOptions().Jstype)
+		require.Equal(t, descriptorpb.FieldOptions_JS_NUMBER, *fifthField.GetOptions().Jstype)
 
-	require.Equal(
-		t,
-		map[string]map[string]struct{}{
-			"a.proto": {
-				internal.GetPathKey([]int32{4, 0, 3, 0, 2, 4, 8, 6}): struct{}{},
+		// still marked even if the image is built without source code info, but
+		// sweep will not take effect.
+		require.Equal(
+			t,
+			map[string]map[string]struct{}{
+				"a.proto": {
+					internal.GetPathKey([]int32{4, 0, 3, 0, 2, 4, 8, 6}): struct{}{},
+				},
 			},
-		},
-		markSweeper.sourceCodeInfoPaths,
-	)
+			markSweeper.sourceCodeInfoPaths,
+		)
+	}
 }
