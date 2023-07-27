ea4a606e86604686d65b0545c632134a4277d597
Oliver Sun <osun@buf.build>
2023-07-27 12:47:35 -0400

    add new line after TODO for clarity

--- private/bufpkg/bufimage/bufimagemodify/internal/internal.go
+++ private/bufpkg/bufimage/bufimagemodify/internal/internal.go
@@ -72,9 +72,13 @@ var fileOptionPath = []int32{8}
 
 // RemoveLocationsFromSourceCodeInfo removes paths from the given sourceCodeInfo.
 // Each path must be for either a file option or a field option.
-func RemoveLocationsFromSourceCodeInfo(sourceCodeInfo *descriptorpb.SourceCodeInfo, pathsToRemove map[string]struct{}) error {
+func RemoveLocationsFromSourceCodeInfo(
+	sourceCodeInfo *descriptorpb.SourceCodeInfo,
+	pathsToRemove map[string]struct{},
+) error {
 	// TODO: in v1 there is no need to check for field options, maybe v1 and v2
-	//  don't need to share this function.
+	// don't need to share this function.
+
 	// We can't just match on an exact path match because the target
 	// file option's parent path elements would remain (i.e [8]),
 	// or the target field option's parent path has no other child left.
