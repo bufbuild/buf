cfa5916d9bf202e242283278c15e2b463af6e62e
Julian Figueroa <jfigueroa@buf.build>
2023-08-04 17:13:09 -0500

    mv util funcs to syncer

--- private/buf/bufsync/syncer.go
+++ private/buf/bufsync/syncer.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/bufbuild/buf/private/bufpkg/bufconfig"
 	"github.com/bufbuild/buf/private/bufpkg/bufmodule/bufmodulebuild"
@@ -587,6 +588,36 @@ func (s *syncer) readModuleAt(
 		return nil, readModuleErr
 	}
 	return builtModule, nil
+}
+
+// printSyncPreparation prints information gathered at the sync preparation step.
+func (s *syncer) printSyncPreparation() {
+	s.logger.Debug(
+		"sync preparation",
+		zap.Any("modulesDirsToSync", s.modulesDirsForSync),
+		zap.Any("commitsTags", s.commitsToTags),
+		zap.Any("branchesModulesToSync", s.branchesToModulesForSync),
+		zap.Any("modulesBranchesSyncPoints", s.modulesToBranchesLastSyncPoints),
+	)
+}
+
+// printCommitsForSync prints syncable commits for a given branch.
+func (s *syncer) printCommitsForSync(branch string, syncableCommits []*syncableCommit) {
+	printableCommits := make([]map[string]string, 0)
+	for _, sCommit := range syncableCommits {
+		var commitModules []string
+		for moduleDir, builtModule := range sCommit.modules {
+			commitModules = append(commitModules, moduleDir+":"+builtModule.ModuleIdentity().IdentityString())
+		}
+		printableCommits = append(printableCommits, map[string]string{
+			sCommit.commit.Hash().Hex(): fmt.Sprintf("(%d)[%s]", len(commitModules), strings.Join(commitModules, ", ")),
+		})
+	}
+	s.logger.Debug(
+		"branch commits to sync",
+		zap.String("branch", branch),
+		zap.Any("commits", printableCommits),
+	)
 }
 
 // syncableCommit holds the modules that need to be synced in a git commit.
