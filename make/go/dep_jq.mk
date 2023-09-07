f25e16f7ba24a18f51970e20f36f100b25a7edad
Philip K. Warren <pwarren@buf.build>
2023-09-07 15:53:27 -0500

    Update to latest makego

--- make/go/dep_jq.mk
+++ make/go/dep_jq.mk
@@ -9,20 +9,22 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://stedolan.github.io/jq/download checked 20230201
-JQ_VERSION ?= 1.6
+# https://jqlang.github.io/jq/download 20230906 checked 20230907
+JQ_VERSION ?= 1.7
 
-# jq does not have an ARM release on Github so we'll use
-# the amd64 version via Rosetta
 ifeq ($(UNAME_OS),Darwin)
-JQ_OS := osx
-JQ_ARCH := -amd64
+JQ_OS := macos
+ifeq ($(UNAME_ARCH),x86_64)
+JQ_ARCH := amd64
+else
+JQ_ARCH := $(UNAME_ARCH)
+endif
 endif
 
 ifeq ($(UNAME_ARCH),x86_64)
 ifeq ($(UNAME_OS),Linux)
 JQ_OS := linux
-JQ_ARCH := 64
+JQ_ARCH := amd64
 endif
 endif
 JQ := $(CACHE_VERSIONS)/jq/$(JQ_VERSION)
@@ -30,7 +32,7 @@ $(JQ):
 	@rm -f $(CACHE_BIN)/jq
 	@mkdir -p $(CACHE_BIN)
 	curl -sSL \
-		https://github.com/stedolan/jq/releases/download/jq-$(JQ_VERSION)/jq-$(JQ_OS)$(JQ_ARCH) \
+		https://github.com/jqlang/jq/releases/download/jq-$(JQ_VERSION)/jq-$(JQ_OS)-$(JQ_ARCH) \
 		-o $(CACHE_BIN)/jq
 	chmod +x $(CACHE_BIN)/jq
 	@rm -rf $(dir $(JQ))
