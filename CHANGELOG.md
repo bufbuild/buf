abca9142bd7226acfe643c0cb1f8c1fb1a7bf71d
Doria Keung <doria.keung@gmail.com>
2023-10-03 12:54:21 -0400

    Make `--branch` a separate flag

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,6 @@
 - Update `buf beta price` with the latest pricing information.
 - Fix issue where `buf generate --exclude-path` was not properly excluding paths
   for remote modules.
-- Alias the `--draft` flag for `buf push` to `--branch` and update flag description.
 
 ## [v1.26.1] - 2023-08-09
 
--- private/buf/cmd/buf/command/push/push.go
+++ private/buf/cmd/buf/command/push/push.go
@@ -73,6 +73,7 @@ func NewCommand(
 
 type flags struct {
 	Tags             []string
+	Branch           string
 	Draft            string
 	ErrorFormat      string
 	DisableSymlinks  bool
@@ -107,9 +108,21 @@ func (f *flags) Bind(flagSet *pflag.FlagSet) {
 		draftFlagName,
 		"",
 		fmt.Sprintf(
-			"Push a commit to a branch with the specified name. Cannot be used together with --%s (-%s)",
+			"Make the pushed commit a draft with the specified name. Cannot be used together with --%s (-%s) or --%s",
 			tagFlagName,
 			tagFlagShortName,
+			branchFlagName,
+		),
+	)
+	flagSet.StringVar(
+		&f.Branch,
+		branchFlagName,
+		"",
+		fmt.Sprintf(
+			"Push a commit to a branch with the specified name. Cannot be used together with --%s (-%s) or --%s",
+			tagFlagName,
+			tagFlagShortName,
+			draftFlagName,
 		),
 	)
 	flagSet.StringVar(
@@ -134,17 +147,6 @@ func (f *flags) Bind(flagSet *pflag.FlagSet) {
 		"Do not use. This flag never had any effect",
 	)
 	_ = flagSet.MarkHidden(trackFlagName)
-	flagSet.SetNormalizeFunc(aliasDraftToBranchFunc)
-}
-
-// aliasDraftToBranchFunc aliases the draft flag to branch.
-// The branch flag will have all the same behaviors as the draft flag does currently,
-// including restrictions on use with tags.
-func aliasDraftToBranchFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
-	if name == draftFlagName {
-		name = branchFlagName
-	}
-	return pflag.NormalizedName(name)
 }
 
 func run(
@@ -158,8 +160,16 @@ func run(
 	if err := bufcli.ValidateErrorFormatFlag(flags.ErrorFormat, errorFormatFlagName); err != nil {
 		return err
 	}
+	if flags.Draft != "" && flags.Branch != "" {
+		return appcmd.NewInvalidArgumentErrorf("--%s and --%s cannot be used together.", draftFlagName, branchFlagName)
+	}
 	if len(flags.Tags) > 0 && flags.Draft != "" {
 		return appcmd.NewInvalidArgumentErrorf("--%s (-%s) and --%s cannot be used together.", tagFlagName, tagFlagShortName, draftFlagName)
+	}
+	// For now, we are restricting branch behavior to be exactly the same as drafts, and thus
+	// cannot be used in conjunction with tags.
+	if len(flags.Tags) > 0 && flags.Branch != "" {
+		return appcmd.NewInvalidArgumentErrorf("--%s (-%s) and --%s cannot be used together.", tagFlagName, tagFlagShortName, branchFlagName)
 	}
 	if flags.CreateVisibility != "" {
 		if !flags.Create {
