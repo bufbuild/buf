0bb15e64ab7a58461a00baa8a4dadbe09dab0d6f
bufdev <bufdev-github@buf.build>
2023-10-29 13:18:39 -0400

    commit

--- private/bufpkg/bufcas/file_set.go
+++ private/bufpkg/bufcas/file_set.go
@@ -20,6 +20,7 @@ import (
 	"sort"
 
 	"github.com/bufbuild/buf/private/pkg/storage"
+	"go.uber.org/multierr"
 )
 
 // FileSet is a pair of a Manifest and its associated BlobSet.
@@ -123,7 +124,7 @@ func PutFileSetToBucket(
 		}
 		blob := fileSet.BlobSet().GetBlob(fileNode.Digest())
 		if _, err := writeObjectCloser.Write(blob.Content()); err != nil {
-			return err
+			return multierr.Append(err, writeObjectCloser.Close())
 		}
 		if err := writeObjectCloser.Close(); err != nil {
 			return err
