5e0a7c58ab7cf30546cd0e22463c9273923bac3a
bufdev <bufdev-github@buf.build>
2023-11-01 15:06:23 -0400

    commit

--- private/bufnew/bufmodule/bufmodule.go
+++ private/bufnew/bufmodule/bufmodule.go
@@ -2,43 +2,19 @@ package bufmodule
 
 import (
 	"context"
+	"errors"
 	"io"
 
+	"github.com/bufbuild/buf/private/bufpkg/bufcas"
 	"github.com/bufbuild/buf/private/pkg/storage"
 )
 
-// What is a Module?
-// It definitely includes its .proto files.
-// It includes its list of resolved dependencies ie buf.lock
-// It includes its LICENSE - this is part of the module, a change in the license is a change to the module
-// It includes its README - documentation is either in comments or in readme, both are part of the module
-// It does not include breaking or lint config - this doesn't comprise module data, this is just used to operate on the module itself in certain situations (CLI)
-// It does not include excludes - these are already excluded when building the module
-// It does not include generation config obviously
-// Basically, if you find yourself saying "X operates on the module", it's probably not part of the module itself (ie lint config, breaking config, excludes)
-//
-// What is a Workspace?
-// It includes all of its modules
-// It includes its list of resolved dependencies?
-// What about LICENSE and README? A workspace isn't licensed, a Module is. A workspace isn't documented, a Module is.
-//
-// What about config?
-// Only the CLI cares. This isn't part of Module or Workspace at all.
-//
-// What about --path/--exclude-path?
-// Only the CLI cares.
-//
-// What about declared dependencies?
-// Only the CLI cares.
-//
-// What about excludes?
-// Only the CLI cares.
-//
-// What does this lead to?
-// buf.yaml should not be part of the digest.
-// Our Workspace and Module types should fall as above.
-// We should figure out how to move Config to something like CommandMeta below, similar to ModuleConfig/ImageConfig/ModuleConfigSet. This should be a type outside of bufwire.
-// We should figure out a nice way to deal with TargetFileInfos at a level outside of Module and Workspace.
+var (
+	// ErrNotExist is the error returned if a File retrieved does not exist.
+	//
+	// Ese errors.Is(err,
+	ErrNotExist = errors.New("file does not exist")
+)
 
 type ModuleFullName interface {
 	Remote() string
@@ -48,17 +24,17 @@ type ModuleFullName interface {
 	isModuleFullName()
 }
 
-type ModuleReference interface {
+type ModuleRef interface {
 	ModuleFullName() ModuleFullName
-	Reference() string
+	Ref() string
 
 	isModuleReference()
 }
 
 type ModulePin interface {
 	ModuleFullName() ModuleFullName
-	Commit() string
-	Digest() string
+	CommitID() string
+	Digest() bufcas.Digest
 
 	isModulePin()
 }
@@ -73,34 +49,36 @@ type ModuleSet interface {
 	isModuleSet()
 }
 
-type ProtoFileInfo interface {
+type FileInfo interface {
 	storage.ObjectInfo
 
 	Module() Module
 
-	isProtoFileInfo()
+	isFileInfo()
 }
 
-type ProtoFile interface {
-	ProtoFileInfo
+type File interface {
+	FileInfo
 	io.ReadCloser
 
-	isProtoFile()
+	isFile()
 }
 
 type Module interface {
-	ProtoFileInfos(context.Context) ([]ProtoFileInfo, error)
-	GetProtoFile(ctx context.Context, path string) (ProtoFile, error)
-	DocContent() string
-	DocPath() string
-	LicenseContent() string
+	// GetFile gets the File within the Module as specified by the path.
+	//
+	// Returns an error that satisfies storage.IsNotExi
+	GetFile(ctx context.Context, path string) (File, error)
+	StatFile(ctx context.Context, path string) (FileInfo, error)
+	WalkFiles(ctx context.Context, f func(FileInfo) error) error
+
 	ModuleSet() ModuleSet
 	// Returns all deps in ModuleSet on v2
 	Deps() []ModulePin
 
-	ID() string
+	Ref() string
 	FullName() ModuleFullName
-	Commit() string
+	CommitID() string
 
 	isModule()
 }
