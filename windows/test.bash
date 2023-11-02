ef273c84d6b07111f16ead9e6b27b0a02c4c2801
bufdev <bufdev-github@buf.build>
2023-11-02 17:35:28 -0400

    fix

--- windows/test.bash
+++ windows/test.bash
@@ -28,7 +28,7 @@ fi
 PATH="${DOWNLOAD_CACHE}/protoc/bin:${PATH}"
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GEN_GO_VERSION}
-go install github.com/bufbuild/connect-go/cmd/protoc-gen-connect-go@${CONNECT_VERSION}
+go install connectrpc.com/connect/cmd/protoc-gen-connect-go@${CONNECT_VERSION}
 go install ./cmd/buf \
   ./private/buf/cmd/buf/command/alpha/protoc/internal/protoc-gen-insertion-point-writer \
   ./private/buf/cmd/buf/command/alpha/protoc/internal/protoc-gen-insertion-point-receiver
