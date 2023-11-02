9020f9aed1747c13bf67204eab9daa3f6da7810d
bufdev <bufdev-github@buf.build>
2023-11-02 16:50:33 -0400

    commit

--- private/bufpkg/bufimage/bufimageutil/bufimageutil_test.go
+++ private/bufpkg/bufimage/bufimageutil/bufimageutil_test.go
@@ -44,7 +44,8 @@ import (
 )
 
 // IF YOU HAVE ANY FAILING TESTS IN HERE, ESPECIALLY AFTER A PROTOC UPGRADE,
-// SWITCH THIS TO TRUE, TURN OFF PARALLEL TESTING go test -parallel 1 ./..., RE-RUN THE TESTS AND THEN SWITCH BACK TO FALSE.
+// SWITCH THIS TO TRUE, TURN OFF PARALLEL TESTING, RE-RUN THE TESTS AND THEN SWITCH BACK TO FALSE.
+// go test -parallel 1 ./privage/pkg/bufimage/bufimageutil
 const shouldUpdateExpectations = false
 
 func TestOptions(t *testing.T) {
