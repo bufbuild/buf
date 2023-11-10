478649b1a8bde8e0f0f1e6888cb60e5a79deef9f
Oliver Sun <osun@buf.build>
2023-11-10 12:31:02 -0500

    2

--- make/buf/scripts/createreleasepr.bash
+++ make/buf/scripts/createreleasepr.bash
@@ -8,7 +8,7 @@ cd "${DIR}"
 # We already have set -u, but want to fail early if a required variable is not set.
 : ${WEBHOOK_URL}
 # However, if you are already logged in for GitHub CLI locally, you can remove this line when running it locally.
-: ${GH_TOKEN}
+# : ${GH_TOKEN}
 
 if [[ "${VERSION}" == v* ]]; then
   echo "error: VERSION ${VERSION} must not start with 'v'" >&2
