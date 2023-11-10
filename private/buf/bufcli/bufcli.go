69edb75efc0ffdae36f94ef7407a9d63acf85a37
oliversun9 <oliversun9@users.noreply.github.com>
2023-11-10 17:45:04 +0000

    Update version to 1.28.0-test

--- private/buf/bufcli/bufcli.go
+++ private/buf/bufcli/bufcli.go
@@ -65,7 +65,7 @@ import (
 
 const (
 	// Version is the CLI version of buf.
-	Version = "1.27.3-dev"
+	Version = "1.28.0-test"
 
 	inputHTTPSUsernameEnvKey      = "BUF_INPUT_HTTPS_USERNAME"
 	inputHTTPSPasswordEnvKey      = "BUF_INPUT_HTTPS_PASSWORD"
