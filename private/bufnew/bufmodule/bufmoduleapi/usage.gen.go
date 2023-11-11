f8fb36547a23f6febb43a02cefaa0abe31ae6525
bufdev <bufdev-github@buf.build>
2023-11-11 17:16:59 -0500

    commit

--- private/bufnew/bufmodule/bufmoduleapi/usage.gen.go
+++ private/bufnew/bufmodule/bufmoduleapi/usage.gen.go
@@ -12,4 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Generated. DO NOT EDIT.
+
 package bufmoduleapi
+
+import _ "github.com/bufbuild/buf/private/usage"
