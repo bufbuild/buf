f25ee67935ab05f52d1a59cae83620a5c489c1f5
bufdev <4228796+bufdev@users.noreply.github.com>
2023-11-12 13:52:08 -0500

    Update bufcas and slicesextended (#2572)

--- private/bufpkg/bufcas/digest_test.go
+++ private/bufpkg/bufcas/digest_test.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package bufcas
+// Making sure that ParseErrors work outside of the bufcas package.
+package bufcas_test
 
 import (
 	"bytes"
@@ -23,57 +24,66 @@ import (
 	"testing"
 	"testing/iotest"
 
+	"github.com/bufbuild/buf/private/bufpkg/bufcas"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewDigestForContent(t *testing.T) {
 	t.Parallel()
-	digest, err := NewDigestForContent(bytes.NewBuffer(nil))
+	digest, err := bufcas.NewDigestForContent(bytes.NewBuffer(nil))
 	require.NoError(t, err)
-	assert.NotEqual(t, DigestType(0), digest.Type())
+	assert.NotEqual(t, bufcas.DigestType(0), digest.Type())
 	assert.NotEmpty(t, digest.Value())
 
-	digest, err = NewDigestForContent(strings.NewReader("some content"))
+	digest, err = bufcas.NewDigestForContent(strings.NewReader("some content"))
 	require.NoError(t, err)
-	assert.NotEqual(t, DigestType(0), digest.Type())
+	assert.NotEqual(t, bufcas.DigestType(0), digest.Type())
 	assert.NotEmpty(t, digest.Value())
 
 	// failing digesting content
 	expectedErr := errors.New("testing error")
-	digest, err = NewDigestForContent(iotest.ErrReader(expectedErr))
+	digest, err = bufcas.NewDigestForContent(iotest.ErrReader(expectedErr))
 	assert.ErrorIs(t, err, expectedErr)
 	assert.Nil(t, digest)
 }
 
 func TestParseDigestError(t *testing.T) {
 	t.Parallel()
-	testParseDigestError(t, "")
-	testParseDigestError(t, "foo")
-	testParseDigestError(t, "shake256 foo")
-	testParseDigestError(t, "shake256:_")
-	validDigest, err := NewDigestForContent(bytes.NewBuffer(nil))
+	testParseDigestError(t, "", false)
+	testParseDigestError(t, "foo", true)
+	testParseDigestError(t, "shake256 foo", true)
+	testParseDigestError(t, "shake256:_", true)
+	validDigest, err := bufcas.NewDigestForContent(bytes.NewBuffer(nil))
 	require.NoError(t, err)
 	validDigestHex := hex.EncodeToString(validDigest.Value())
-	testParseDigestError(t, fmt.Sprintf("%s:%s", validDigest.Type(), validDigestHex[:10]))
-	testParseDigestError(t, fmt.Sprintf("md5:%s", validDigestHex))
+	testParseDigestError(t, fmt.Sprintf("%s:%s", validDigest.Type(), validDigestHex[:10]), true)
+	testParseDigestError(t, fmt.Sprintf("md5:%s", validDigestHex), true)
 }
 
 func TestDigestEqual(t *testing.T) {
 	t.Parallel()
 	fileContent := "one line\nanother line\nyet another one\n"
-	d1, err := NewDigestForContent(strings.NewReader(fileContent))
+	d1, err := bufcas.NewDigestForContent(strings.NewReader(fileContent))
 	require.NoError(t, err)
-	d2, err := NewDigestForContent(strings.NewReader(fileContent))
+	d2, err := bufcas.NewDigestForContent(strings.NewReader(fileContent))
 	require.NoError(t, err)
-	d3, err := NewDigestForContent(strings.NewReader(fileContent + "foo"))
+	d3, err := bufcas.NewDigestForContent(strings.NewReader(fileContent + "foo"))
 	require.NoError(t, err)
-	assert.True(t, DigestEqual(d1, d2))
-	assert.False(t, DigestEqual(d1, d3))
-	assert.False(t, DigestEqual(d2, d3))
+	assert.True(t, bufcas.DigestEqual(d1, d2))
+	assert.False(t, bufcas.DigestEqual(d1, d3))
+	assert.False(t, bufcas.DigestEqual(d2, d3))
 }
 
-func testParseDigestError(t *testing.T, digestString string) {
-	_, err := ParseDigest(digestString)
+func testParseDigestError(t *testing.T, digestString string, expectParseError bool) {
+	_, err := bufcas.ParseDigest(digestString)
 	assert.Error(t, err)
+	parseError := &bufcas.ParseError{}
+	isParseError := errors.As(err, &parseError)
+	if expectParseError {
+		assert.True(t, isParseError)
+		assert.Equal(t, digestString, parseError.Input())
+	} else {
+		assert.False(t, isParseError)
+	}
 }
