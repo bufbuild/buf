00bd41b1e7ed19095ecdad0fa42aba5a777633bf
Oliver Sun <osun@buf.build>
2023-11-15 16:51:33 -0500

    fix

--- private/bufpkg/bufapimodule/module_reader.go
+++ private/bufpkg/bufapimodule/module_reader.go
@@ -79,7 +79,7 @@ func (m *moduleReader) GetModule(ctx context.Context, modulePin bufmoduleref.Mod
 		return nil, err
 	}
 	if logger, repositoryClientFactory := m.logger, m.repositoryClientFactory; logger != nil && repositoryClientFactory != nil {
-		if err := warnIfDeprecated(ctx, m.repositoryClientFactory, modulePin, m.logger); err != nil {
+		if err := warnIfDeprecated(ctx, repositoryClientFactory, modulePin, logger); err != nil {
 			return nil, err
 		}
 	}
