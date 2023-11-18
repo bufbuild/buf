0c9c8cad8d15bc4e16aeedf84b5c03a8a2426657
bufdev <bufdev-github@buf.build>
2023-11-17 22:28:29 -0500

    commit

--- private/bufnew/bufworkspace/workspace.go
+++ private/bufnew/bufworkspace/workspace.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/bufbuild/buf/private/bufnew/bufconfig"
 	"github.com/bufbuild/buf/private/bufnew/bufmodule"
+	"github.com/bufbuild/buf/private/pkg/normalpath"
 	"github.com/bufbuild/buf/private/pkg/slicesextended"
 	"github.com/bufbuild/buf/private/pkg/storage"
 )
@@ -42,6 +43,12 @@ func newWorkspaceForBucket(
 	workspaceOptions := newWorkspaceOptions()
 	for _, option := range options {
 		option(workspaceOptions)
+	}
+	// This also automatically makes "" into "."
+	var err error
+	workspaceOptions.subDirPath, err = normalpath.NormalizeAndValidate(workspaceOptions.subDirPath)
+	if err != nil {
+		return nil, err
 	}
 	// TODO
 	return nil, errors.New("TODO newWorkspaceForBucket")
@@ -68,3 +75,67 @@ func (w *workspace) LockedDepModuleKeys() []bufmodule.ModuleKey {
 }
 
 func (*workspace) isWorkspace() {}
+
+// *** PRIVATE ***
+
+const (
+	// searchResultTypeBufYAMLV1OrV1Beta1NoBufWorkYAMLV1 indicates that a v1 or v1beta1 buf.yaml
+	// was found at the subDirPath, but no v1 buf.work.yaml was found anywhere in the bucket
+	// going up from the subDirPath to the root of the bucket.
+	searchResultTypeBufYAMLV1OrV1Beta1NoBufWorkYAMLV1 searchResultType = iota + 1
+	// searchResultTypeBufYAMLV1OrV1Beta1WithBufWorkYAMLV1 indicates that a v1 or v1beta1 buf.yaml
+	// was found at the subDirPath, and a v1 buf.work.yaml was found in the bucket going up
+	// from the subDirPath to the root of the bucket.
+	searchResultTypeBufYAMLV1OrV1Beta1WithBufWorkYAMLV1
+	// searchResultTypeBufYAMLV1OrV1Beta1NoBufWorkYAMLV1 indicates that no v1 or v1beta1 buf.yaml
+	// was found, and no buf.work.yaml was found. In this situation, we act as if there was a
+	// default v1 buf.yaml at the subDirPath with no enclosing workspace.
+	searchResultTypeNoBufYAMLV1OrV1Beta1NoBufWorkYAMLV1
+	// searchResultTypeBufYAMLV1OrV1Beta1WithBufWorkYAMLV1 indicates that no v1 or v1beta1 buf.work.yaml
+	// was found, but a buf.work.yaml was found. In this situation, we act as if there was a default
+	// v1 buf.yaml at the subDirPath, using the buf.work.yaml as the workspace if the buf.work.yaml
+	// contains the subDirPath as a directory.
+	searchResultTypeNoBufYAMLV1OrV1Beta1WithBufWorkYAMLV1
+
+	searchResultTypeBufWorkYAMLV1
+	searchResultTypeBufYAMLV1
+	searchResultTypeEmpty
+)
+
+type searchResultType int
+
+type searchResult struct {
+	// ctx is the input Context.
+	ctx context.Context
+	// bucket is the input storage.ReadBucket.
+	bucket storage.ReadBucket
+	// subDirPath is the input subDirPath.
+	subDirPath string
+
+	// searchResultType indicates how we should handle this search result.
+	searchResultType searchResultType
+	// bufYAMLV1OrV1Beta1Path indicates the path within the bucket that contains the
+	// v1 or v1beta1 buf.yaml
+	bufYAMLV1OrV1Beta1Path string
+	// bufWorkYAMLPath indicates the path within the bucket that contains the buf.work.yaml.
+	bufWorkYAMLPath string
+}
+
+func newSearchResult(
+	ctx context.Context,
+	bucket storage.ReadBucket,
+	subDirPath string,
+) (*searchResult, error) {
+	return nil, errors.New("TODO")
+}
+
+// for curPath := subDirPath; curPath != "."; curPath = normalpath.Dir(curPath) {
+// }
+
+func readBufYAML(
+	ctx context.Context,
+	bucket storage.ReadBucket,
+	path string,
+) (bufconfig.ConfigFile, error) {
+	return nil, errors.New("TODO")
+}
