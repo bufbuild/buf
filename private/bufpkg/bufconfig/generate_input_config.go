c6237c73f233229319b6b7f41097333c486ef2d5
Oliver Sun <osun@buf.build>
2023-11-27 18:57:59 -0500

    update input interface

--- private/bufpkg/bufconfig/generate_input_config.go
+++ private/bufpkg/bufconfig/generate_input_config.go
@@ -15,35 +15,32 @@
 
 package bufconfig
 
+type InputConfigType int
+
+const (
+	InputConfigTypeModule InputConfigType = iota + 1
+	InputConfigTypeDirectory
+	InputConfigTypeGitRepo
+	InputConfigTypeProtoFile
+	InputConfigTypeTarball
+	InputConfigTypeZipArchive
+	InputConfigTypeBinaryImage
+	InputConfigTypeJSONImage
+	InputConfigTypeTextImage
+)
+
 // GenerateInputConfig is an input configuration for code generation.
 type GenerateInputConfig interface {
-	// Exactly one of Module, Directory, ProtoFile, Tarball, ZipArchive, BinaryImage,
-	// JSONImage or GitRepo is not empty, indicating the input format.
-
-	// Module returns the module name.
-	Module() string
-	// Direcotry returns the path to the directory, relative or absolute.
-	Directory() string
-	// ProtoFile returns the path to the proto file, relative or absolute.
-	ProtoFile() string
-	// Tarball returns the path to the tarball, relative or absolute.
-	Tarball() string
-	// ZipArchive returns the path to the zip archive, relative or absolute.
-	ZipArchive() string
-	// BinaryImage returns the path to the binary buf image, relative or absolute.
-	BinaryImage() string
-	// JSONImage returns the path to the JSON buf image, relative or absolute.
-	JSONImage() string
-	// TextImage returns the path to the text buf image, relative or absolute.
-	TextImage() string
-	// GitRepo returns the git repository address in https, ssh or local format.
-	GitRepo() string
+	// Type returns the input type.
+	Type() InputConfigType
+	// Location returns the location for the input.
+	Location() string
 	// Compression returns the compression scheme, not empty only if format is
 	// one of tarball, binary image, json image or text image.
 	Compression() string
-	// StripComponent returns the number of directories to strip for tar or zip
+	// StripComponents returns the number of directories to strip for tar or zip
 	// inputs, not empty only if format is tarball or zip archive.
-	StripComponent() uint32
+	StripComponents() *uint32
 	// Subdir returns the subdirectory to use, not empty only if format is one
 	// git repo, tarball and zip archive.
 	Subdir() string
@@ -54,16 +51,16 @@ type GenerateInputConfig interface {
 	// Ref returns the git ref to checkout, not empty only if format is git.
 	Ref() string
 	// Ref returns the depth to clone the git repo with, not empty only if format is git.
-	Depth() string
+	Depth() *uint32
 	// RecurseSubmodules returns whether to clone submodules recursively. Not empty
 	// only if input if git.
 	RecurseSubmodules() bool
 	// IncludePackageFiles returns other files in the same package as the proto file,
 	// not empty only if format is proto file.
 	IncludePackageFiles() bool
-	// Types returns the types to generate. If GenerateConfig.GenerateTypeConfig()
+	// IncludeTypes returns the types to generate. If GenerateConfig.GenerateTypeConfig()
 	// returns a non-empty list of types.
-	Types() []string
+	IncludeTypes() []string
 	// ExcludePaths returns paths not to generate for.
 	ExcludePaths() []string
 	// IncludePaths returns paths to generate for.
