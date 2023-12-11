9280a7b2b627d747b79688f7258179d7e9b2e994
John Chadwick <jchadwick@buf.build>
2023-12-11 15:34:29 -0500

    Fix gofmt issues

--- private/bufpkg/bufimage/bufimagetesting/bufimagetesting.go
+++ private/bufpkg/bufimage/bufimagetesting/bufimagetesting.go
@@ -17,8 +17,8 @@ package bufimagetesting
 import (
 	"testing"
 
-	"github.com/bufbuild/buf/private/bufpkg/bufmodule"
 	"github.com/bufbuild/buf/private/bufpkg/bufimage"
+	"github.com/bufbuild/buf/private/bufpkg/bufmodule"
 	imagev1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/image/v1"
 	"github.com/bufbuild/buf/private/pkg/protodescriptor"
 	"github.com/stretchr/testify/assert"
