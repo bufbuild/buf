e7c2781132e8f94f756d44d869581f1c6293a19b
bufdev <bufdev-github@buf.build>
2023-12-27 00:46:05 -0500

    update dependencies

--- windows/test.bash
+++ windows/test.bash
@@ -3,8 +3,8 @@
 set -eo pipefail
 
 PROTOC_VERSION="25.0"
-PROTOC_GEN_GO_VERSION="v1.31.0"
-CONNECT_VERSION="v1.12.0"
+PROTOC_GEN_GO_VERSION="v1.32.0"
+CONNECT_VERSION="v1.14.0"
 
 # Convert DOWNLOAD_CACHE from d:\path to /d/path
 DOWNLOAD_CACHE="$(echo "/${DOWNLOAD_CACHE}" | sed 's|\\|/|g' | sed 's/://')"
