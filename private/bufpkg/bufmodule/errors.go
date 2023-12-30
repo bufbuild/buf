d4af960a936a6e56ad2b6c36f958c29610cb1e82
bufdev <bufdev-github@buf.build>
2023-12-30 18:21:55 -0500

    commit

--- private/bufpkg/bufmodule/errors.go
+++ private/bufpkg/bufmodule/errors.go
@@ -17,6 +17,7 @@ package bufmodule
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 var (
@@ -34,6 +35,56 @@ var (
 	// We do assume flag names here, but we're just going with reality.
 	ErrNoTargetProtoFiles = errors.New("no .proto files were targeted. This can occur if no .proto files are found in your input, --path points to files that do not exist, or --exclude-path excludes all files.")
 )
+
+// ParseError is an error that occurred during parsing.
+//
+// This is returned by all Parse.* functions in this package.
+type ParseError struct {
+	// typeString is the user-consumable string representing of the type that was attempted to be parsed.
+	//
+	// Users cannot rely on this data being structured.
+	// Examples: "digest", "digest type".
+	typeString string
+	// input is the input string that was attempted to be parsed.
+	input string
+	// err is the underlying error.
+	//
+	// Err may be a *ParseError itself.
+	//
+	// This is an error we may give back to the user, use pretty strings that should
+	// be read.
+	err error
+}
+
+// Error implements the error interface.
+func (p *ParseError) Error() string {
+	var builder strings.Builder
+	_, _ = builder.WriteString(`could not parse`)
+	if p.typeString != "" {
+		_, _ = builder.WriteString(` `)
+		_, _ = builder.WriteString(p.typeString)
+	}
+	if p.input != "" {
+		_, _ = builder.WriteString(` "`)
+		_, _ = builder.WriteString(p.input)
+		_, _ = builder.WriteString(`"`)
+	}
+	if p.err != nil {
+		_, _ = builder.WriteString(`: `)
+		_, _ = builder.WriteString(p.err.Error())
+	}
+	return builder.String()
+}
+
+// Unwrap returns the underlying error.
+func (p *ParseError) Unwrap() error { return p.err }
+
+// Input returns the input string that was attempted to be parsed.
+func (p *ParseError) Input() string {
+	return p.input
+}
+
+// *** PRIVATE ***
 
 func newErrNoProtoFiles(moduleID string) error {
 	return fmt.Errorf("module %q had no .proto files", moduleID)
