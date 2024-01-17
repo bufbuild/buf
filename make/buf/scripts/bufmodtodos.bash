1ff0e51811306a7bafae11a5c507b4fb54c4ef7c
Doria Keung <doria.keung@gmail.com>
2024-01-16 21:14:52 -0500

    Create a script to collect all bufmod TODOs

---
+++ make/buf/scripts/bufmodtodos.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="$(CDPATH= cd "$(dirname "${0}")/../../.." && pwd)"
+cd "${DIR}"
+
+BUFMOD_TODOS="$(git diff main bufmod | awk '
+  /^diff / {f="?"; next}
+  f=="?" {if (/^\+\+\+ /) f=substr($0, 7)"\n"; next}
+  /^@@/ {n=$3; sub(/,.*/,"",n); n=0+$3; next}
+  /^\+.*TODO/ {print f n ":" substr($0,2); f=""}
+  substr($0,1,1)~/[ +]/ {n++}')"
+
+FILENAME=""
+while read -r line; do
+  if [[ "${line}" =~ [a-z,0-9]+.go ]]; then
+    FILENAME="${line}"
+  elif [[ "${line}" =~ PACKAGES.md ]]; then
+    # ignore the contents of PACKAGES.md
+    FILENAME="no print"
+  else
+    if [[ "${FILENAME}" != "no print" ]]; then
+      LINENUMBER="${line%%:*}"
+      TODO="${line#*:}"
+      echo "${FILENAME}": "${LINENUMBER}" "${TODO#"${TODO%%[![:space:]]*}"}"
+    fi
+  fi
+done <<< "${BUFMOD_TODOS}"
