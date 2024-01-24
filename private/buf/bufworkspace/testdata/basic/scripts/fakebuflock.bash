b62f421d962d3206d2840884071913ce156d7a53
bufdev <bufdev-github@buf.build>
2024-01-24 18:33:22 -0500

    fix

--- private/buf/bufworkspace/testdata/basic/scripts/fakebuflock.bash
+++ private/buf/bufworkspace/testdata/basic/scripts/fakebuflock.bash
@@ -7,8 +7,8 @@ cd "${DIR}"
 
 DATE_DIGEST="$(buf-digest bsr/buf.testing/acme/date bsr/buf.testing/acme/extension | grep date | cut -f 2 -d ' ')"
 EXTENSION_DIGEST="$(buf-digest bsr/buf.testing/acme/date bsr/buf.testing/acme/extension | grep extension | cut -f 2 -d ' ')"
-DATE_COMMIT_ID="ffded0b4cf6b47cab74da08d291a3c2f"
-EXTENSION_COMMIT_ID="b8488077ea6d4f6d9562a337b98259c8"
+DATE_COMMIT_ID="ffded0b4-cf6b-47ca-b74d-a08d291a3c2f"
+EXTENSION_COMMIT_ID="b8488077-ea6d-4f6d-9562-a337b98259c8"
 
 rm -f workspacev1/finance/bond/proto/buf.lock
 cat <<EOF > workspacev1/finance/bond/proto/buf.lock
