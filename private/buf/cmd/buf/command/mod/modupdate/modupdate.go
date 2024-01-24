1d9fd7f10a2443bc1e28730211c3dbf050bbabaf
Edward McFarlane <emcfarlane@buf.build>
2024-01-24 12:25:52 -0500

    Fix InvalidRemoteErr panic
    On an unknown remote ModuleIdentity may be nil. Use the fallback logic
    to select the correct dependency and derive the related module
    identity. Fixes #2725

--- private/buf/cmd/buf/command/mod/modupdate/modupdate.go
+++ private/buf/cmd/buf/command/mod/modupdate/modupdate.go
@@ -215,9 +215,13 @@ func getDependencies(
 	if len(moduleConfig.Build.DependencyModuleReferences) == 0 {
 		return nil, nil
 	}
-	var remote string
+	var (
+		remote         string
+		moduleIdentity string
+	)
 	if moduleConfig.ModuleIdentity != nil && moduleConfig.ModuleIdentity.Remote() != "" {
 		remote = moduleConfig.ModuleIdentity.Remote()
+		moduleIdentity = moduleConfig.ModuleIdentity.IdentityString()
 	} else {
 		// At this point we know there's at least one dependency. If it's an unnamed module, select
 		// the right remote from the list of dependencies.
@@ -226,10 +230,11 @@ func getDependencies(
 			return nil, fmt.Errorf(`File %q has invalid "deps" references`, existingConfigFilePath)
 		}
 		remote = selectedRef.Remote()
+		moduleIdentity = selectedRef.IdentityString()
 		container.Logger().Debug(fmt.Sprintf(
 			`File %q does not specify the "name" field. Based on the dependency %q, it appears that you are using a BSR instance at %q. Did you mean to specify "name: %s/..." within %q?`,
 			existingConfigFilePath,
-			selectedRef.IdentityString(),
+			moduleIdentity,
 			remote,
 			remote,
 			existingConfigFilePath,
@@ -269,7 +274,7 @@ func getDependencies(
 	)
 	if err != nil {
 		if remote != bufconnect.DefaultRemote {
-			return nil, bufcli.NewInvalidRemoteError(err, remote, moduleConfig.ModuleIdentity.IdentityString())
+			return nil, bufcli.NewInvalidRemoteError(err, remote, moduleIdentity)
 		}
 		return nil, err
 	}
