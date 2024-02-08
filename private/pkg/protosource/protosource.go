2da3f36fec4e084055a5b22388a6793de8170432
Gilbert Wong <gilwong3105@gmail.com>
2024-02-08 12:28:39 -0800

    Update comment

--- private/pkg/protosource/protosource.go
+++ private/pkg/protosource/protosource.go
@@ -148,7 +148,7 @@ type OptionExtensionDescriptor interface {
 	// If no relevant location is found in source code info, this returns nil.
 	OptionExtensionLocation(extensionType protoreflect.ExtensionType, extraPath ...int32) Location
 
-	// PresentExtensionNumbers returns field numbers for all extensions/custom options only
+	// PresentExtensionNumbers returns field numbers for all extensions/custom options
 	// that have a set value on this descriptor.
 	PresentExtensionNumbers() []int32
 
