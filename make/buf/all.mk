c550e076e1b726a8a3f334953cf44ca04d406fef
bufdev <bufdev-github@buf.build>
2024-03-11 17:06:13 -0400

    commit

--- make/buf/all.mk
+++ make/buf/all.mk
@@ -5,6 +5,7 @@ GO_BINS := $(GO_BINS) \
 	cmd/protoc-gen-buf-breaking \
 	cmd/protoc-gen-buf-lint \
 	private/buf/bufwkt/cmd/wkt-go-data \
+	private/bufpkg/bufmodule/bufmoduleapi/cmd/buf-legacyfederation-go-data \
 	private/bufpkg/bufmodule/bufmoduletesting/cmd/buf-commit-id-from-dashless \
 	private/bufpkg/bufmodule/bufmoduletesting/cmd/buf-commit-id-to-dashless \
 	private/bufpkg/bufmodule/bufmoduletesting/cmd/buf-digest \
@@ -43,6 +44,8 @@ BUF_BREAKING_INPUT := .
 BUF_BREAKING_AGAINST_INPUT ?= .git\#branch=main
 BUF_FORMAT_INPUT := .
 
+LEGACY_FEDERATION_FILE_PATH ?=
+
 include make/go/bootstrap.mk
 include make/go/dep_buf.mk
 include make/go/dep_minisign.mk
@@ -69,13 +72,18 @@ bandeps: installbandeps
 postlonglint:: bandeps
 
 .PHONY: godata
-godata: installspdx-go-data installwkt-go-data $(PROTOC)
+godata: installspdx-go-data installwkt-go-data installbuf-legacyfederation-go-data $(PROTOC)
 	rm -rf private/gen/data/datawkt
 	mkdir -p private/gen/data/datawkt
 	wkt-go-data "$(CACHE_INCLUDE)" --package datawkt --protobuf-version "$(PROTOC_VERSION)" > private/gen/data/datawkt/datawkt.gen.go
 	rm -rf private/gen/data/dataspdx
 	mkdir -p private/gen/data/dataspdx
 	spdx-go-data --package dataspdx > private/gen/data/dataspdx/dataspdx.gen.go
+ifdef LEGACY_FEDERATION_FILE_PATH
+	rm -rf private/gen/data/datalegacyfederation
+	mkdir -p private/gen/data/datalegacyfederation
+	cat "$(LEGACY_FEDERATION_FILE_PATH)" | buf-legacyfederation-go-data --package datalegacyfederation > private/gen/data/datalegacyfederation/datalegacyfederation.gen.go
+endif
 
 prepostgenerate:: godata
 
