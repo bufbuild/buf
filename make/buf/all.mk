046fa218e4e1c78d1f561e99bbd7847c7df5d21a
bufdev <bufdev-github@buf.build>
2024-03-19 21:55:36 +0100

    Switch to install buf in Makefiles

--- make/buf/all.mk
+++ make/buf/all.mk
@@ -37,7 +37,7 @@ LICENSE_HEADER_YEAR_RANGE := 2020-2024
 LICENSE_HEADER_IGNORES := \/testdata enterprise
 PROTOVALIDATE_VERSION := v0.6.0
 # Comment out to use released buf
-#BUF_GO_INSTALL_PATH := ./cmd/buf
+BUF_GO_INSTALL_PATH := ./cmd/buf
 
 BUF_LINT_INPUT := .
 BUF_BREAKING_INPUT := .
