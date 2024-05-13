af052d49778b3b333f9e61d854901579d287fc95
Josh Humphries <2035234+jhump@users.noreply.github.com>
2024-05-13 10:06:02 -0400

    the JSON format rules should be in WIRE_JSON category

--- private/bufpkg/bufcheck/bufbreaking/internal/bufbreakingv2/vars.go
+++ private/bufpkg/bufcheck/bufbreaking/internal/bufbreakingv2/vars.go
@@ -99,6 +99,7 @@ var (
 		"ENUM_SAME_JSON_FORMAT": {
 			"FILE",
 			"PACKAGE",
+			"WIRE_JSON",
 		},
 		"ENUM_SAME_TYPE": {
 			"FILE",
@@ -280,6 +281,7 @@ var (
 		"MESSAGE_SAME_JSON_FORMAT": {
 			"FILE",
 			"PACKAGE",
+			"WIRE_JSON",
 		},
 		"MESSAGE_SAME_MESSAGE_SET_WIRE_FORMAT": {
 			"FILE",
