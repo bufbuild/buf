17bf4a8a4c5734bf641165ce5c8e3f3d1146c550
bufdev <bufdev-github@buf.build>
2024-05-27 12:08:58 -0400

    commit

--- make/go/dep_golangci_lint.mk
+++ make/go/dep_golangci_lint.mk
@@ -7,9 +7,9 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/golangci/golangci-lint/releases 20240320 checked 20240320
+# https://github.com/golangci/golangci-lint/releases 20240519 checked 20240524
 # Contrast golangci-lint configuration with the one in https://github.com/connectrpc/connect-go/blob/main/.golangci.yml when upgrading
-GOLANGCI_LINT_VERSION ?= v1.57.1
+GOLANGCI_LINT_VERSION ?= v1.58.2
 
 GOLANGCI_LINT := $(CACHE_VERSIONS)/golangci-lint/$(GOLANGCI_LINT_VERSION)
 $(GOLANGCI_LINT):
