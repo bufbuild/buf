f5899c3ccf2a46eb3eafd8056b021730826b9123
Oliver Sun <osun@buf.build>
2024-05-30 19:34:24 -0400

    untested

--- private/buf/bufcli/errors.go
+++ private/buf/bufcli/errors.go
@@ -48,10 +48,10 @@ func NewRepositoryNameAlreadyExistsError(name string) error {
 	return fmt.Errorf("a repository named %q already exists", name)
 }
 
-// NewTagOrDraftNameAlreadyExistsError informs the user that a tag
-// or draft with that name already exists.
-func NewTagOrDraftNameAlreadyExistsError(name string) error {
-	return fmt.Errorf("a tag or draft named %q already exists", name)
+// NewLabelNameAlreadyExistsError informs the user that a label
+// with that name already exists.
+func NewLabelNameAlreadyExistsError(name string) error {
+	return fmt.Errorf("a label named %q already exists", name)
 }
 
 // NewOrganizationNotFoundError informs the user that an organization with
