6663c603c4074c5daa30d40f703d9ace0e509d7f
Oliver Sun <osun@buf.build>
2024-07-09 20:37:05 -0400

    commit

--- private/buf/bufcli/flags_args.go
+++ private/buf/bufcli/flags_args.go
@@ -28,6 +28,8 @@ import (
 )
 
 const (
+	DefaultArchiveStatus = unarchivedArchiveStatus
+
 	inputHashtagFlagName      = "__hashtag__"
 	inputHashtagFlagShortName = "#"
 
@@ -168,7 +170,7 @@ func BindArchiveStatus(flagSet *pflag.FlagSet, addr *string, flagName string) {
 	flagSet.StringVar(
 		addr,
 		flagName,
-		unarchivedArchiveStatus,
+		DefaultArchiveStatus,
 		fmt.Sprintf(`The archive status of the labels listed. Must be one of %s`, stringutil.SliceToString(allArchiveStatusStrings)),
 	)
 }
