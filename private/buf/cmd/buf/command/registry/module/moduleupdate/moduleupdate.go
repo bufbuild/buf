042f7e3d0d1720da22cbfb8fe1979813aa5c641f
Oliver Sun <osun@buf.build>
2024-07-22 15:40:44 -0400

    typo

--- private/buf/cmd/buf/command/registry/module/moduleupdate/moduleupdate.go
+++ private/buf/cmd/buf/command/registry/module/moduleupdate/moduleupdate.go
@@ -139,7 +139,7 @@ func run(
 		}
 		return err
 	}
-	if _, err := fmt.Fprintln(container.Stdout(), "Module Updated."); err != nil {
+	if _, err := fmt.Fprintln(container.Stdout(), "Module updated."); err != nil {
 		return syserror.Wrap(err)
 	}
 	return nil
