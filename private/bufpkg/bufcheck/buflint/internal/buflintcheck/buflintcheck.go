a07beeca3078c41a2ac816eccb7b8f88952d8896
Doria Keung <doria.keung@gmail.com>
2024-08-01 12:37:27 -0400

    Fix implementation and add test.

--- private/bufpkg/bufcheck/buflint/internal/buflintcheck/buflintcheck.go
+++ private/bufpkg/bufcheck/buflint/internal/buflintcheck/buflintcheck.go
@@ -1030,26 +1030,34 @@ func checkServiceSuffix(add addFunc, service bufprotosource.Service, suffix stri
 }
 
 // CheckStablePackageNoImportUnstable is a check function.
-var CheckStablePackageNoImportUnstable = newFileCheckFunc(checkStablePackageNoImportUnstable)
+var CheckStablePackageNoImportUnstable = newFilesCheckFunc(checkStablePackageNoImportUnstable)
 
-func checkStablePackageNoImportUnstable(add addFunc, file bufprotosource.File) error {
-	packageVersion, ok := protoversion.NewPackageVersionForPackage(file.Package())
-	if !ok {
-		// No package, or no version on package - unstable to determine if stable.
-		return nil
+func checkStablePackageNoImportUnstable(add addFunc, files []bufprotosource.File) error {
+	filePathToFile, err := bufprotosource.FilePathToFile(files...)
+	if err != nil {
+		return err
 	}
-	if packageVersion.StabilityLevel() != protoversion.StabilityLevelStable {
-		// If package is not stable, no failure.
-		return nil
-	}
-	// Package is stable. Check imports.
-	for _, fileImport := range file.FileImports() {
-		packageVersion, ok := protoversion.NewPackageVersionForPackage(fileImport.File().Package())
+	for _, file := range files {
+		packageVersion, ok := protoversion.NewPackageVersionForPackage(file.Package())
 		if !ok {
+			// No package, or no version on package - unstable to determine if stable.
 			continue
 		}
 		if packageVersion.StabilityLevel() != protoversion.StabilityLevelStable {
-			add(fileImport, fileImport.Location(), nil, `This file has package %q that represents a stable version, but it imports %q, which has package %q that represents an unstable version. Stable packages should not depend on unstable packages.`, file.Package(), fileImport.Import(), fileImport.File().Package())
+			// If package is not stable, no failure.
+			continue
+		}
+		// Package is stable. Check imports.
+		for _, fileImport := range file.FileImports() {
+			if importedFile, ok := filePathToFile[fileImport.Import()]; ok {
+				importedFilePackageVersion, ok := protoversion.NewPackageVersionForPackage(importedFile.Package())
+				if !ok {
+					continue
+				}
+				if importedFilePackageVersion.StabilityLevel() != protoversion.StabilityLevelStable {
+					add(importedFile, fileImport.Location(), nil, `This file is in stable package %q, so it should not depend on %q from unstable package %q.`, file.Package(), fileImport.Import(), importedFile.Package())
+				}
+			}
 		}
 	}
 	return nil
