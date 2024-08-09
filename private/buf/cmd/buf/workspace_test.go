8d9eb427dd8dd73f30b200425a5a033fcf65e170
Oliver Sun <osun@buf.build>
2024-08-08 20:24:56 -0400

    workspace test

--- private/buf/cmd/buf/workspace_test.go
+++ private/buf/cmd/buf/workspace_test.go
@@ -1009,6 +1009,11 @@ func TestWorkspaceBreakingFail(t *testing.T) {
 	)
 }
 
+func TestWorkspaceDuplicateDirPathSuccess(t *testing.T) {
+	t.Parallel()
+	testRunStdout(t, nil, 0, ``, "build", filepath.Join("testdata", "workspace", "success", "duplicate_dir_path"))
+}
+
 func TestWorkspaceDuplicateFail(t *testing.T) {
 	t.Parallel()
 	// The workspace includes multiple images that define the same file.
