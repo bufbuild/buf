239982d516a4b2690a5ff13f195509f93f2fff3a
bufdev <bufdev-github@buf.build>
2024-08-09 17:50:32 -0400

    other helpers

--- private/buf/bufcheckserver/internal/bufcheckserverutil/lint.go
+++ private/buf/bufcheckserver/internal/bufcheckserverutil/lint.go
@@ -77,9 +77,195 @@ func NewLintFileRuleHandler(
 	)
 }
 
+// NewLintFileImportRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each FileImport within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintFileImportRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		fileImport bufprotosource.FileImport,
+	) error,
+) check.RuleHandler {
+	return NewLintFileRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			file bufprotosource.File,
+		) error {
+			for _, fileImport := range file.FileImports() {
+				if err := f(responseWriter, request, fileImport); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	)
+}
+
+// NewLintEnumRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each Enum within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintEnumRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		enum bufprotosource.Enum,
+	) error,
+) check.RuleHandler {
+	return NewLintFileRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			file bufprotosource.File,
+		) error {
+			return bufprotosource.ForEachEnum(
+				func(enum bufprotosource.Enum) error {
+					return f(responseWriter, request, enum)
+				},
+				file,
+			)
+		},
+	)
+}
+
+// NewLintEnumValueRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each EnumValue within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintEnumValueRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		enumValue bufprotosource.EnumValue,
+	) error,
+) check.RuleHandler {
+	return NewLintEnumRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			enum bufprotosource.Enum,
+		) error {
+			for _, enumValue := range enum.Values() {
+				if err := f(responseWriter, request, enumValue); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	)
+}
+
+// NewLintMessageRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each Message within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintMessageRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		message bufprotosource.Message,
+	) error,
+) check.RuleHandler {
+	return NewLintFileRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			file bufprotosource.File,
+		) error {
+			return bufprotosource.ForEachMessage(
+				func(message bufprotosource.Message) error {
+					return f(responseWriter, request, message)
+				},
+				file,
+			)
+		},
+	)
+}
+
+// NewLintFieldRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each Field within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintFieldRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		field bufprotosource.Field,
+	) error,
+) check.RuleHandler {
+	return NewLintFileRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			file bufprotosource.File,
+		) error {
+			if err := bufprotosource.ForEachMessage(
+				func(message bufprotosource.Message) error {
+					for _, field := range message.Fields() {
+						if err := f(responseWriter, request, field); err != nil {
+							return err
+						}
+					}
+					for _, field := range message.Extensions() {
+						if err := f(responseWriter, request, field); err != nil {
+							return err
+						}
+					}
+					return nil
+				},
+				file,
+			); err != nil {
+				return err
+			}
+			for _, field := range file.Extensions() {
+				if err := f(responseWriter, request, field); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	)
+}
+
+// NewLintOneofRuleHandler returns a new check.RuleHandler for the given function.
+//
+// The function will be called for each Oneof within each File in the request.
+//
+// Files that are imports are skipped.
+func NewLintOneofRuleHandler(
+	f func(
+		responseWriter ResponseWriter,
+		request Request,
+		oneof bufprotosource.Oneof,
+	) error,
+) check.RuleHandler {
+	return NewLintMessageRuleHandler(
+		func(
+			responseWriter ResponseWriter,
+			request Request,
+			message bufprotosource.Message,
+		) error {
+			for _, oneof := range message.Oneofs() {
+				if err := f(responseWriter, request, oneof); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	)
+}
+
 // NewLintServiceRuleHandler returns a new check.RuleHandler for the given function.
 //
-// The function will be called for each service within each File in the request.
+// The function will be called for each Service within each File in the request.
 //
 // Files that are imports are skipped.
 func NewLintServiceRuleHandler(
@@ -107,7 +293,7 @@ func NewLintServiceRuleHandler(
 
 // NewLintMethodRuleHandler returns a new check.RuleHandler for the given function.
 //
-// The function will be called for each method within each File in the request.
+// The function will be called for each Method within each File in the request.
 //
 // Files that are imports are skipped.
 func NewLintMethodRuleHandler(
