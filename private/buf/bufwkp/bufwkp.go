c7c0303d35b2924dfea0f99d0ca8d81624d662f3
bufdev <bufdev-github@buf.build>
2024-08-09 15:08:59 -0400

    commit

--- private/buf/bufwkp/bufwkp.go
+++ private/buf/bufwkp/bufwkp.go
@@ -17,8 +17,13 @@ package bufwkp
 import (
 	"context"
 
+	"github.com/bufbuild/buf/private/bufpkg/bufmodule"
 	"github.com/bufbuild/buf/private/bufpkg/bufprotosource"
+	"github.com/bufbuild/buf/private/pkg/slicesext"
 	"github.com/bufbuild/bufplugin-go/check"
+	"github.com/gofrs/uuid/v5"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/types/descriptorpb"
 )
 
 var (
@@ -41,6 +46,9 @@ var (
 	}
 )
 
+type protosourceFilesContextKey struct{}
+type againstProtosourceFilesContextKey struct{}
+
 func handleV2ServicePascalCase(
 	_ context.Context,
 	responseWriter check.ResponseWriter,
@@ -49,11 +57,13 @@ func handleV2ServicePascalCase(
 	return nil
 }
 
-func newFilesRuleHandler(
+func newRuleHandler(
 	f func(
+		ctx context.Context,
 		responseWriter check.ResponseWriter,
+		request check.Request,
 		files []bufprotosource.File,
-		options check.Options,
+		againstFiles []bufprotosource.File,
 	) error,
 ) check.RuleHandler {
 	return check.RuleHandlerFunc(
@@ -62,7 +72,15 @@ func newFilesRuleHandler(
 			responseWriter check.ResponseWriter,
 			request check.Request,
 		) error {
-			return nil
+			return f(
+				ctx,
+				responseWriter,
+				request,
+				// Is this OK with nil?
+				ctx.Value(protosourceFilesContextKey{}).([]bufprotosource.File),
+				// Is this OK with nil?
+				ctx.Value(againstProtosourceFilesContextKey{}).([]bufprotosource.File),
+			)
 		},
 	)
 }
@@ -71,5 +89,66 @@ func before(
 	ctx context.Context,
 	request check.Request,
 ) (context.Context, check.Request, error) {
+	protosourceFiles, err := protosourceFilesForFiles(ctx, request.Files())
+	if err != nil {
+		return nil, nil, err
+	}
+	againstProtosourceFiles, err := protosourceFilesForFiles(ctx, request.Files())
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(protosourceFiles) > 0 {
+		ctx = context.WithValue(ctx, protosourceFilesContextKey{}, protosourceFiles)
+	}
+	if len(againstProtosourceFiles) > 0 {
+		ctx = context.WithValue(ctx, againstProtosourceFilesContextKey{}, againstProtosourceFiles)
+	}
 	return ctx, request, nil
+}
+
+func protosourceFilesForFiles(ctx context.Context, files []check.File) ([]bufprotosource.File, error) {
+	if len(files) == 0 {
+		return nil, nil
+	}
+	resolver, err := newResolver(files)
+	if err != nil {
+		return nil, err
+	}
+	return bufprotosource.NewFiles(ctx, slicesext.Map(files, newInputFile), resolver)
+}
+
+func newResolver(files []check.File) (protodesc.Resolver, error) {
+	return protodesc.NewFiles(
+		&descriptorpb.FileDescriptorSet{
+			File: slicesext.Map(files, check.File.FileDescriptorProto),
+		},
+	)
+}
+
+type inputFile struct {
+	check.File
+}
+
+func newInputFile(file check.File) *inputFile {
+	return &inputFile{
+		File: file,
+	}
+}
+
+func (i *inputFile) Path() string {
+	return i.File.FileDescriptorProto().GetName()
+}
+
+// TODO: We will need to reconcile this on the client-side as right now we rely on ExternalPath
+// being passed end-to-end.
+func (i *inputFile) ExternalPath() string {
+	return i.Path()
+}
+
+func (i *inputFile) ModuleFullName() bufmodule.ModuleFullName {
+	return nil
+}
+
+func (i *inputFile) CommitID() uuid.UUID {
+	return uuid.Nil
 }
