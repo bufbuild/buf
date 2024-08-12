5a4fa1cc5aa940cb169edb19dd081ac92ab8e6ef
bufdev <bufdev-github@buf.build>
2024-08-12 15:19:17 -0400

    commit

--- private/buf/bufcheckserver/internal/bufcheckserveropt/bufcheckserveropt.go
+++ private/buf/bufcheckserver/internal/bufcheckserveropt/bufcheckserveropt.go
@@ -14,9 +14,16 @@
 
 // TODO: This will probably go into some common bufcheck package that includes
 // both the client and server. We will need to set options on the client side.
+//
+// Note that we don't expose OptionsBuilder for the server-side rules, instead we rely
+// on the static functions, as we want to move our rules to be as native to bufplugin-go
+// as possible. Instead of i.e. attaching an Options struct to bufcheckserverutil.Requests,
+// we have individual rules go through the direct reading of check.Options using
+// the static functions below.
 package bufcheckserveropt
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/bufbuild/bufplugin-go/check"
@@ -28,10 +35,66 @@ const (
 	rpcAllowGoogleProtobufEmptyRequestsKey  = "rpc_allow_google_protobuf_empty_requests"
 	rpcAllowGoogleProtobufEmptyResponsesKey = "rpc_allow_google_protobuf_empty_responses"
 	serviceSuffixKey                        = "service_suffix"
+	commentExcludesKey                      = "comment_excludes"
 
 	defaultEnumZeroValueSuffix = "_UNSPECIFIED"
 	defaultServiceSuffix       = "Service"
 )
+
+// OptionsBuilder builds check.Options for clients.
+//
+// These can then be sent over the wire to servers.
+type OptionsBuilder struct {
+	EnumZeroValueSuffix                  string
+	RPCAllowSameRequestResponse          bool
+	RPCAllowGoogleProtobufEmptyRequests  bool
+	RPCAllowGoogleProtobufEmptyResponses bool
+	ServiceSuffix                        string
+	// CommentExcludes are lines of comments that should be excluded for the COMMENT.* Rules.
+	//
+	// If a comment line starts with one of these excludes, it is not considered an actual comment.
+	//
+	// Right now, this should just be []string{"buf:lint:ignore"}, however we do this as a proper option
+	// to maintain the client/server split we want; the server (ie the Rules) should not have the lint comment
+	// ignore strings as part of their logic, all lint comment ignore logic is a client-side concern. However,
+	// it is concievable that a COMMENT.* Rule might want to say "I don't want to consider this generic
+	// line to be a comment", which is exclusive of the lint comment ignore logic. We could even potentially
+	// give users the ability to configure things to ignore as part of their buf.yaml configuration. So,
+	// this feels OK to expose here.
+	//
+	// In practice, right now, the client-side should just set this to be []string{"buf:lint:ignore"}.
+	//
+	// All elements must be non-empty.
+	CommentExcludes []string
+}
+
+// Build builds a check.Options.
+func (o OptionsBuilder) Build() (check.Options, error) {
+	keyToValue := make(map[string][]byte, 5)
+	if value := o.EnumZeroValueSuffix; len(value) > 0 {
+		keyToValue[enumZeroValueSuffixKey] = []byte(value)
+	}
+	if o.RPCAllowSameRequestResponse {
+		keyToValue[rpcAllowSameRequestResponseKey] = []byte("true")
+	}
+	if o.RPCAllowGoogleProtobufEmptyRequests {
+		keyToValue[rpcAllowGoogleProtobufEmptyRequestsKey] = []byte("true")
+	}
+	if o.RPCAllowGoogleProtobufEmptyResponses {
+		keyToValue[rpcAllowGoogleProtobufEmptyResponsesKey] = []byte("true")
+	}
+	if value := o.ServiceSuffix; len(value) > 0 {
+		keyToValue[serviceSuffixKey] = []byte(value)
+	}
+	if value := o.CommentExcludes; len(value) > 0 {
+		optionValue, err := toStringSliceValue(value)
+		if err != nil {
+			return nil, err
+		}
+		keyToValue[commentExcludesKey] = optionValue
+	}
+	return check.NewOptions(keyToValue)
+}
 
 // GetEnumZeroValueSuffix gets the enum zero-value suffix.
 //
@@ -76,6 +139,15 @@ func GetServiceSuffix(options check.Options) string {
 	return defaultServiceSuffix
 }
 
+// CommentExcludes are lines of comments that should be excluded for the COMMENT.* Rules.
+//
+// If a comment line starts with one of these excludes, it is not considered an actual comment.
+//
+// The returned slice is guaranteed to have only non-empty elements.
+func GetCommentExcludes(options check.Options) ([]string, error) {
+	return getStringSliceValue(options, commentExcludesKey)
+}
+
 // *** PRIVATE ***
 
 func getBoolValue(options check.Options, key string) (bool, error) {
@@ -87,4 +159,37 @@ func getBoolValue(options check.Options, key string) (bool, error) {
 	default:
 		return false, fmt.Errorf("invalid value for option %s: %q", key, value)
 	}
+}
+
+func getStringSliceValue(options check.Options, key string) ([]string, error) {
+	value := options.Get(key)
+	if len(value) == 0 {
+		return nil, nil
+	}
+	var s []string
+	if err := json.Unmarshal(value, &s); err != nil {
+		return nil, fmt.Errorf("invalid value for option %s: %q: %w", key, string(value), err)
+	}
+	for _, e := range s {
+		if len(e) == 0 {
+			return nil, fmt.Errorf("invalid value for option %s: %q: all elements must be non-empty", key, string(value))
+		}
+	}
+	return s, nil
+}
+
+func toStringSliceValue(s []string) ([]byte, error) {
+	if len(s) == 0 {
+		return nil, nil
+	}
+	for _, e := range s {
+		if len(e) == 0 {
+			return nil, fmt.Errorf("could not marshal %v into a value to be sent as a check.Options value: all elements must be non-empty", s)
+		}
+	}
+	data, err := json.Marshal(s)
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal %v into a value to be sent as a check.Options value: %w", s, err)
+	}
+	return data, nil
 }
