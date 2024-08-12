8a10aaa7d3815693c3b859e511ee38692ead4246
bufdev <bufdev-github@buf.build>
2024-08-12 14:50:19 -0400

    commit

--- private/buf/bufcheckserver/internal/bufcheckserveropt/bufcheckserveropt.go
+++ private/buf/bufcheckserver/internal/bufcheckserveropt/bufcheckserveropt.go
@@ -16,7 +16,11 @@
 // both the client and server. We will need to set options on the client side.
 package bufcheckserveropt
 
-import "github.com/bufbuild/bufplugin-go/check"
+import (
+	"fmt"
+
+	"github.com/bufbuild/bufplugin-go/check"
+)
 
 const (
 	enumZeroValueSuffixKey                  = "enum_zero_value_suffix"
@@ -30,6 +34,8 @@ const (
 )
 
 // GetEnumZeroValueSuffix gets the enum zero-value suffix.
+//
+// Returns the default suffix if the option is not set.
 func GetEnumZeroValueSuffix(options check.Options) string {
 	if value := options.Get(enumZeroValueSuffixKey); len(value) > 0 {
 		return string(value)
@@ -38,26 +44,47 @@ func GetEnumZeroValueSuffix(options check.Options) string {
 }
 
 // GetRPCAllowSameRequestResponse returns true if the rpc_allow_same_request_response option is set to true.
-func GetRPCAllowSameRequestResponse(options check.Options) bool {
-	return string(options.Get(rpcAllowSameRequestResponseKey)) == "true"
+//
+// Returns error if the value was unrecognized.
+func GetRPCAllowSameRequestResponse(options check.Options) (bool, error) {
+	return getBoolValue(options, rpcAllowSameRequestResponseKey)
 }
 
 // GetRPCAllowGoogleProtobufEmptyRequests returns true if the rpc_allow_google_protobuf_empty_requests
 // option is set to true.
-func GetRPCAllowGoogleProtobufEmptyRequests(options check.Options) bool {
-	return string(options.Get(rpcAllowGoogleProtobufEmptyRequestsKey)) == "true"
+//
+// Returns error if the value was unrecognized.
+func GetRPCAllowGoogleProtobufEmptyRequests(options check.Options) (bool, error) {
+	return getBoolValue(options, rpcAllowGoogleProtobufEmptyRequestsKey)
 }
 
 // GetRPCAllowGoogleProtobufEmptyResponses returns true if the rpc_allow_google_protobuf_empty_responses
 // option is set to true.
-func GetRPCAllowGoogleProtobufEmptyResponses(options check.Options) bool {
-	return string(options.Get(rpcAllowGoogleProtobufEmptyResponsesKey)) == "true"
+//
+// Returns error if the value was unrecognized.
+func GetRPCAllowGoogleProtobufEmptyResponses(options check.Options) (bool, error) {
+	return getBoolValue(options, rpcAllowGoogleProtobufEmptyResponsesKey)
 }
 
 // GetServiceSuffix gets the service suffix.
+//
+// Returns the default suffix if the option is not set.
 func GetServiceSuffix(options check.Options) string {
 	if value := options.Get(serviceSuffixKey); len(value) > 0 {
 		return string(value)
 	}
 	return defaultServiceSuffix
+}
+
+// *** PRIVATE ***
+
+func getBoolValue(options check.Options, key string) (bool, error) {
+	switch value := string(options.Get(key)); value {
+	case "true":
+		return true, nil
+	case "false", "":
+		return false, nil
+	default:
+		return false, fmt.Errorf("invalid value for option %s: %q", key, value)
+	}
 }
