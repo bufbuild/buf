b760dee8ecb3f990550e7a6273110d82ec725a8d
bufdev <bufdev-github@buf.build>
2024-08-13 13:43:57 -0400

    Try updating golangci-lint to pinned commit on go 1.23 branch

--- make/go/dep_golangci_lint.mk
+++ make/go/dep_golangci_lint.mk
@@ -9,7 +9,8 @@ $(call _assert_var,CACHE_BIN)
 # Settable
 # https://github.com/golangci/golangci-lint/releases 20240609 checked 20240813
 # Contrast golangci-lint configuration with the one in https://github.com/connectrpc/connect-go/blob/main/.golangci.yml when upgrading
-GOLANGCI_LINT_VERSION ?= v1.59.1
+#GOLANGCI_LINT_VERSION ?= v1.59.1
+GOLANGCI_LINT_VERSION ?= 45f4e7e937880b0024bbec20f3bda14dcd0c5f91
 
 GOLANGCI_LINT := $(CACHE_VERSIONS)/golangci-lint/$(GOLANGCI_LINT_VERSION)
 $(GOLANGCI_LINT):
