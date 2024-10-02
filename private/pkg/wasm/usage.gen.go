fa269875e42e4ebedb60a6803e205ddeb13003fd
Edward McFarlane <emcfarlane@buf.build>
2024-10-01 21:03:47 -0400

    Move wasm pkg

--- private/pkg/wasm/usage.gen.go
+++ private/pkg/wasm/usage.gen.go
@@ -14,6 +14,6 @@
 
 // Generated. DO NOT EDIT.
 
-package bufwasm
+package wasm
 
 import _ "github.com/bufbuild/buf/private/usage"
--- private/pkg/wasm/wasm.go
+++ private/pkg/wasm/wasm.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package bufwasm provides a Wasm runtime for plugins.
-package bufwasm
+// Package wasm provides a Wasm runtime for plugins.
+package wasm
 
 import (
 	"context"
