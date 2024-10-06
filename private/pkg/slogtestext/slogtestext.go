a706100c824fb7035e17d052ce6bcd310c0308e2
bufdev <bufdev-github@buf.build>
2024-10-06 09:38:13 -0400

    Rename slogtest to slogtestext

--- private/pkg/slogtestext/slogtestext.go
+++ private/pkg/slogtestext/slogtestext.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package slogtest provides Loggers for testing.
-package slogtest
+// Package slogtestext provides Loggers for testing.
+package slogtestext
 
 import (
 	"log/slog"
