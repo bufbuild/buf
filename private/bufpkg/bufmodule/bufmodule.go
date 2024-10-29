4b6c4840d7e6025cdf465901dfa61b3db2aa5bbc
Edward McFarlane <emcfarlane@buf.build>
2024-10-29 16:47:45 -0400

    Drop pkg comments

--- private/bufpkg/bufmodule/bufmodule.go
+++ private/bufpkg/bufmodule/bufmodule.go
@@ -12,5 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package bufmodule implements types and functions for Buf modules.
 package bufmodule
--- private/bufpkg/bufplugin/bufplugin.go
+++ private/bufpkg/bufplugin/bufplugin.go
@@ -12,5 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package bufplugin implements types and functions for Buf plugins.
 package bufplugin
