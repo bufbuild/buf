39043a586cdd7abf237dafe93551cbfe1877bf69
Edward McFarlane <emcfarlane@buf.build>
2025-01-14 13:04:47 -0500

    Upgrade deps

--- make/go/dep_protoc_gen_go.mk
+++ make/go/dep_protoc_gen_go.mk
@@ -7,8 +7,8 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/protocolbuffers/protobuf-go/releases 20241223 checked 20241227
-PROTOC_GEN_GO_VERSION ?= v1.36.1
+# https://github.com/protocolbuffers/protobuf-go/releases 20250107 checked 20250113
+PROTOC_GEN_GO_VERSION ?= v1.36.2
 
 GO_GET_PKGS := $(GO_GET_PKGS) \
 	google.golang.org/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
