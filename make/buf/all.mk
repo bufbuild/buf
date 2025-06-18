49857a77a61567a29c51bff8626924ff02398373
Doria Keung <doria.keung@gmail.com>
2025-06-18 12:47:08 -0400

    Add to updategoversion make target

--- make/buf/all.mk
+++ make/buf/all.mk
@@ -190,6 +190,7 @@ endif
 	$(SED_I) "s/golang:1\.[0-9][0-9]*/golang:$(GOVERSION)/g" $(shell git-ls-files-unstaged | grep Dockerfile)
 	$(SED_I) "s/golang:1\.[0-9][0-9]*/golang:$(GOVERSION)/g" $(shell git-ls-files-unstaged | grep \.mk$)
 	$(SED_I) "s/go-version: '1\.[0-9][0-9].x'/go-version: '$(GOVERSION).x'/g" $(shell git-ls-files-unstaged | grep \.github\/workflows | grep -v previous.yaml)
+	$(MAKE) checkandupdateprecommithooks
 
 .PHONY: bufimageutilupdateexpectations
 bufimageutilupdateexpectations:
