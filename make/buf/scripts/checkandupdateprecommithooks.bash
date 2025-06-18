d8dd346a85e487e2485a729185e3c86c7b961396
Doria Keung <doria.keung@gmail.com>
2025-06-18 12:45:07 -0400

    Adjust script to add patch version when not present in go.mod

--- make/buf/scripts/checkandupdateprecommithooks.bash
+++ make/buf/scripts/checkandupdateprecommithooks.bash
@@ -6,13 +6,18 @@ DIR="$(CDPATH= cd "$(dirname "${0}")/../../.." && pwd)"
 cd "${DIR}"
 
 GO_VERSION=$(grep '^go ' go.mod | awk '{print $2}')
+# If the version in go.mod does not include the patch version, e.g. go 1.24, for the
+# .pre-commit-hook.yaml configuration, we'll need to add a 0.
+if [[ ! "${GO_VERSION}" =~ '^([0-9]+\.){3}$' ]]; then
+  GO_VERSION="${GO_VERSION}.0"
+fi
 
 function lesser_version() {
   echo -e "$1\n$2" | sort -V | head -n 1
 }
 
 function update_pre_commit_hooks() {
-  yq .[].language_version="${GO_VERSION}" .pre-commit-hooks.yaml > .pre-commit-hooks.yaml.tmp
+  yq .[].language_version=\"${GO_VERSION}\" .pre-commit-hooks.yaml > .pre-commit-hooks.yaml.tmp
   mv .pre-commit-hooks.yaml.tmp .pre-commit-hooks.yaml
 }
 
