f37417b2dd73dfc2186800e9daf04832d0acd923
Edward McFarlane <emcfarlane@buf.build>
2025-06-24 10:38:09 -0400

    Update policy data provider to use PolicyConfig

--- private/bufpkg/bufpolicy/bufpolicyapi/policy_data_provider.go
+++ private/bufpkg/bufpolicy/bufpolicyapi/policy_data_provider.go
@@ -19,11 +19,13 @@ import (
 	"log/slog"
 
 	policyv1beta1 "buf.build/gen/go/bufbuild/registry/protocolbuffers/go/buf/registry/policy/v1beta1"
+	"buf.build/go/bufplugin/option"
+	"buf.build/go/standard/xslices"
 	"connectrpc.com/connect"
+	"github.com/bufbuild/buf/private/bufpkg/bufconfig"
 	"github.com/bufbuild/buf/private/bufpkg/bufparse"
 	"github.com/bufbuild/buf/private/bufpkg/bufpolicy"
 	"github.com/bufbuild/buf/private/bufpkg/bufregistryapi/bufregistryapipolicy"
-	"github.com/bufbuild/buf/private/pkg/slicesext"
 	"github.com/bufbuild/buf/private/pkg/syserror"
 	"github.com/bufbuild/buf/private/pkg/uuidutil"
 	"github.com/google/uuid"
@@ -80,13 +82,13 @@ func (p *policyDataProvider) GetPolicyDatasForPolicyKeys(
 		return nil, err
 	}
 
-	registryToIndexedPolicyKeys := slicesext.ToIndexedValuesMap(
+	registryToIndexedPolicyKeys := xslices.ToIndexedValuesMap(
 		policyKeys,
 		func(policyKey bufpolicy.PolicyKey) string {
 			return policyKey.FullName().Registry()
 		},
 	)
-	indexedPolicyDatas := make([]slicesext.Indexed[bufpolicy.PolicyData], 0, len(policyKeys))
+	indexedPolicyDatas := make([]xslices.Indexed[bufpolicy.PolicyData], 0, len(policyKeys))
 	for registry, indexedPolicyKeys := range registryToIndexedPolicyKeys {
 		indexedRegistryPolicyDatas, err := p.getIndexedPolicyDatasForRegistryAndIndexedPolicyKeys(
 			ctx,
@@ -98,15 +100,15 @@ func (p *policyDataProvider) GetPolicyDatasForPolicyKeys(
 		}
 		indexedPolicyDatas = append(indexedPolicyDatas, indexedRegistryPolicyDatas...)
 	}
-	return slicesext.IndexedToSortedValues(indexedPolicyDatas), nil
+	return xslices.IndexedToSortedValues(indexedPolicyDatas), nil
 }
 
 func (p *policyDataProvider) getIndexedPolicyDatasForRegistryAndIndexedPolicyKeys(
 	ctx context.Context,
 	registry string,
-	indexedPolicyKeys []slicesext.Indexed[bufpolicy.PolicyKey],
-) ([]slicesext.Indexed[bufpolicy.PolicyData], error) {
-	values := slicesext.Map(indexedPolicyKeys, func(indexedPolicyKey slicesext.Indexed[bufpolicy.PolicyKey]) *policyv1beta1.DownloadRequest_Value {
+	indexedPolicyKeys []xslices.Indexed[bufpolicy.PolicyKey],
+) ([]xslices.Indexed[bufpolicy.PolicyData], error) {
+	values := xslices.Map(indexedPolicyKeys, func(indexedPolicyKey xslices.Indexed[bufpolicy.PolicyKey]) *policyv1beta1.DownloadRequest_Value {
 		return &policyv1beta1.DownloadRequest_Value{
 			ResourceRef: &policyv1beta1.ResourceRef{
 				Value: &policyv1beta1.ResourceRef_Id{
@@ -130,9 +132,9 @@ func (p *policyDataProvider) getIndexedPolicyDatasForRegistryAndIndexedPolicyKey
 		return nil, syserror.New("did not get the expected number of policy datas")
 	}
 
-	commitIDToIndexedPolicyKeys, err := slicesext.ToUniqueValuesMapError(
+	commitIDToIndexedPolicyKeys, err := xslices.ToUniqueValuesMapError(
 		indexedPolicyKeys,
-		func(indexedPolicyKey slicesext.Indexed[bufpolicy.PolicyKey]) (uuid.UUID, error) {
+		func(indexedPolicyKey xslices.Indexed[bufpolicy.PolicyKey]) (uuid.UUID, error) {
 			return indexedPolicyKey.Value.CommitID(), nil
 		},
 	)
@@ -140,7 +142,7 @@ func (p *policyDataProvider) getIndexedPolicyDatasForRegistryAndIndexedPolicyKey
 		return nil, err
 	}
 
-	indexedPolicyDatas := make([]slicesext.Indexed[bufpolicy.PolicyData], 0, len(indexedPolicyKeys))
+	indexedPolicyDatas := make([]xslices.Indexed[bufpolicy.PolicyData], 0, len(indexedPolicyKeys))
 	for _, policyContent := range policyContents {
 		commitID, err := uuid.Parse(policyContent.Commit.Id)
 		if err != nil {
@@ -150,20 +152,144 @@ func (p *policyDataProvider) getIndexedPolicyDatasForRegistryAndIndexedPolicyKey
 		if !ok {
 			return nil, syserror.Newf("did not get policy key from store with commitID %q", commitID)
 		}
-		getData := func() ([]byte, error) {
-			return policyContent.Content, nil
+		getContent := func() (bufpolicy.PolicyConfig, error) {
+			return newPolicyConfig(registry, policyContent.GetConfig())
 		}
-		policyData, err := bufpolicy.NewPolicyData(ctx, indexedPolicyKey.Value, getData)
+		policyData, err := bufpolicy.NewPolicyData(ctx, indexedPolicyKey.Value, getContent)
 		if err != nil {
 			return nil, err
 		}
 		indexedPolicyDatas = append(
 			indexedPolicyDatas,
-			slicesext.Indexed[bufpolicy.PolicyData]{
+			xslices.Indexed[bufpolicy.PolicyData]{
 				Value: policyData,
 				Index: indexedPolicyKey.Index,
 			},
 		)
 	}
 	return indexedPolicyDatas, nil
+}
+
+// policyConfig implements bufpolicy.PolicyConfig.
+type policyConfig struct {
+	lintConfig     bufconfig.LintConfig
+	breakingConfig bufconfig.BreakingConfig
+	pluginConfigs  []bufconfig.PluginConfig
+}
+
+func newPolicyConfig(
+	registry string,
+	policyConfigV1Beta1 *policyv1beta1.PolicyConfig,
+) (*policyConfig, error) {
+	lintConfig, err := getLintConfigForV1Beta1LintConfig(policyConfigV1Beta1.Lint)
+	if err != nil {
+		return nil, err
+	}
+	breakingConfig, err := getBreakingConfigForV1Beta1BreakingConfig(policyConfigV1Beta1.Breaking)
+	if err != nil {
+		return nil, err
+	}
+	pluginConfigs, err := xslices.MapError(
+		policyConfigV1Beta1.Plugins,
+		func(pluginConfigV1Beta1 *policyv1beta1.PolicyConfig_CheckPluginConfig) (bufconfig.PluginConfig, error) {
+			return getPluginConfigForV1Beta1PluginConfig(registry, pluginConfigV1Beta1)
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &policyConfig{
+		lintConfig:     lintConfig,
+		breakingConfig: breakingConfig,
+		pluginConfigs:  pluginConfigs,
+	}, nil
+}
+
+// LintConfig returns the LintConfig for the File.
+func (p *policyConfig) LintConfig() bufconfig.LintConfig {
+	return p.lintConfig
+}
+
+// BreakingConfig returns the BreakingConfig for the File.
+func (p *policyConfig) BreakingConfig() bufconfig.BreakingConfig {
+	return p.breakingConfig
+}
+
+// PluginConfigs returns the PluginConfigs for the File.
+func (p *policyConfig) PluginConfigs() []bufconfig.PluginConfig {
+	return p.pluginConfigs
+}
+
+func getLintConfigForV1Beta1LintConfig(
+	lintConfigV1Beta1 *policyv1beta1.PolicyConfig_LintConfig,
+) (bufconfig.LintConfig, error) {
+	checkConfig, err := bufconfig.NewEnabledCheckConfig(
+		bufconfig.FileVersionV2,
+		lintConfigV1Beta1.Use,
+		lintConfigV1Beta1.Except,
+		nil,
+		nil,
+		false,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return bufconfig.NewLintConfig(
+		checkConfig,
+		lintConfigV1Beta1.EnumZeroValueSuffix,
+		lintConfigV1Beta1.RpcAllowSameRequestResponse,
+		lintConfigV1Beta1.RpcAllowGoogleProtobufEmptyRequests,
+		lintConfigV1Beta1.RpcAllowGoogleProtobufEmptyResponses,
+		lintConfigV1Beta1.ServiceSuffix,
+		false, // Comment ignores are not allowed in Policy files.
+	), nil
+}
+
+func getBreakingConfigForV1Beta1BreakingConfig(
+	breakingConfigV1Beta1 *policyv1beta1.PolicyConfig_BreakingConfig,
+) (bufconfig.BreakingConfig, error) {
+	checkConfig, err := bufconfig.NewEnabledCheckConfig(
+		bufconfig.FileVersionV2,
+		breakingConfigV1Beta1.Use,
+		breakingConfigV1Beta1.Except,
+		nil,
+		nil,
+		false,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return bufconfig.NewBreakingConfig(
+		checkConfig,
+		breakingConfigV1Beta1.IgnoreUnstablePackages,
+	), nil
+}
+
+func getPluginConfigForV1Beta1PluginConfig(
+	registry string,
+	pluginConfigV1Beta1 *policyv1beta1.PolicyConfig_CheckPluginConfig,
+) (bufconfig.PluginConfig, error) {
+	nameV1Beta1 := pluginConfigV1Beta1.Name
+	pluginRef, err := bufparse.NewRef(
+		registry,
+		nameV1Beta1.Owner,
+		nameV1Beta1.Plugin,
+		nameV1Beta1.Ref,
+	)
+	if err != nil {
+		return nil, err
+	}
+	options, err := option.OptionsForProtoOptions(pluginConfigV1Beta1.Options)
+	if err != nil {
+		return nil, err
+	}
+	optionsMap := make(map[string]any)
+	options.Range(func(key string, value any) {
+		optionsMap[key] = value
+	})
+	return bufconfig.NewRemoteWasmPluginConfig(
+		pluginRef,
+		optionsMap,
+		pluginConfigV1Beta1.Args,
+	)
 }
