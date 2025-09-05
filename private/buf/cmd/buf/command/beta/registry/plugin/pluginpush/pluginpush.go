63d995bbe04f3b97c73e280c84c35df9cd9a0f3a
Edward McFarlane <3036610+emcfarlane@users.noreply.github.com>
2025-09-05 19:20:25 +0200

    Fix curated plugin latest fetching (#3994)

--- private/buf/cmd/buf/command/beta/registry/plugin/pluginpush/pluginpush.go
+++ private/buf/cmd/buf/command/beta/registry/plugin/pluginpush/pluginpush.go
@@ -307,6 +307,26 @@ func run(
 			slog.String("name", pluginConfig.Name.IdentityString()),
 			slog.String("digest", plugin.ContainerImageDigest()),
 		)
+		if latestPluginResp == nil {
+			latestPluginResp, err = service.GetLatestCuratedPlugin(
+				ctx,
+				connect.NewRequest(
+					registryv1alpha1.GetLatestCuratedPluginRequest_builder{
+						Owner:    pluginConfig.Name.Owner(),
+						Name:     pluginConfig.Name.Plugin(),
+						Version:  pluginConfig.PluginVersion,
+						Revision: 0, // get latest revision for the plugin version.
+					}.Build(),
+				),
+			)
+			if err != nil {
+				return fmt.Errorf("unable to fetch latest plugin after AlreadyExists error: %w", err)
+			}
+		}
+		// Ensure the image digest matches.
+		if latestPluginResp.Msg.GetPlugin().GetContainerImageDigest() != plugin.ContainerImageDigest() {
+			return fmt.Errorf("a plugin with the same name and version already exists, but with a different image digest (%s). If you want to push a new revision, please retry", latestPluginResp.Msg.GetPlugin().GetContainerImageDigest())
+		}
 		curatedPlugin = latestPluginResp.Msg.GetPlugin()
 	} else {
 		curatedPlugin = createPluginResp.Msg.GetConfiguration()
