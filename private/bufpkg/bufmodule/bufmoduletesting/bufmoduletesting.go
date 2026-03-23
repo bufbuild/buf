2e3990c0c39431c529efcfff71cf46fb23c0e00c
Stefan VanBuren <svanburen@buf.build>
2026-03-23 12:29:30 -0400

    Remove xslog.NopLogger usage (#4403)
    Dropped upstream in favor of standard library.
    
    Ref: bufbuild/standard-go#4

--- private/bufpkg/bufmodule/bufmoduletesting/bufmoduletesting.go
+++ private/bufpkg/bufmodule/bufmoduletesting/bufmoduletesting.go
@@ -19,9 +19,9 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"log/slog"
 	"time"
 
-	"buf.build/go/standard/xlog/xslog"
 	"buf.build/go/standard/xslices"
 	"github.com/bufbuild/buf/private/bufpkg/bufconfig"
 	"github.com/bufbuild/buf/private/bufpkg/bufmodule"
@@ -344,7 +344,7 @@ func newModuleSet(
 	// may be nil
 	commitIDToCreateTime map[uuid.UUID]time.Time,
 ) (bufmodule.ModuleSet, error) {
-	moduleSetBuilder := bufmodule.NewModuleSetBuilder(context.Background(), xslog.NopLogger, bufmodule.NopModuleDataProvider, bufmodule.NopCommitProvider)
+	moduleSetBuilder := bufmodule.NewModuleSetBuilder(context.Background(), slog.New(slog.DiscardHandler), bufmodule.NopModuleDataProvider, bufmodule.NopCommitProvider)
 	for i, moduleData := range moduleDatas {
 		if err := addModuleDataToModuleSetBuilder(
 			moduleSetBuilder,
