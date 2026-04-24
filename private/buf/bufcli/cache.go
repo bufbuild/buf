fe03adaf02143d1d44538309a4cba30e3369d0b9
Edward McFarlane <emcfarlane@buf.build>
2026-04-24 16:19:14 +0200

    Unbound WASM plugins for local checks

--- private/buf/bufcli/cache.go
+++ private/buf/bufcli/cache.go
@@ -216,7 +216,11 @@ func NewWasmRuntime(ctx context.Context, container appext.Container) (wasm.Runti
 		return nil, err
 	}
 	fullCacheDirPath := normalpath.Join(container.CacheDirPath(), v3CacheWasmRuntimeRelDirPath)
-	wasmRuntime, err := wasm.NewRuntime(ctx, wasm.WithLocalCacheDir(fullCacheDirPath))
+	wasmRuntime, err := wasm.NewRuntime(
+		ctx,
+		wasm.WithLocalCacheDir(fullCacheDirPath),
+		wasm.WithMaxMemoryBytes(1<<32-1), // Max for unbounded size locally.
+	)
 	if err != nil {
 		return nil, err
 	}
