ba489d2cc250ec14ded382015376ec7f479ffe86
Edward McFarlane <emcfarlane@buf.build>
2026-08-21 15:10:31 +0100

    Bump Go 1.27

--- make/go/dep_protoc_gen_go.mk
+++ make/go/dep_protoc_gen_go.mk
@@ -7,8 +7,8 @@ $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
 # Settable
-# https://github.com/protocolbuffers/protobuf-go/releases 20251212 checked 20251219
-PROTOC_GEN_GO_VERSION ?= v1.36.11
+# https://github.com/protocolbuffers/protobuf-go/releases 20260810 checked 20260813
+PROTOC_GEN_GO_VERSION ?= v1.36.12
 
 GO_GET_PKGS := $(GO_GET_PKGS) \
 	google.golang.org/protobuf/proto@$(PROTOC_GEN_GO_VERSION)
